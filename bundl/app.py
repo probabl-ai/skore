216d633b9b7a2f6e67ef9b809551737098c0a205
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-06-12 14:41:50 +0000

    moar

--- bundl/app.py
+++ bundl/app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 def fetch_mander(*path):
     InfoMander(*path)
 
-def render_views():
+def render_views(name, path):
     pass
 
 def render_info(*path):
     mander = InfoMander(*path)
-    pass
+    return mander.fetch()
 
 def render_logs():
     pass
@@ -29,39 +29,41 @@ def render_top_nav(*args):
     nav_temp = read_template('partials/nav-top.html')
     path_pairs = []
     for i, p in enumerate(args):
-        print(args)
         path_pairs.append(['/' + '/'.join(args[:i+1]), p])
-    print(path_pairs)
-    return nav_temp.render(path_pairs=path_pairs)
+    curr_file_path = Path('.datamander')
+    for arg in args:
+        curr_file_path = curr_file_path / arg
+    glob = Path(curr_file_path).glob("*")
+    links_out = [str(g).replace('.datamander', '') for g in glob if g.is_dir()]
+    return nav_temp.render(path_pairs=path_pairs, links_out=links_out)
+
+def render_mid_nav(*args):
+    nav_temp = read_template('partials/nav-mid.html')
+    return nav_temp.render(path='/'.join(args))
 
 def render_mander(*args):
     p = Path(__file__).parent / 'templates' / 'page.html'
     t = Template(p.read_text())
     res = render_top_nav(*args)
-    res += t.render(breadcrumbs=args)
-    return res
+    res += render_mid_nav(*args)
+    return t.render(body=res)
 
-@app.route('/')
-def home():
-    return render_mander()
-
-@app.route('/<path1>')
-def home1(path1):
-    print('this is hit')
-    return render_mander(*[path1])
-
-@app.route('/<path1>/<path2>')
-def home2(path1, path2):
-    return render_mander(*[path1, path2])
-
-@app.route('/<path1>/<path2>/<path3>')
-def home3(path1, path2, path3):
-    return render_mander(*[path1, path2, path3])
-
-@app.route('/<path1>/<path2>/<path3>/<path4>')
-def home4(path1, path2, path3, path4):
-    return render_mander(*[path1, path2, path3, path4])
-
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def home(path):
+    if len(path) == 0:
+        return render_mander(*[])
+    path_parts = path.split('/')
+    print(path_parts)
+    if path_parts[0] == 'info':
+        return render_info(*path_parts[1:])
+    if path_parts[0] == 'views':
+        return 'info fetches!'
+    if path_parts[0] == 'logs':
+        return 'info fetches!'
+    if path_parts[0] == 'artifacts':
+        return 'info fetches!'
+    return render_mander(*path.split('/'))
 
 if __name__ == '__main__':
     app.run(debug=True, reload=True)
