34692d9c96afa9f2b6cba54533ccd88ebe63726b
Auguste Baum <auguste@probabl.ai>
2024-07-03 17:43:55 +0200

    [squashme] wip

--- mandr/infomander.py
+++ mandr/infomander.py
@@ -1,27 +1,30 @@
-import json
+from pathlib import Path
 from time import time
+
 from diskcache import Cache
 from joblib import dump
-from pathlib import Path
 from rich.console import Console
+
 from .templates import TemplateRenderer
 
 console = Console()
-LOGS_KEY = 'logs'
-VIEWS_KEY = 'views'
-TEMPLATES_KEY = 'templates'
-ARTIFACTS_KEY = 'artifacts'
+LOGS_KEY = "logs"
+VIEWS_KEY = "views"
+TEMPLATES_KEY = "templates"
+ARTIFACTS_KEY = "artifacts"
 
-STATS_FOLDER = '.stats'
-ARTIFACTS_FOLDER = '.artifacts'
-LOGS_FOLDER = '.logs'
+STATS_FOLDER = ".stats"
+ARTIFACTS_FOLDER = ".artifacts"
+LOGS_FOLDER = ".logs"
+
 
 class InfoMander:
     """Represents a dictionary, on disk, with a path-like structure."""
-    def __init__(self, path):        
+
+    def __init__(self, path):
         # Set local disk paths
         self.path = path
-        self.project_path = Path('.datamander/' + path)
+        self.project_path = Path(".datamander/" + path)
         self.cache = Cache(self.project_path / STATS_FOLDER)
 
         # For practical reasons the logs and artifacts are stored on disk, not sqlite
@@ -33,37 +36,39 @@ class InfoMander:
         for key in [ARTIFACTS_KEY, TEMPLATES_KEY, VIEWS_KEY, LOGS_KEY]:
             if key not in self.cache:
                 self.cache[key] = {}
-        
+
         # This will be used for rendering templates into views
         self.renderer = TemplateRenderer(self)
-    
+
     def _check_key(self, key):
         if key in [ARTIFACTS_KEY, TEMPLATES_KEY, VIEWS_KEY, LOGS_KEY]:
-            raise ValueError(f'Cannot overwrite {key} key. This is reserved for internal use.')
-               
-    def add_info(self, key, value, method='overwrite'):
-        if method == 'overwrite':
+            raise ValueError(
+                f"Cannot overwrite {key} key. This is reserved for internal use."
+            )
+
+    def add_info(self, key, value, method="overwrite"):
+        if method == "overwrite":
             self.cache[key] = value
-        if method == 'append':
+        if method == "append":
             if not isinstance(value, list):
                 value = [value]
             if key in self.cache:
                 value = value + self.cache[key]
             self.cache[key] = value
-        self.cache['updated_at'] = int(time())
+        self.cache["updated_at"] = int(time())
 
     def _add_to_key(self, top_key, key, value):
-        orig = self.cache[top_key] 
+        orig = self.cache[top_key]
         orig[key] = value
         self.add_info(top_key, orig)
 
     def add_artifact(self, key, obj, **metadata):
         self._check_key(key)
-        file_location = self.artifact_path / f'{key}.joblib'
+        file_location = self.artifact_path / f"{key}.joblib"
         if not file_location.parent.exists():
             file_location.parent.mkdir(parents=True)
         dump(obj, file_location)
-        self._add_to_key(ARTIFACTS_KEY, key, {'obj': obj, **metadata})
+        self._add_to_key(ARTIFACTS_KEY, key, {"obj": obj, **metadata})
 
     def add_view(self, key, html):
         self._check_key(key)
@@ -72,34 +77,39 @@ class InfoMander:
     def add_template(self, key, template):
         self._check_key(key)
         if key in self.cache[VIEWS_KEY].keys():
-            raise ValueError(f'Cannot add template {key} because there is already a view with the same name.')
-        self._add_to_key('_templates', key, template)
+            raise ValueError(
+                f"Cannot add template {key} because there is already a view with the same name."
+            )
+        self._add_to_key("_templates", key, template)
 
     def render_templates(self):
-        for name, template in self.cache['_templates'].items():
+        for name, template in self.cache["_templates"].items():
             self.add_view(name, template.render(self))
 
     def add_logs(self, key, logs):
         self._add_to_key(LOGS_KEY, key, logs)
-                    
+
     def fetch(self):
         return {k: self.cache[k] for k in self.cache.iterkeys()}
-    
+
     def __getitem__(self, key):
         return self.cache[key]
 
     @classmethod
     def get_property(cls, mander, dsl_str):
-        # Note that we may be dealing with a nested retreival
-        prop_chain = [e for e in dsl_str.split('.') if e]
+        # Note that we may be dealing with a nested retrieval
+        prop_chain = [e for e in dsl_str.split(".") if e]
         if len(prop_chain) == 1:
             without_dot = prop_chain[0]
             return mander.cache[without_dot]
-        
+
         # Handle special case with getting a property from direct children
-        if prop_chain[0].startswith('*'):
-            return [child.get('@mander' + '.'.join(prop_chain[1:])) for child in mander.children()]
-        
+        if prop_chain[0].startswith("*"):
+            return [
+                child.get("@mander" + ".".join(prop_chain[1:]))
+                for child in mander.children()
+            ]
+
         # At this point we know for sure it's just a nested property in a single mander
         item_of_interest = mander.cache[prop_chain[0]]
         for prop in prop_chain[1:]:
@@ -107,9 +117,9 @@ class InfoMander:
         return item_of_interest
 
     def dsl_path_exists(self, path):
-        actual_path = '.datamander' / path  
+        actual_path = ".datamander" / path
         assert Path(actual_path).exists()
-    
+
     def get_child(self, *path):
         new_path = self.project_path
         for p in path:
@@ -117,19 +127,23 @@ class InfoMander:
         return InfoMander(str(new_path))
 
     def children(self):
-        return [InfoMander('/'.join(p.parts[1:])) for p in self.project_path.iterdir() if p.is_dir() and not p.name.startswith('.')]
+        return [
+            InfoMander("/".join(p.parts[1:]))
+            for p in self.project_path.iterdir()
+            if p.is_dir() and not p.name.startswith(".")
+        ]
 
     def get(self, dsl_str):
-        if '@mander' not in dsl_str:
-            raise ValueError('@mander is needed at the start of dsl string')
-        path = [p for p in dsl_str.replace('@mander', '').split('/') if p]
+        if "@mander" not in dsl_str:
+            raise ValueError("@mander is needed at the start of dsl string")
+        path = [p for p in dsl_str.replace("@mander", "").split("/") if p]
         # There is no path to another mander, but we may have a nested property
         if len(path) == 1:
-            if path[0] == '*':
+            if path[0] == "*":
                 return self.children()
             return InfoMander.get_property(self, path[0])
         mander = self.get_child(*path[:-1])
         return mander.get_property(mander, path[-1])
-    
+
     def __repr__(self):
-        return f'InfoMander({self.project_path})'
+        return f"InfoMander({self.project_path})"
--- mandr/templates.py
+++ mandr/templates.py
@@ -1,60 +1,65 @@
-import markdown
-import pandas as pd 
+import json
+
 import altair as alt
-import json 
+import markdown
+import pandas as pd
 from jinja2 import Template
 from sklearn.utils._estimator_html_repr import estimator_html_repr
 
 
 def sklearn_model_repr(pipeline):
-    print('pipeline', pipeline)
+    print("pipeline", pipeline)
     return estimator_html_repr(pipeline)
 
 
 def scatter_chart(title, x, y, **kwargs):
     # Grab the dataframe that is assumed to be stored in the datamander.
-    dataf = pd.DataFrame({'x': x, 'y': y})
+    dataf = pd.DataFrame({"x": x, "y": y})
 
     # Render the altair chart internally
-    c = alt.Chart(dataf).mark_circle(size=60).encode(
-        x='x',
-        y='y',
-    ).interactive().properties(title=title)
+    c = (
+        alt.Chart(dataf)
+        .mark_circle(size=60)
+        .encode(
+            x="x",
+            y="y",
+        )
+        .interactive()
+        .properties(title=title)
+    )
 
     # Add the container width property
     json_blob = json.loads(c.to_json())
-    json_blob['width'] = "container"
-    
+    json_blob["width"] = "container"
+
     return c.to_html()
 
 
-registry = {
-    'scatter-chart': scatter_chart,
-    'sklearn-model-repr': sklearn_model_repr
-}
+registry = {"scatter-chart": scatter_chart, "sklearn-model-repr": sklearn_model_repr}
 
 
 class TemplateRenderer:
     """We do a few things on top of Jinja2 here"""
+
     def __init__(self, mander):
         self.mander = mander
-    
-    def clean_value(self, val): 
-        return val.replace('/>', '').replace('"', '').replace("'", '')
-        
+
+    def clean_value(self, val):
+        return val.replace("/>", "").replace('"', "").replace("'", "")
+
     def insert_custom_ui(self, template):
         # For each registered element, check if it is there.
         for name, func in registry.items():
-            element_of_interest = f'<{name}'
-            print('element_of_interest', element_of_interest)
+            element_of_interest = f"<{name}"
+            print("element_of_interest", element_of_interest)
             start = template.find(element_of_interest)
             end = template[start:].find("/>")
-            substr = template[start:start + end + 2]
+            substr = template[start : start + end + 2]
             if substr:
-                elems = [e.split('=') for e in substr.split(' ') if '=' in e]
+                elems = [e.split("=") for e in substr.split(" ") if "=" in e]
                 params = {k: self.clean_value(v) for k, v in elems}
                 for k, v in params.items():
-                    if v.startswith('@mander'):
+                    if v.startswith("@mander"):
                         params[k] = self.mander.get(v)
                 ui = func(**params)
                 template = template.replace(substr, ui)
