a43cdb354a32682de15accb7734baee1a8a5ff5d
Matthieu Jouis <matthieu@probabl.ai>
2024-07-08 16:21:18 +0200

    lint & tests CI workflow

--- tests/test_mander_dsl.py
+++ tests/test_mander_dsl.py
@@ -2,3 +2,8 @@
 The mander comes with a special DSL to fetch the right elements from the cache.
 This is tested here.
 """
+
+
+def test_the_evidence():
+    """Temporary test to ensure that CI runs it."""
+    assert 2 + 2 == 4
