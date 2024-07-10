f2416b44b78081132101c40866307cd95827b233
Matthieu Jouis <matthieu@probabl.ai>
2024-07-10 18:12:16 +0200

    GET /mandrs/{path}

--- tests/integration/dashboard/test_webapp.py
+++ tests/integration/dashboard/test_webapp.py
@@ -18,3 +18,14 @@ def test_get_mandrs(client: TestClient):
     mander_paths = response.json()
     assert len(mander_paths) == number_of_manders
     assert response.status_code == 200
+
+
+def test_get_mandr(client: TestClient):
+    mander_path = "probabl-ai/test-mandr/42"
+    mander = InfoMander(mander_path)
+    mander.add_info("hey", "ho let's go")
+
+    response = client.get(f"/mandrs/{mander_path}")
+    mander_json = response.json()
+    assert mander_path in mander_json.get("path")
+    assert response.status_code == 200
