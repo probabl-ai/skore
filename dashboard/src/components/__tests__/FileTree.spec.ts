0e6620c8e810663bf5a99b8d8752b0022040a74b
Matthieu Jouis <matthieu@probabl.ai>
2024-07-12 14:28:07 +0200

    enable frontend code testing in CI

--- dashboard/src/components/__tests__/FileTree.spec.ts
+++ dashboard/src/components/__tests__/FileTree.spec.ts
@@ -3,9 +3,9 @@ import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import FileTree from "../FileTree.vue";
 
-describe("HelloWorld", () => {
+describe("FileTree", () => {
   it("renders properly", () => {
     const wrapper = mount(FileTree);
-    expect(wrapper.text()).toContain("Hey");
+    expect(wrapper.text()).toContain("file tree");
   });
 });
