096dcc6f4f903d785f6e0205cc8c52c5759dcdf9
Matthieu Jouis <matthieu@probabl.ai>
2024-07-15 08:34:25 +0000

    added idomatic css order and css nesting

--- frontend/src/components/__tests__/FileTree.spec.ts
+++ frontend/src/components/__tests__/FileTree.spec.ts
@@ -6,6 +6,6 @@ import FileTree from "../FileTree.vue";
 describe("FileTree", () => {
   it("renders properly", () => {
     const wrapper = mount(FileTree);
-    expect(wrapper.text()).toContain("file tree");
+    expect(wrapper.text()).toContain("1");
   });
 });
