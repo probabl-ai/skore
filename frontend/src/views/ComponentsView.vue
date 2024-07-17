eb7d2b8c2f90261466b6bdae0ad47c1e4e811754
Matthieu Jouis <matthieu@probabl.ai>
2024-07-17 15:04:27 +0200

    crude tab component

--- frontend/src/views/ComponentsView.vue
+++ frontend/src/views/ComponentsView.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import markdownString from "../assets/fixtures/markdown.md?raw";
 import MarkdownWidget from "../components/MarkdownWidget.vue";
+import Tabs from "../components/TabsWidget.vue";
+import TabsItem from "../components/TabsWidgetItem.vue";
 </script>
 
 <template>
   <main>
     <h1>Components library</h1>
-    <MarkdownWidget :source="markdownString" />
+    <Tabs :tab-names="['markdown', 'vega']">
+      <TabsItem :value="0">
+        <MarkdownWidget :source="markdownString" />
+      </TabsItem>
+      <TabsItem :value="1">
+        <div>vega</div>
+      </TabsItem>
+    </Tabs>
   </main>
 </template>
