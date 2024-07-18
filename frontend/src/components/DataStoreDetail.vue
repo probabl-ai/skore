39808c1805b64906897e86daf0afd27912539148
Matthieu Jouis <matthieu@probabl.ai>
2024-07-18 16:12:09 +0200

    DataStoreItemList component

--- frontend/src/components/DataStoreDetail.vue
+++ frontend/src/components/DataStoreDetail.vue
@@ -1,54 +1,26 @@
 <script setup lang="ts">
 import { type DataStore } from "../models";
+import DataStoreItemList from "./DataStoreItemList.vue";
 
 const props = defineProps<{ dataStore: DataStore }>();
 </script>
 
 <template>
-  <h1>{{ props.dataStore.path }}</h1>
-  <aside>
-    <h2>Views</h2>
-    <div v-for="(value, name, index) in props.dataStore.views" :key="index">
-      <h3>{{ name }}</h3>
-      <div v-html="value" />
-    </div>
-  </aside>
-  <aside>
-    <h2>Logs</h2>
-    <div v-for="(value, name, index) in props.dataStore.logs" :key="index">
-      <h3>{{ name }}</h3>
-      <div v-html="value" />
-    </div>
-  </aside>
-  <aside>
-    <h2>Artifacts</h2>
-    <div v-for="(value, name, index) in props.dataStore.artifacts" :key="index">
-      <h3>{{ name }}</h3>
-      <div v-html="value.path" />
-    </div>
-  </aside>
-  <aside>
-    <h2>Info</h2>
-    <div v-for="(value, name, index) in props.dataStore.info" :key="index">
-      <h3>{{ name }}</h3>
-      <div v-html="value" />
-    </div>
-  </aside>
+  <div class="item-list">
+    <DataStoreItemList title="Views" icon="icon-plot" :elements="Object.keys(dataStore.views)" />
+    <DataStoreItemList title="Info" icon="icon-text" :elements="Object.keys(dataStore.info)" />
+    <DataStoreItemList title="Logs" icon="icon-gift" :elements="Object.keys(dataStore.logs)" />
+    <DataStoreItemList
+      title="Logs"
+      icon="icon-folder"
+      :elements="Object.keys(dataStore.artifacts)"
+    />
+  </div>
 </template>
 
 <style scoped>
-aside {
-  border: 1px solid #e9e9e9;
-  margin: 5px;
-
-  h2 {
-    background-color: #f2f1f1;
-    color: #7e7e7e;
-    font-weight: 300;
-  }
-
-  & > div {
-    padding: 5px;
-  }
+.item-list {
+  display: flex;
+  flex-direction: column;
 }
 </style>
