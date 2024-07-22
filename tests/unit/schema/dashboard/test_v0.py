567bcc21c95fb59f689b319a0cee1fc109eb33c0
Thomas S <thomas@probabl.ai>
2024-07-23 01:07:56 +0200

    Rewrite lightly the API exchange JSON schema to have explicit errors

--- tests/unit/schema/dashboard/test_v0.py
+++ tests/unit/schema/dashboard/test_v0.py
@@ -1,12 +1,14 @@
+from contextlib import nullcontext as does_not_raise
 from datetime import UTC, date, datetime
 
 import altair as alt
 import httpx
-import jsonschema
 import mandr.schema.dashboard
 import pandas as pd
 import pytest
 import referencing
+from jsonschema import Draft202012Validator
+from jsonschema.exceptions import ValidationError
 
 
 class TestV0:
@@ -29,83 +31,113 @@ class TestV0:
             return referencing.Resource.from_contents(response.json())
 
         registry = referencing.Registry(retrieve=retrieve_via_httpx)
-        draft202012validator = jsonschema.Draft202012Validator(
+        draft202012validator = Draft202012Validator(
             schema=mandr.schema.dashboard.v0,
             registry=registry,
-            format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER,
+            format_checker=Draft202012Validator.FORMAT_CHECKER,
         )
 
         return draft202012validator.validate
 
     @pytest.mark.parametrize(
-        "payload",
+        "payload,expectation",
         [
-            {"key": {"type": "any", "data": None}},
-            {"key": {"type": "array", "data": [0, 1, 2]}},
-            {"key": {"type": "boolean", "data": True}},
-            {"key": {"type": "date", "data": date.today().isoformat()}},
-            {"key": {"type": "datetime", "data": datetime.now(tz=UTC).isoformat()}},
-            {
-                "key": {
+            (
+                {"type": "unknown", "data": None},
+                pytest.raises(
+                    ValidationError, match="Failed validating 'enum' in schema"
+                ),
+            ),
+            ({"type": "any", "data": None}, does_not_raise()),
+            ({"type": "array", "data": [0, 1, 2]}, does_not_raise()),
+            (
+                {"type": "array", "data": (0, 1, 2)},
+                pytest.raises(ValidationError, match="is not of type 'array'"),
+            ),
+            ({"type": "boolean", "data": True}, does_not_raise()),
+            (
+                {"type": "boolean", "data": "True"},
+                pytest.raises(ValidationError, match="is not of type 'boolean'"),
+            ),
+            ({"type": "date", "data": date.today().isoformat()}, does_not_raise()),
+            (
+                {"type": "date", "data": datetime.now(tz=UTC).isoformat()},
+                pytest.raises(ValidationError, match="is not a 'date'"),
+            ),
+            (
+                {"type": "datetime", "data": datetime.now(tz=UTC).isoformat()},
+                does_not_raise(),
+            ),
+            (
+                {"type": "datetime", "data": date.today().isoformat()},
+                pytest.raises(ValidationError, match="is not a 'date-time'"),
+            ),
+            (
+                {
                     "type": "file",
                     "data": "file:///tmp/file.txt",
                     "metadata": None,
                     "internal": None,
-                }
-            },
-            {"key": {"type": "html", "data": "<!DOCTYPE html><head></head></html>"}},
-            {"key": {"type": "integer", "data": 1}},
-            {"key": {"type": "float", "data": 1.2}},
-            {"key": {"type": "string", "data": "True"}},
-            {
-                "key": {
+                },
+                does_not_raise(),
+            ),
+            (
+                {
+                    "type": "file",
+                    "data": "file.txt",
+                    "metadata": None,
+                    "internal": None,
+                },
+                pytest.raises(ValidationError, match="is not a 'uri'"),
+            ),
+            (
+                {"type": "html", "data": "<!DOCTYPE html><head></head></html>"},
+                does_not_raise(),
+            ),
+            (
+                {"type": "html", "data": "<head></head>"},
+                pytest.raises(ValidationError, match="Failed validating 'pattern'"),
+            ),
+            ({"type": "integer", "data": 1}, does_not_raise()),
+            (
+                {"type": "integer", "data": 1.2},
+                pytest.raises(ValidationError, match="is not of type 'integer'"),
+            ),
+            ({"type": "number", "data": 1.2}, does_not_raise()),
+            (
+                {"type": "number", "data": "1.2"},
+                pytest.raises(ValidationError, match="is not of type 'number'"),
+            ),
+            ({"type": "string", "data": "True"}, does_not_raise()),
+            (
+                {"type": "string", "data": True},
+                pytest.raises(ValidationError, match="is not of type 'string'"),
+            ),
+            (
+                {
                     "type": "vega",
                     "data": (
                         alt.Chart(pd.DataFrame({"a": ["A"], "b": [28]}))
                         .mark_bar()
                         .to_dict()
                     ),
-                }
-            },
+                },
+                does_not_raise(),
+            ),
+            (
+                {"type": "vega", "data": None},
+                pytest.raises(
+                    ValidationError, match="is not valid under any of the given schemas"
+                ),
+            ),
         ],
     )
-    def test_payload_validation(self, validator, payload):
-        validator(
-            instance={
-                "schema": "schema:dashboard:v0",
-                "uri": "test",
-                "payload": payload,
-            }
-        )
-
-    @pytest.mark.parametrize(
-        "payload",
-        [
-            {"key": {"type": "array", "data": (0, 1, 2)}},
-            {"key": {"type": "boolean", "data": "True"}},
-            {"key": {"type": "date", "data": datetime.now(tz=UTC).isoformat()}},
-            {"key": {"type": "datetime", "data": date.today().isoformat()}},
-            {
-                "key": {
-                    "type": "file",
-                    "data": "file.txt",
-                    "metadata": None,
-                    "internal": None,
-                }
-            },
-            {"key": {"type": "html", "data": "<head></head>"}},
-            {"key": {"type": "integer", "data": 1.2}},
-            {"key": {"type": "float", "data": "1.2"}},
-            {"key": {"type": "string", "data": True}},
-            {"key": {"type": "vega", "data": None}},
-        ],
-    )
-    def test_payload_invalidation(self, validator, payload):
-        with pytest.raises(jsonschema.exceptions.ValidationError):
+    def test_payload_validation(self, validator, payload, expectation):
+        with expectation:
             validator(
                 instance={
                     "schema": "schema:dashboard:v0",
                     "uri": "test",
-                    "payload": payload,
+                    "payload": {"key": payload},
                 }
             )
