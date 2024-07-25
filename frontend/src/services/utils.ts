6c7836efdf2361d0459d30de43e0ee86f8626086
Matthieu Jouis <matthieu@probabl.ai>
2024-07-25 11:10:17 +0200

    markdown widgte treats objetx as json code

--- frontend/src/services/utils.ts
+++ frontend/src/services/utils.ts
@@ -1,3 +1,7 @@
 export function sleep(delay: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, delay));
 }
+
+export function isString(s: any) {
+  return typeof s === "string" || s instanceof String;
+}
