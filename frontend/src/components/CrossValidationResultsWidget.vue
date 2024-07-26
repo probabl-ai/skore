6d1b0d385bd2a87ad7d572340087886f706436e3
Auguste Baum <auguste@probabl.ai>
2024-07-26 14:41:13 +0200

    add cross-validation results widget
    <!-- ps-id: 7a8228e9-333d-48b3-b1b8-2113eb6b952c -->

---
+++ frontend/src/components/CrossValidationResultsWidget.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { VisualizationSpec } from "vega-embed";
+import type { DataFrameWidgetProps } from "@/components/DataFrameWidget.vue";
+import DataFrameWidget from "@/components/DataFrameWidget.vue";
+import VegaWidget from "@/components/VegaWidget.vue";
+
+const props = defineProps<{
+  roc_curve_spec: VisualizationSpec;
+  cv_results_table: DataFrameWidgetProps;
+}>();
+</script>
+
+<template>
+  <VegaWidget :spec="props.roc_curve_spec" />
+  <DataFrameWidget :columns="props.cv_results_table.columns" :data="props.cv_results_table.data" />
+</template>
