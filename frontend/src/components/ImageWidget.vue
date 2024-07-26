46c419153d52467aa6a475e99844c75a27db7c96
Matt J <matthieu@probabl.ai>
2024-07-26 10:52:33 +0200

    96 create an image widget that handles svg jpeg png  webp (#100)
    **⚠️ depends on #91** 
    
    - The new widget as been implemented.
    - Integrated to the component page.
    <img width="1309" alt="Screenshot 2024-07-25 at 15 16 51"
    src="https://github.com/user-attachments/assets/5906045c-74fc-4f1e-aa09-34d3faae4648">
    - Plugged in the dashboard
    <img width="744" alt="Screenshot 2024-07-25 at 15 16 38"
    src="https://github.com/user-attachments/assets/c64910ef-7677-4590-ac9e-337132e66587">
    
    Mock json has been updated.
    
    ---------
    
    Co-authored-by: Auguste Baum <auguste@probabl.ai>

---
+++ frontend/src/components/ImageWidget.vue
@@ -0,0 +1,18 @@
+<script setup lang="tsx">
+import { computed } from "vue";
+
+interface ImageWidgetProps {
+  base64Src: string;
+  mimeType: "image/svg+xml" | "image/png" | "image/jpeg" | "image/webp";
+  alt?: string;
+}
+
+const props = withDefaults(defineProps<ImageWidgetProps>(), {
+  alt: "",
+});
+const src = computed(() => `data:${props.mimeType};base64,${props.base64Src}`);
+</script>
+
+<template>
+  <img :src="src" :alt="alt" />
+</template>
