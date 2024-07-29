dad1f5e7a70a57583e710fd2158a9cb2be1b1b4b
Thomas S <thomas@probabl.ai>
2024-07-29 12:29:15 +0200

    Move `src/dashboad_type.py` to `src/item/display_type.py`

---
+++ tests/unit/item/test_display_type.py
@@ -0,0 +1,26 @@
+import datetime
+import pathlib
+
+import pytest
+from mandr.display_type import DisplayType
+
+
+@pytest.mark.parametrize(
+    "x,expected",
+    [
+        (3, DisplayType.INTEGER),
+        (None, DisplayType.ANY),
+        ((1, "b"), DisplayType.ANY),
+        ("hello", DisplayType.MARKDOWN),
+        (3.0, DisplayType.NUMBER),
+        (False, DisplayType.BOOLEAN),
+        ([1, 2], DisplayType.ARRAY),
+        (datetime.date(2024, 7, 24), DisplayType.DATE),
+        (datetime.datetime(2024, 7, 24), DisplayType.DATETIME),
+        (pathlib.PosixPath("./my_file.txt"), DisplayType.FILE),
+        ({"a": 1}, DisplayType.ANY),
+        (set([1, 2]), DisplayType.ANY),
+    ],
+)
+def test_infer(x, expected):
+    assert DisplayType.infer(x) == expected
