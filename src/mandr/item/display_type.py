1529e193382a97a39bb1b9f14750e2594d9f7896
Thomas S <thomas@probabl.ai>
2024-08-05 11:51:57 +0200

    Revert some changes

--- src/mandr/item/display_type.py
+++ src/mandr/item/display_type.py
@@ -12,6 +12,7 @@ from enum import StrEnum, auto
 from typing import Any
 
 import altair
+import matplotlib.figure
 import numpy
 import pandas
 import polars
@@ -28,8 +29,10 @@ class DisplayType(StrEnum):
     DATETIME = auto()
     FILE = auto()
     HTML = auto()
+    IMAGE = auto()
     INTEGER = auto()
     MARKDOWN = auto()
+    MATPLOTLIB = auto()
     NUMBER = auto()
     NUMPY_ARRAY = auto()
     STRING = auto()
@@ -69,6 +72,7 @@ class DisplayType(StrEnum):
             datetime.datetime: DisplayType.DATETIME,
             int: DisplayType.INTEGER,
             str: DisplayType.MARKDOWN,
+            matplotlib.figure.Figure: DisplayType.MATPLOTLIB,
             float: DisplayType.NUMBER,
             numpy.ndarray: DisplayType.NUMPY_ARRAY,
             altair.vegalite.v5.api.Chart: DisplayType.VEGA,
