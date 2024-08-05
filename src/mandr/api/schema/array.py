1aa72081d433d27109cedf50bac8bf5764cbe051
Thomas S. <thomas@probabl.ai>
2024-08-05 14:10:24 +0200

    Add a dedicated exporter class to manage exchanges between store and dashboard (#126)
    Co-authored-by: Auguste Baum <auguste@probabl.ai>
    
    ---------
    
    Co-authored-by: Auguste Baum <auguste@probabl.ai>

---
+++ src/mandr/api/schema/array.py
@@ -0,0 +1,31 @@
+"""Schema to transfer listable value from store to dashboard."""
+
+import typing
+
+import pydantic
+
+
+class Array(pydantic.BaseModel):
+    """Schema to transfer listable value from store to dashboard.
+
+    Examples
+    --------
+    >>> Array(data=[1, 2, 3])
+    Array(...)
+
+    >>> Array(data=(1, 2, 3))
+    Array(...)
+
+    >>> Array(type="array", data=(1, 2, 3))
+    Array(...)
+    """
+
+    model_config = pydantic.ConfigDict(strict=True)
+
+    type: typing.Literal["array"] = "array"
+    data: typing.Iterable
+
+    @pydantic.field_serializer("data")
+    def serialize_data(self, data: typing.Iterable) -> list:
+        """Serialize data from iterable to list."""
+        return list(data)
