730780446f16e276dcc72ff808f6f45b4aaf1157
Thomas S. <thomas@probabl.ai>
2024-08-07 17:41:57 +0200

    Fix dashboard launch so that it can be run in a python script (#141)

--- src/mandr/dashboard/__init__.py
+++ src/mandr/dashboard/__init__.py
@@ -1,8 +1,9 @@
 """The dashboard to display stores."""
 
-from mandr.dashboard.app import create_dashboard_app, serve_dashboard_app
+from mandr.dashboard.app import create_dashboard_app
+from mandr.dashboard.dashboard import Dashboard
 
 __all__ = [
+    "Dashboard",
     "create_dashboard_app",
-    "serve_dashboard_app",
 ]
---
+++ src/mandr/dashboard/dashboard.py
@@ -0,0 +1,57 @@
+"""Dashboard to display stores."""
+
+import threading
+import webbrowser
+
+import uvicorn
+
+
+class Dashboard:
+    """Dashboard to display stores.
+
+    .. highlight:: python
+    .. code-block:: python
+
+        import contextlib
+        dashboard = Dashboard()
+        with contextlib.closing(dashboard.open()):
+            ...
+
+    .. highlight:: python
+    .. code-block:: python
+
+        dashboard = Dashboard()
+        dashboard.open()
+        ...
+        dashboard.close()
+    """
+
+    def __init__(self, *, port=8000):
+        configuration = uvicorn.Config(
+            app="mandr.dashboard.app:create_dashboard_app",
+            port=port,
+            log_level="error",
+            reload=True,
+            factory=True,
+        )
+
+        self.port = port
+        self.server = uvicorn.Server(configuration)
+        self.thread = threading.Thread(target=self.server.run)
+
+    def open(self, *, open_browser=True):
+        """Open the dashboard's activity."""
+        self.thread.start()
+
+        while not self.server.started:
+            continue
+
+        if open_browser:
+            webbrowser.open(f"http://localhost:{self.port}")
+
+        return self
+
+    def close(self):
+        """Close the dashboard's activity."""
+        self.server.should_exit = True
+        self.thread.join()
