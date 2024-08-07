4adcdca73e77e97bb12d5fcfffa4f180eeafb30c
Thomas S <thomas@probabl.ai>
2024-08-07 17:41:57 +0200

    Fix dashboard launch so that it can be run in a python script (#141)

--- src/mandr/dashboard/app.py
+++ src/mandr/dashboard/app.py
@@ -1,10 +1,7 @@
 """FastAPI factory used to create the dashboard to display stores."""
 
-import asyncio
-import webbrowser
 from pathlib import Path
 
-import uvicorn
 from fastapi.staticfiles import StaticFiles
 
 from mandr.api import create_api_app
@@ -26,27 +23,3 @@ def create_dashboard_app():
     )
 
     return app
-
-
-def serve_dashboard_app(port=8000):
-    """Serve the dashboard to display stores."""
-
-    async def server_coroutine():
-        server = uvicorn.Server(
-            uvicorn.Config(
-                app="mandr.dashboard.app:create_dashboard_app",
-                port=port,
-                log_level="error",
-                reload=True,
-                factory=True,
-            )
-        )
-
-        await server.serve()
-
-    async def open_browser_after_delay():
-        await asyncio.sleep(1)
-        webbrowser.open(f"http://localhost:{port}")
-
-    asyncio.create_task(server_coroutine())
-    asyncio.create_task(open_browser_after_delay())
