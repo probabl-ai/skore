907a771c3784e5bd0879ff3708b9e6dcceadb4d5
Auguste Baum <auguste@probabl.ai>
2024-08-07 16:42:18 +0200

    make app test simpler and more robust

--- tests/integration/dashboard/test_app.py
+++ tests/integration/dashboard/test_app.py
@@ -12,14 +12,5 @@ class TestDashboardApp:
         response = client.get("/")
 
         assert response.status_code == 200
-        assert response.content == (
-            b'<!DOCTYPE html>\n<html lang="en">\n\n<head>\n  <meta charset="UTF-8">\n  '
-            b'<link rel="icon" href="/favicon.ico" sizes="any">\n  <link rel="icon" hre'
-            b'f="/favicon.svg" type="image/svg+xml">\n  <link rel="apple-touch-icon" hr'
-            b'ef="/apple-touch-icon.png">\n  <meta name="theme-color" content="#1E22AA"'
-            b'>\n  <meta name="viewport" content="width=device-width, initial-scale=1.0'
-            b'">\n  <title>:mandr.</title>\n  <script type="module" crossorigin src='
-            b'"/assets/index-C7ihIfh-.js"></script>\n  <link rel="stylesheet" crossorig'
-            b'in href="/assets/index-B2ZdhJBO.css">\n</head>\n\n<body>\n  <div id="app'
-            b'"></div>\n</body>\n\n</html>\n'
-        )
+        assert b"<!DOCTYPE html>" in response.content
+        assert b"<title>:mandr.</title>" in response.content
