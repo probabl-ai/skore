5bf15c170282c588969b4e0e51900752b9043a64
Auguste Baum <auguste@probabl.ai>
2024-08-09 11:29:33 +0200

    set default behavior when `storage` argument is not given to `Store`
    Closes #125

---
+++ tests/integration/test_default_store.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import pytest
+from mandr.storage import FileSystem
+from mandr.store import Store
+from mandr.store.store import MandrRootException
+
+
+class TestDefaultStorage:
+    """Test the behaviour of initialisation Mandr when `storage` is not given."""
+
+    def test_absolute_path(self, monkeypatch, tmp_path):
+        """If MANDR_ROOT is an absolute path, the storage is in MANDR_ROOT."""
+        monkeypatch.setenv("MANDR_ROOT", str(tmp_path))
+
+        store = Store("root/probabl")
+
+        assert isinstance(store.storage, FileSystem)
+        assert Path(store.storage.cache.directory) == tmp_path
+
+    def test_relative_path(self, monkeypatch, tmp_path):
+        """If MANDR_ROOT is a relative path, we raise an error."""
+        monkeypatch.setattr("pathlib.Path.cwd", lambda: tmp_path)
+        monkeypatch.setenv("MANDR_ROOT", ".not_datamander")
+
+        with pytest.raises(MandrRootException, match="got '.not_datamander'"):
+            Store("root/probabl")
+
+    def test_relative_path_no_mandr_root(self, monkeypatch, tmp_path):
+        """If MANDR_ROOT is unset, the storage is in ".datamander",
+        relative to the current working directory."""
+        monkeypatch.setattr("pathlib.Path.cwd", lambda: tmp_path)
+
+        store = Store("root/probabl")
+
+        assert isinstance(store.storage, FileSystem)
+        assert Path(store.storage.cache.directory) == tmp_path / ".datamander"
