18129b05d1f9bf00d80d2853ed92c2b353b62885
Vincent Maladiere <maladiere.vincent@yahoo.fr>
2024-08-19 18:45:13 +0200

    add altair LayerChart to schema

--- src/mandr/api/schema/vega.py
+++ src/mandr/api/schema/vega.py
@@ -21,7 +21,7 @@ class Vega(pydantic.BaseModel):
     model_config = pydantic.ConfigDict(strict=True, arbitrary_types_allowed=True)
 
     type: typing.Literal["vega"] = "vega"
-    data: altair.vegalite.v5.api.Chart
+    data: typing.Union[altair.vegalite.v5.api.Chart, altair.vegalite.v5.api.LayerChart]
 
     @pydantic.field_serializer("data")
     def serialize_data(self, data: altair.vegalite.v5.api.Chart) -> dict:
