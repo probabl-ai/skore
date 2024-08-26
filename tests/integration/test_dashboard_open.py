2b8931a2d10363be520e548b97bbb3a73db864fd
Auguste Baum <52001167+augustebaum@users.noreply.github.com>
2024-08-26 17:09:09 +0200

    fix behavior when opening dashboard multiple times with the same port (#219)
    - exit fast when dashboard thread fails soon after starting (makes
    testing easier)
    - check if address is already in use before starting the dashboard, log
    an INFO-level message if it is
    - change default port from dashboard from 8000 to 22140 as 8000 is more
    likely to be already in use
    - add 5 minutes timeout for backend tests in CI
    
    Closes #212

--- tests/integration/test_dashboard_open.py
+++ tests/integration/test_dashboard_open.py
@@ -46,3 +46,28 @@ def test_dashboard_open_with_absolute_mandr_root(monkeypatch, tmp_path):
         assert response.is_success
 
     assert (tmp_path / ".datamander").exists()
+
+
+def test_dashboard_open_twice(monkeypatch, tmp_path, caplog):
+    monkeypatch.setenv("MANDR_ROOT", str(tmp_path / ".datamander"))
+
+    dashboard1 = Dashboard()
+    with contextlib.closing(dashboard1.open(open_browser=False)):
+        assert dashboard1.server.started
+
+        host = dashboard1.server.config.host
+        port = dashboard1.server.config.port
+        response = httpx.get(f"http://{host}:{port}/api/mandrs")
+        assert response.is_success
+
+        dashboard2 = Dashboard()
+        dashboard2.open(open_browser=False)
+        # assert "Address is already in use" was properly logged
+        assert caplog.record_tuples == [
+            (
+                "mandr",
+                20,
+                "Address 127.0.0.1:22140 is already in use. Check if the dashboard or "
+                "another service is already running at that address.",
+            )
+        ]
