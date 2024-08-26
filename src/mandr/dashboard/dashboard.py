1632017559005d9c9a0ca8a5be8e90531fc14334
Auguste Baum <52001167+augustebaum@users.noreply.github.com>
2024-08-26 17:09:09 +0200

    fix behavior when opening dashboard multiple times with the same port (#219)
    - exit fast when dashboard thread fails soon after starting (makes
    testing easier)
    - check if address is already in use before starting the dashboard, log
    an INFO-level message if it is
    - change default port from dashboard from 8000 to 22140 as 8000 is more
    likely to be already in use
    - add 5 minutes timeout for backend tests in CI
    
    Closes #212

--- src/mandr/dashboard/dashboard.py
+++ src/mandr/dashboard/dashboard.py
@@ -2,8 +2,11 @@
 
 import threading
 import webbrowser
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import uvicorn
+
+from mandr import logger
 
 
 class Dashboard:
@@ -26,7 +29,7 @@ class Dashboard:
         dashboard.close()
     """
 
-    def __init__(self, *, port=8000):
+    def __init__(self, *, port=22140):
         configuration = uvicorn.Config(
             app="mandr.dashboard.app:create_dashboard_app",
             port=port,
@@ -41,9 +44,30 @@ class Dashboard:
 
     def open(self, *, open_browser=True):
         """Open the dashboard's activity."""
+        try:
+            test_server = None
+            test_server = HTTPServer(("127.0.0.1", self.port), BaseHTTPRequestHandler)
+        except OSError as e:
+            if e.errno == 98:
+                logger.info(
+                    f"Address 127.0.0.1:{self.port} is already in use. "
+                    "Check if the dashboard or another service is already running at "
+                    "that address."
+                )
+                return
+        finally:
+            if test_server is not None:
+                test_server.server_close()
+
         self.thread.start()
 
         while not self.server.started:
+            if not self.thread.is_alive():
+                self.close()
+                logger.error(
+                    "Server failed to start; refer to runtime logs "
+                    "for more information."
+                )
             continue
 
         if open_browser:
