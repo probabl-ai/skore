13f8a9cf4c7591973094208baa90d083b934d01e
Matthieu Jouis <matthieu@probabl.ai>
2024-09-02 16:22:12 +0200

    unit test reports store

--- frontend/tests/test.utils.ts
+++ frontend/tests/test.utils.ts
@@ -1,3 +1,4 @@
+import { DataStore, type IPayloadItem, type ItemType, type Layout } from "@/models";
 import { flushPromises, mount } from "@vue/test-utils";
 import { vi } from "vitest";
 import { type ComponentPublicInstance, defineComponent, h, Suspense } from "vue";
@@ -5,10 +6,22 @@ import { type ComponentPublicInstance, defineComponent, h, Suspense } from "vue"
 export const mockedFetch = vi.fn();
 global.fetch = mockedFetch;
 
+/**
+ * Helper to create a fake fetch response.
+ * @param data the data to return
+ * @param status the http status code
+ * @returns a promise that will resolve with the given data
+ */
 export function createFetchResponse(data: object, status = 200) {
   return { json: () => new Promise((resolve) => resolve(data)), status };
 }
 
+/**
+ * Helper to mount a suspense component.
+ * @param component the component to mount.
+ * @param options options to pass to `vitest.mount`
+ * @returns the wrapped component
+ */
 export async function mountSuspense(
   component: new () => ComponentPublicInstance,
   options: any = {}
@@ -24,4 +37,37 @@ export async function mountSuspense(
   const wrapper = mount(suspensedComponent, options);
   await flushPromises();
   return wrapper;
+}
+
+/**
+ * Create a afake payload item.
+ * @param type the type of the paylaod item
+ * @param data the inner data of the item
+ * @returns an object with IPayloadItem interface
+ */
+export function makePayloadItem(type: ItemType, data: any = {}): IPayloadItem {
+  const now = new Date().toISOString();
+  return {
+    type,
+    data,
+    metadata: {
+      display_type: type,
+      created_at: now,
+      updated_at: now,
+    },
+  };
+}
+
+/**
+ * Create a fake `DataStore` model.
+ * @param uri the uri of the DataStore
+ * @param types a list of types that must be created
+ * @param layout the layout to store
+ */
+export function makeDataStore(uri: string, types: ItemType[], layout: Layout = []) {
+  const payload = types.reduce(
+    (previous, current) => ({ ...previous, [current]: makePayloadItem(current) }),
+    {}
+  );
+  return new DataStore(uri, payload, layout);
 }
