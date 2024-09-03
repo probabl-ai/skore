78f8a0adba6af512b681c3f706196d65ae595c89
Matt J. <matthieu@probabl.ai>
2024-09-03 11:01:08 +0200

    enable vitest coverage with v8 and enable report in github (#283)
    This PR allow the CI to generate a coverage report when it runs.
    The report is posted by a bot as a comment of the PR and also available
    in the action page.

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from "node:url";
+import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "./vite.config";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "e2e/**"],
+      root: fileURLToPath(new URL("./", import.meta.url)),
+      coverage: {
+        provider: "v8",
+        reporter: ["text", "json", "json-summary", "html"],
+      },
+    },
   })
-)
+);
