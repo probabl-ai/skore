16f2db19d42a9620d087d3aa7b7c80c6bb09d661
Matt J. <matthieu@probabl.ai>
2024-09-05 16:30:56 +0200

    change report card header (#293)
    Report cards now have a visual header separated from the content, in accordance with the Figma design.
    
    Light mode:
    ![Screenshot 2024-09-04 at 12 34
    36](https://github.com/user-attachments/assets/bf59f6dc-38af-4b33-95b9-bdeb4e1aa97e)
    
    Dark mode:
    ![Screenshot 2024-09-04 at 12 34
    47](https://github.com/user-attachments/assets/c891c4b6-02c9-4a86-b2a1-7840097ecdb0)

--- frontend/src/components/DataStoreCard.vue
+++ frontend/src/components/DataStoreCard.vue
@@ -3,6 +3,7 @@ import type { KeyLayoutSize, KeyMoveDirection } from "@/models";
 
 const props = defineProps<{
   title: string;
+  subtitle?: string;
   showButtons: boolean;
   canMoveUp: boolean;
   canMoveDown: boolean;
@@ -17,28 +18,36 @@ const emit = defineEmits<{
 
 <template>
   <div class="card">
-    <div v-if="props.showButtons" class="buttons">
-      <button @click="emit('layoutChanged', 'small')">
-        <span class="icon-grid-layout-small"></span>
-      </button>
-      <button @click="emit('layoutChanged', 'medium')">
-        <span class="icon-grid-layout-medium"></span>
-      </button>
-      <button @click="emit('layoutChanged', 'large')">
-        <span class="icon-grid-layout-large"></span>
-      </button>
-      <button @click="emit('layoutChanged', 'large')">
-        <span class="icon-grid-layout-large"></span>
-      </button>
-      <button v-if="props.canMoveUp" @click="emit('positionChanged', 'up')">
-        <span class="icon-up"></span>
-      </button>
-      <button v-if="props.canMoveDown" @click="emit('positionChanged', 'down')">
-        <span class="icon-down"></span>
-      </button>
-      <button @click="emit('cardRemoved')">x</button>
+    <div class="header">
+      <div class="titles">
+        <div class="title">{{ props.title }}</div>
+        <div class="subtitle" v-if="props.subtitle">
+          {{ props.subtitle }}
+        </div>
+      </div>
+      <div v-if="props.showButtons" class="buttons">
+        <button @click="emit('layoutChanged', 'small')">
+          <span class="icon-grid-layout-small"></span>
+        </button>
+        <button @click="emit('layoutChanged', 'medium')">
+          <span class="icon-grid-layout-medium"></span>
+        </button>
+        <button @click="emit('layoutChanged', 'large')">
+          <span class="icon-grid-layout-large"></span>
+        </button>
+        <button @click="emit('layoutChanged', 'large')">
+          <span class="icon-grid-layout-large"></span>
+        </button>
+        <button v-if="props.canMoveUp" @click="emit('positionChanged', 'up')">
+          <span class="icon-up"></span>
+        </button>
+        <button v-if="props.canMoveDown" @click="emit('positionChanged', 'down')">
+          <span class="icon-down"></span>
+        </button>
+        <button @click="emit('cardRemoved')">x</button>
+      </div>
     </div>
-    <div class="header">{{ props.title }}</div>
+    <hr />
     <div class="content">
       <slot></slot>
     </div>
@@ -56,50 +65,84 @@ const emit = defineEmits<{
     background-color var(--transition-duration) var(--transition-easing),
     border var(--transition-duration) var(--transition-easing);
 
-  .buttons {
-    position: absolute;
-    top: var(--spacing-padding-large);
-    right: var(--spacing-padding-large);
+  .header {
     display: flex;
-    gap: var(--spacing-gap-small);
-    opacity: 0;
-    transition: opacity var(--transition-duration) var(--transition-easing);
+    align-items: flex-start;
+    justify-content: space-between;
+    color: var(--text-color-highlight);
+    font-size: var(--text-size-normal);
+    font-weight: var(--text-weight-title);
 
-    & button {
-      width: 16.5px;
-      height: 16.5px;
-      padding: 0;
-      border: none;
-      border-radius: var(--border-radius);
-      margin: 0;
-      background-color: transparent;
-      color: var(--text-color-highlight);
-      font-size: 8px;
-      line-height: 8px;
-      text-align: center;
-      transition:
-        background-color var(--transition-duration) var(--transition-easing),
-        color var(--transition-duration) var(--transition-easing);
+    & .titles {
+      position: relative;
+      padding-left: calc(var(--spacing-padding-small) + 4px);
 
-      &:hover {
-        background-color: var(--text-color-title);
-        color: var(--button-color);
+      & .title {
+        color: var(--text-color-highlight);
+        font-size: var(--text-size-highlight);
+        font-weight: var(--text-weight-title);
+      }
+
+      & .subtitle {
+        color: var(--text-color-normal);
+        font-size: var(--text-size-normal);
+        font-weight: var(--text-weight-normal);
+      }
+
+      &::before {
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: block;
+        width: 4px;
+        height: 100%;
+        border-radius: var(--border-radius);
+        background-color: var(--color-primary);
+        content: "";
+      }
+    }
+
+    & .buttons {
+      display: flex;
+      gap: var(--spacing-gap-small);
+      opacity: 0;
+      transition: opacity var(--transition-duration) var(--transition-easing);
+
+      & button {
+        width: 16.5px;
+        height: 16.5px;
+        padding: 0;
+        border: none;
+        border-radius: var(--border-radius);
+        margin: 0;
+        background-color: transparent;
+        color: var(--text-color-highlight);
+        font-size: 8px;
+        line-height: 8px;
+        text-align: center;
+        transition:
+          background-color var(--transition-duration) var(--transition-easing),
+          color var(--transition-duration) var(--transition-easing);
+
+        &:hover {
+          background-color: var(--text-color-title);
+          color: var(--button-color);
+        }
       }
     }
   }
 
-  .header {
-    margin-bottom: var(--spacing-padding-large);
-    color: var(--text-color-highlight);
-    font-size: var(--text-size-normal);
-    font-weight: var(--text-weight-title);
+  & hr {
+    border: none;
+    border-top: solid 1px var(--border-color-normal);
+    margin: var(--spacing-padding-large) 0;
   }
 
   &:hover {
     border: solid 1px var(--border-color-elevated);
     background-color: var(--background-color-elevated);
 
-    & .buttons {
+    & .header .buttons {
       opacity: 1;
     }
   }
