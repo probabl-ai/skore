3568a2b1c045e2c63383551033aa8e5989508671
Auguste Baum <auguste@probabl.ai>
2024-09-13 15:47:17 +0200

    fix item tests

--- tests/unit/item/test_numpy_array_item.py
+++ tests/unit/item/test_numpy_array_item.py
@@ -4,22 +4,34 @@ from skore.item import NumpyArrayItem
 
 
 class TestNumpyArrayItem:
+    @pytest.fixture(autouse=True)
+    def monkeypatch_datetime(self, monkeypatch, MockDatetime):
+        monkeypatch.setattr("skore.item.numpy_array_item.datetime", MockDatetime)
+
     @pytest.mark.order(0)
-    def test_factory(self):
+    def test_factory(self, mock_nowstr):
         array = numpy.array([1, 2, 3])
         array_list = array.tolist()
 
         item = NumpyArrayItem.factory(array)
 
-        assert vars(item) == {"array_list": array_list}
+        assert vars(item) == {
+            "array_list": array_list,
+            "created_at": mock_nowstr,
+            "updated_at": mock_nowstr,
+        }
 
     @pytest.mark.order(1)
-    def test_array(self):
+    def test_array(self, mock_nowstr):
         array = numpy.array([1, 2, 3])
         array_list = array.tolist()
 
         item1 = NumpyArrayItem.factory(array)
-        item2 = NumpyArrayItem(array_list)
+        item2 = NumpyArrayItem(
+            array_list=array_list,
+            created_at=mock_nowstr,
+            updated_at=mock_nowstr,
+        )
 
         numpy.testing.assert_array_equal(item1.array, array)
         numpy.testing.assert_array_equal(item2.array, array)
--- tests/unit/item/test_pandas_dataframe_item.py
+++ tests/unit/item/test_pandas_dataframe_item.py
@@ -5,22 +5,34 @@ from skore.item import PandasDataFrameItem
 
 
 class TestPandasDataFrameItem:
+    @pytest.fixture(autouse=True)
+    def monkeypatch_datetime(self, monkeypatch, MockDatetime):
+        monkeypatch.setattr("skore.item.pandas_dataframe_item.datetime", MockDatetime)
+
     @pytest.mark.order(0)
-    def test_factory(self):
+    def test_factory(self, mock_nowstr):
         dataframe = DataFrame([{"key": "value"}])
         dataframe_dict = dataframe.to_dict(orient="tight")
 
         item = PandasDataFrameItem.factory(dataframe)
 
-        assert vars(item) == {"dataframe_dict": dataframe_dict}
+        assert vars(item) == {
+            "dataframe_dict": dataframe_dict,
+            "created_at": mock_nowstr,
+            "updated_at": mock_nowstr,
+        }
 
     @pytest.mark.order(1)
-    def test_dataframe(self):
+    def test_dataframe(self, mock_nowstr):
         dataframe = DataFrame([{"key": "value"}])
         dataframe_dict = dataframe.to_dict(orient="tight")
 
         item1 = PandasDataFrameItem.factory(dataframe)
-        item2 = PandasDataFrameItem(dataframe_dict)
+        item2 = PandasDataFrameItem(
+            dataframe_dict=dataframe_dict,
+            created_at=mock_nowstr,
+            updated_at=mock_nowstr,
+        )
 
         assert_frame_equal(item1.dataframe, dataframe)
         assert_frame_equal(item2.dataframe, dataframe)
