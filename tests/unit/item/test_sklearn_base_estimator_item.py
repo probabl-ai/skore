e684f8e901aeeeba7e93aeb91d37c15074eacff2
Thomas S <thomas@probabl.ai>
2024-10-01 15:59:05 +0200

    fix: Temporarily disable security feature of skops used in `SklearnBaseEstimatorItem`

--- tests/unit/item/test_sklearn_base_estimator_item.py
+++ tests/unit/item/test_sklearn_base_estimator_item.py
@@ -11,19 +11,26 @@ class TestSklearnBaseEstimatorItem:
 
     @pytest.mark.order(0)
     def test_factory(self, monkeypatch, mock_nowstr):
-        monkeypatch.setattr("skops.io.dumps", lambda _: "<estimator_skops>")
-        monkeypatch.setattr(
-            "sklearn.utils.estimator_html_repr", lambda _: "<estimator_html_repr>"
-        )
-
         estimator = sklearn.svm.SVC()
-        estimator_skops = "<estimator_skops>"
         estimator_html_repr = "<estimator_html_repr>"
+        estimator_skops = "<estimator_skops>"
+        estimator_skops_untrusted_types = "<estimator_skops_untrusted_types>"
+
+        monkeypatch.setattr(
+            "sklearn.utils.estimator_html_repr",
+            lambda *args, **kwargs: estimator_html_repr,
+        )
+        monkeypatch.setattr("skops.io.dumps", lambda *args, **kwargs: estimator_skops)
+        monkeypatch.setattr(
+            "skops.io.get_untrusted_types",
+            lambda *args, **kwargs: estimator_skops_untrusted_types,
+        )
 
         item = SklearnBaseEstimatorItem.factory(estimator)
 
-        assert item.estimator_skops == estimator_skops
         assert item.estimator_html_repr == estimator_html_repr
+        assert item.estimator_skops == estimator_skops
+        assert item.estimator_skops_untrusted_types == estimator_skops_untrusted_types
         assert item.created_at == mock_nowstr
         assert item.updated_at == mock_nowstr
 
@@ -31,12 +38,15 @@ class TestSklearnBaseEstimatorItem:
     def test_estimator(self, mock_nowstr):
         estimator = sklearn.svm.SVC()
         estimator_skops = skops.io.dumps(estimator)
-        estimator_html_repr = "<estimator_html_repr>"
+        estimator_skops_untrusted_types = skops.io.get_untrusted_types(
+            data=estimator_skops
+        )
 
         item1 = SklearnBaseEstimatorItem.factory(estimator)
         item2 = SklearnBaseEstimatorItem(
+            estimator_html_repr=None,
             estimator_skops=estimator_skops,
-            estimator_html_repr=estimator_html_repr,
+            estimator_skops_untrusted_types=estimator_skops_untrusted_types,
             created_at=mock_nowstr,
             updated_at=mock_nowstr,
         )
