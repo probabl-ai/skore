c88518d3d136ff00d85fba1a16b8bab41904172a
Auguste Baum <auguste@probabl.ai>
2024-10-09 12:06:12 +0200

    squashme wip

--- skore/src/skore/item/cross_validate_item.py
+++ skore/src/skore/item/cross_validate_item.py
@@ -5,7 +5,23 @@ This class represents the output of a cross-validation workflow.
 
 from __future__ import annotations
 
+import hashlib
+from typing import TYPE_CHECKING, Any
+
 from skore.item.item import Item
+
+if TYPE_CHECKING:
+    import sklearn.base
+
+
+def _hash_numpy(array):
+    return hashlib.sha256(array.tobytes()).hexdigest()
+
+
+# Data used for training, passed as input to scikit-learn
+Data = Any
+# Target used for training, passed as input to scikit-learn
+Target = Any
 
 
 class CrossValidateItem(Item):
@@ -19,6 +35,9 @@ class CrossValidateItem(Item):
     def __init__(
         self,
         cv_results: dict,
+        estimator_info: dict,
+        X_info: dict,
+        y_info: dict,
         created_at: str | None = None,
         updated_at: str | None = None,
     ):
@@ -27,8 +46,14 @@ class CrossValidateItem(Item):
 
         Parameters
         ----------
-        cv_results: dict
+        cv_results : dict
             The dict output of scikit-learn's cross_validate function.
+        estimator_info : dict
+            The estimator that was cross-validated.
+        X_info : dict
+            Summary of the data, input of scikit-learn's cross_validation function.
+        y_info : dict
+            Summary of the target, input of scikit-learn's cross_validation function.
         created_at : str
             The creation timestamp in ISO format.
         updated_at : str
@@ -37,9 +62,18 @@ class CrossValidateItem(Item):
         super().__init__(created_at, updated_at)
 
         self.cv_results = cv_results
+        self.estimator_info = estimator_info
+        self.X_info = X_info
+        self.y_info = y_info
 
     @classmethod
-    def factory(cls, cv_results: dict) -> CrossValidateItem:
+    def factory(
+        cls,
+        cv_results: dict,
+        estimator: sklearn.base.BaseEstimator,
+        X: Data,
+        y: Target | None,
+    ) -> CrossValidateItem:
         """
         Create a new CrossValidateItem instance.
 
@@ -47,6 +81,12 @@ class CrossValidateItem(Item):
         ----------
         cv_results : dict
             The dict output of scikit-learn's cross_validate function.
+        estimator: sklearn.base.BaseEstimator,
+            The estimator that was cross-validated.
+        X
+            The data, input of scikit-learn's cross_validation function.
+        y
+            The target, input of scikit-learn's cross_validation function.
 
         Returns
         -------
@@ -56,6 +96,24 @@ class CrossValidateItem(Item):
         if not isinstance(cv_results, dict):
             raise TypeError(f"Type '{cv_results.__class__}' is not supported.")
 
-        instance = cls(cv_results=cv_results)
+        estimator_info = {
+            "name": estimator.__class__.__name__,
+            "params": estimator.get_params(),
+        }
+
+        y_info = None if y is None else {"hash": _hash_numpy(y)}
+
+        X_info = {
+            "nb_rows": X.shape[0],
+            "nb_cols": X.shape[1],
+            "hash": _hash_numpy(X),
+        }
+
+        instance = cls(
+            cv_results=cv_results,
+            estimator_info=estimator_info,
+            X_info=X_info,
+            y_info=y_info,
+        )
 
         return instance
