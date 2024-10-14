18d68d38a3f3c4ed9e50b6c75baaf6da6546e692
Auguste Baum <auguste@probabl.ai>
2024-10-14 10:50:17 +0200

    Add Marie's tests

--- skore/tests/integration/test_cross_validate.py
+++ skore/tests/integration/test_cross_validate.py
@@ -1,13 +1,18 @@
+import numpy
 import pytest
 import sklearn.model_selection
+from numpy import array
+from sklearn import datasets, linear_model
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.svm import LinearSVC
 from skore.cross_validate import cross_validate
 from skore.item.cross_validate_item import CrossValidationItem, plot_cross_validation
 
 
 @pytest.fixture()
 def lasso():
-    from sklearn import datasets, linear_model
-
     diabetes = datasets.load_diabetes()
     X = diabetes.data[:150]
     y = diabetes.target[:150]
@@ -15,7 +20,7 @@ def lasso():
     return lasso, X, y
 
 
-def test_cross_validate(in_memory_project, lasso):
+def test_cross_validate_regression(in_memory_project, lasso):
     args = lasso
     kwargs = {"cv": 3}
 
@@ -29,7 +34,7 @@ def test_cross_validate(in_memory_project, lasso):
     assert all(len(v) == kwargs["cv"] for v in cv_results.values())
 
 
-def test_cross_validate_extra_metrics(in_memory_project, lasso):
+def test_cross_validate_regression_extra_metrics(in_memory_project, lasso):
     args = list(lasso)
     kwargs = {"scoring": "r2", "cv": 3}
 
@@ -57,11 +62,7 @@ def test_cross_validate_2_extra_metrics(in_memory_project, lasso):
     assert all(len(v) == kwargs["cv"] for v in cv_results.values())
 
 
-def test_cross_validation_multi_class(in_memory_project):
-    from sklearn import datasets
-    from sklearn.multiclass import OneVsOneClassifier
-    from sklearn.svm import LinearSVC
-
+def test_cross_validation_multi_class_classification_sub_estimator(in_memory_project):
     X, y = datasets.load_iris(return_X_y=True)
     model = OneVsOneClassifier(LinearSVC())
 
@@ -78,9 +79,63 @@ def test_cross_validation_multi_class(in_memory_project):
     assert all(len(v) == kwargs["cv"] for v in cv_results.values())
 
 
-def test_plot_cross_validation():
-    from numpy import array
+def test_cross_validation_multi_class_classification(in_memory_project):
+    iris = datasets.load_iris()
+    X = iris.data[:250]
+    y = iris.target[:250]
+    rf = RandomForestClassifier()
 
+    args = [rf, X, y]
+    kwargs = {"cv": 3}
+
+    cv_results = cross_validate(*args, **kwargs, project=in_memory_project)
+    cv_results_sklearn = sklearn.model_selection.cross_validate(*args, **kwargs)
+
+    assert isinstance(
+        in_memory_project.get_item("cross_validation"), CrossValidationItem
+    )
+    assert cv_results.keys() == cv_results_sklearn.keys()
+    assert all(len(v) == kwargs["cv"] for v in cv_results.values())
+
+
+def test_cross_validation_binary_classification(in_memory_project):
+    iris = datasets.load_iris()
+    X = iris.data[:150]
+    y = numpy.random.randint(2, size=150)
+    rf = RandomForestClassifier()
+
+    args = [rf, X, y]
+    kwargs = {"cv": 3}
+
+    cv_results = cross_validate(*args, **kwargs, project=in_memory_project)
+    cv_results_sklearn = sklearn.model_selection.cross_validate(*args, **kwargs)
+
+    assert isinstance(
+        in_memory_project.get_item("cross_validation"), CrossValidationItem
+    )
+    assert cv_results.keys() == cv_results_sklearn.keys()
+    assert all(len(v) == kwargs["cv"] for v in cv_results.values())
+
+
+def test_cross_validation_clustering(in_memory_project):
+    iris = datasets.load_iris()
+    X = iris.data[:150]
+    kmeans = KMeans()
+
+    args = [kmeans, X]
+    kwargs = {"cv": 3}
+
+    cv_results = cross_validate(*args, **kwargs, project=in_memory_project)
+    cv_results_sklearn = sklearn.model_selection.cross_validate(*args, **kwargs)
+
+    assert isinstance(
+        in_memory_project.get_item("cross_validation"), CrossValidationItem
+    )
+    assert cv_results.keys() == cv_results_sklearn.keys()
+    assert all(len(v) == kwargs["cv"] for v in cv_results.values())
+
+
+def test_plot_cross_validation():
     cv_results = {
         "fit_time": array([0.00058246, 0.00041819, 0.00039363]),
         "score_time": array([0.00101399, 0.00072646, 0.00072432]),
