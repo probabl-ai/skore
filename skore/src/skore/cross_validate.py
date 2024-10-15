b087e63cdbd837ba174a1a2a517783e4fc681d0a
Auguste Baum <auguste@probabl.ai>
2024-10-15 14:17:15 +0200

    clean docstring

--- skore/src/skore/cross_validate.py
+++ skore/src/skore/cross_validate.py
@@ -161,15 +161,14 @@ def cross_validate(
     """Evaluate estimator by cross-validation and output UI-friendly object.
 
     This function wraps scikit-learn's [cross_validate](https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_validate.html#sklearn.model_selection.cross_validate)
-    function, to provide more context
-    and facilitate the analysis.
+    function, to provide more context and facilitate the analysis.
     As such, the arguments are the same as scikit-learn's cross_validate function.
 
     Parameters
     ----------
     The same parameters as scikit-learn's cross_validate function, except for
 
-    project : Project or None
+    project : Project, optional
         A project to save cross-validation data into. If None, no save is performed.
 
     Returns
