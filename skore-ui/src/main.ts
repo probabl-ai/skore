6140f8d26407a0f30ff9e8c6b4db13a6c64ab134
Matthieu Jouis <matthieu@probabl.ai>
2024-10-17 15:28:33 +0200

    feat(UI): add a tooltip component

--- skore-ui/src/main.ts
+++ skore-ui/src/main.ts
@@ -1,10 +1,10 @@
-import "./assets/styles/main.css";
-
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
-import App from "./App.vue";
-import router from "./router";
+import "@/assets/styles/main.css";
+
+import App from "@/App.vue";
+import router from "@/router";
 
 const app = createApp(App);
 
