a2b4b47893f012c9db7ddd700be097095799c0ec
Auguste Baum <auguste@probabl.ai>
2024-10-22 16:23:47 +0200

    Simplify docstring

--- skore/src/skore/project.py
+++ skore/src/skore/project.py
@@ -68,7 +68,7 @@ class Project:
         key : str | dict[str, Any]
             The key to associate with `value` in the Project,
             or dict of key-value pairs to add to the Project.
-        value : Any | None, default: None
+        value : Any, optional
             The value to associate with `key` in the Project.
             If `key` is a dict, this argument is ignored.
 
