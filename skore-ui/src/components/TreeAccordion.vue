45a0eebc313813fef5e13f704194bbde4c6755ad
Auguste Baum <52001167+augustebaum@users.noreply.github.com>
2024-10-23 17:10:11 +0200

    fix(ui): Correct wrong background color in TreeAccordion (#597)
    This fixes a problem introduced in
    https://github.com/probabl-ai/skore/commit/2393378debfc3903cca300e954288a201c2d5900#diff-a53f31132b666510175172f97266d64794afa1a64ad7e2b89b59642b858d7ad7R55
    
    Closes #587

--- skore-ui/src/components/TreeAccordion.vue
+++ skore-ui/src/components/TreeAccordion.vue
@@ -52,7 +52,7 @@ watch([lastItemAction, lastItemName], () => {
 .accordion {
   display: flex;
   flex-direction: column;
-  background-color: var(--background-color-elevated);
+  background-color: var(--background-color-normal);
   gap: var(--spacing-gap-large);
 }
 </style>
