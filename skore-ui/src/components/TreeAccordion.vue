probabl-ai/skore

commit 2393378debfc3903cca300e954288a201c2d5900

feat(UI): Use corrects background colors, use orange for views and blue for items (#569)

skore-ui/src/components/TreeAccordion.vue
@@ -52,7 +52,7 @@ watch([lastItemAction, lastItemName], () => {
 .accordion {
   display: flex;
   flex-direction: column;
-  background-color: var(--background-color-elevated-high);
+  background-color: var(--background-color-elevated);
   gap: var(--spacing-gap-large);
 }
 </style>

skore-ui/src/assets/styles/_variables.css
@@ -39,12 +39,16 @@
   /* misc */
   --transition-duration: 0.3s;
   --transition-easing: ease;
+
+  /* brand colors */
+  --color-blue: #2329ff;
+  --color-orange: #f08b30;
 }
 
 @media (prefers-color-scheme: dark) {
   :root {
-    --color-primary: #f08b30;
-    --color-secondary: #2329ff;
+    --color-primary: var(--color-orange);
+    --color-secondary: var(--color-blue);
 
     /* typography */
     --text-color-low: rgb(99 99 99);
@@ -54,8 +58,6 @@
     /* backgrounds */
     --background-color-normal: #121212;
     --background-color-elevated: #222;
-    --background-color-elevated-high: #2f2f2f;
-    --background-color-selected: rgb(191 191 191 / 25%);
 
     /* buttons */
     --button-color: #fff;
@@ -65,6 +67,9 @@
     --border-color-lower: #171717;
     --border-color-normal: #292a2a;
     --border-color-elevated: #3f3f3f;
+
+    /* shadows */
+    --shadow-color: rgb(0 0 0 / 58%);
 
     /* toast */
     --toast-background-color: hsl(240deg 5% 91%);
@@ -79,8 +84,8 @@
 
 @media (prefers-color-scheme: light) {
   :root {
-    --color-primary: #2329ff;
-    --color-secondary: #f08b30;
+    --color-primary: var(--color-blue);
+    --color-secondary: var(--color-orange);
 
     /* typography */
     --text-color-low: #babbbd;
@@ -90,8 +95,6 @@
     /* backgrounds */
     --background-color-normal: #fafafb;
     --background-color-elevated: #f3f3f6;
-    --background-color-elevated-high: #fff;
-    --background-color-selected: rgb(101 106 255 / 8%);
 
     /* buttons */
     --button-color: #fff;
@@ -101,6 +104,9 @@
     --border-color-lower: #f0f0f0;
     --border-color-normal: #eeeef0;
     --border-color-elevated: #dadada;
+
+    /* shadows */
+    --shadow-color: rgb(0 0 0 /19%);
 
     /* toast */
     --toast-background-color: #151517;

skore-ui/src/components/DataFrameWidget.vue
@@ -271,8 +271,8 @@ watch([() => toValue(props.data), () => toValue(props.columns)], () => {
       border: 1px solid var(--border-color-lower);
       border-radius: var(--border-radius);
       margin-left: var(--spacing-gap-normal);
-      background-color: var(--background-color-elevated-high);
-      box-shadow: 0 1px 2px var(--background-color-selected);
+      background-color: var(--background-color-elevated);
+      box-shadow: 0 1px 2px var(--shadow-color);
       color: var(--text-color-highlight);
       cursor: pointer;
       font-size: var(--text-size-highlight);

skore-ui/src/components/DropdownButton.vue
@@ -110,7 +110,7 @@ onBeforeUnmount(() => {
     border: solid 1px var(--border-color-normal);
     border-radius: var(--border-radius);
     background-color: var(--background-color-normal);
-    box-shadow: 4px 10px 20px var(--background-color-selected);
+    box-shadow: 4px 10px 20px var(--shadow-color);
   }
 }
 </style>

skore-ui/src/components/EditableListItem.vue
@@ -101,7 +101,7 @@ onMounted(() => {
   transition: background-color var(--transition-duration) var(--transition-easing);
 
   .icon {
-    color: var(--color-secondary);
+    color: var(--color-orange);
   }
 
   .label {

skore-ui/src/components/ModalDialog.vue
@@ -102,7 +102,7 @@ onUnmounted(() => {
         calc(var(--spacing-padding-normal) * 2) var(--spacing-padding-normal);
       border: solid 1px var(--border-color-normal);
       border-radius: var(--modal-border-radius);
-      background-color: var(--background-color-elevated-high);
+      background-color: var(--background-color-elevated);
       gap: 24px;
 
       & h2 {
@@ -126,7 +126,7 @@ onUnmounted(() => {
         gap: var(--spacing-gap-normal);
 
         & .text-input {
-          background-color: var(--background-color-elevated-high);
+          background-color: var(--background-color-elevated);
         }
       }
     }
@@ -145,7 +145,7 @@ onUnmounted(() => {
       gap: var(--spacing-gap-normal);
 
       & button.regular {
-        background-color: var(--background-color-elevated-high);
+        background-color: var(--background-color-elevated);
       }
 
       &.alert button {

skore-ui/src/components/TextInput.vue
@@ -49,7 +49,7 @@ onMounted(() => {
   border: 1px solid var(--border-color-normal);
   border-radius: var(--border-radius);
   background-color: var(--background-color-normal);
-  box-shadow: 0 1px 2px var(--background-color-selected);
+  box-shadow: 0 1px 2px var(--shadow-color);
   color: var(--text-color-normal);
   transition: all var(--transition-duration) var(--transition-easing);
 

skore-ui/src/components/TreeAccordionItem.vue
@@ -124,7 +124,7 @@ function onAction(action: string) {
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    background-color: var(--background-color-elevated-high);
+    background-color: var(--background-color-normal);
 
     .label {
       display: flex;
@@ -134,7 +134,7 @@ function onAction(action: string) {
       transition: background-color var(--transition-duration) var(--transition-easing);
 
       &:not(.has-children):hover {
-        background-color: var(--background-color-selected);
+        background-color: var(--background-color-elevated);
         color: var(--text-color-highlight);
       }
 
@@ -148,7 +148,7 @@ function onAction(action: string) {
 
       & .icon {
         margin-right: var(--spacing-gap-small);
-        color: var(--color-primary);
+        color: var(--color-blue);
       }
 
       & .text {

commit 0631713da5dacff9eec0cbf7eae6120951e4c9d8

fix(ui): Correct wrong background color in TreeAccordion
This fixes a problem introduced in https://github.com/probabl-ai/skore/commit/2393378debfc3903cca300e954288a201c2d5900#diff-a53f31132b666510175172f97266d64794afa1a64ad7e2b89b59642b858d7ad7R55

Closes #587

skore-ui/src/components/TreeAccordion.vue
@@ -52,7 +52,7 @@ watch([lastItemAction, lastItemName], () => {
 .accordion {
   display: flex;
   flex-direction: column;
-  background-color: var(--background-color-elevated);
+  background-color: var(--background-color-normal);
   gap: var(--spacing-gap-large);
 }
 </style>