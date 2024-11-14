ea974f411d1178e607e8ffd9edbcf776c9e96f3c
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-14 11:27:01 +0100

    docs: Move button for notebooks on the side bar (#732)
    In sphinx-gallery, the default button for notebook are available in the
    bottom of the example.
    
    With the pydata-sphinx-theme, this is possible to move them in the
    secondary bar such that they are always available. It is more ergonomic.
    Here, I did the trick that we have also implemented in scikit-learn and
    skrub:
    
    <img width="274" alt="image"
    src="https://github.com/user-attachments/assets/067d2915-ecbd-40fe-9eb9-378a5916c7cc">
    
    Co-authored-by: Matt J. <matthieu@probabl.ai>

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -72,6 +72,13 @@ html_theme_options = {
     #     "image_light": "_static/skrub.svg",
     #     "image_dark": "_static/skrub.svg",
     # },
+    # When specified as a dictionary, the keys should follow glob-style patterns, as in
+    # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-exclude_patterns
+    # In particular, "**" specifies the default for all pages
+    # Use :html_theme.sidebar_secondary.remove: for file-wide removal
+    "secondary_sidebar_items": {
+        "**": ["page-toc", "sourcelink", "sg_download_links", "sg_launcher_links"]
+    },
     "external_links": [
         {
             "url": "https://probabl.ai",
