400978cad5cbe1cbc857ca0069b1104c304107ab
Thomas S <thomas@probabl.ai>
2024-11-18 17:42:21 +0100

    WIP - Add versions switcher in navbar

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -9,8 +9,15 @@
 project = "skore"
 copyright = "2024, Probabl"
 author = "Probabl"
-# version = "0"
-# release = "0"
+version = "0.3"  # dynamic
+release = "0.3.0"  # dynamic
+
+# 0.1/
+# 0.2/
+# 0.3/
+# dev/
+# index.html
+# versions.json
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -50,10 +57,10 @@ sphinx_gallery_conf = {
     "within_subsection_order": "FileNameSortKey",  # See https://sphinx-gallery.github.io/stable/configuration.html#sorting-gallery-examples for alternatives
     "show_memory": False,
     "write_computation_times": False,
-    'reference_url': {
+    "reference_url": {
         # The module you locally document uses None
-        'skore': None,
-        }
+        "skore": None,
+    },
 }
 
 # intersphinx configuration
@@ -65,15 +72,12 @@ numpydoc_show_class_members = False
 
 html_title = "skore"
 
+# html_additional_pages = {"index": "index.html"}
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    # "logo": {
-    #     "image_relative": "_static/skrub.svg",
-    #     "image_light": "_static/skrub.svg",
-    #     "image_dark": "_static/skrub.svg",
-    # },
     # When specified as a dictionary, the keys should follow glob-style patterns, as in
     # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-exclude_patterns
     # In particular, "**" specifies the default for all pages
@@ -106,6 +110,11 @@ html_theme_options = {
         },
     ],
     "announcement": "This code is still in development. <strong>The API is subject to change.</strong>",
+    "switcher": {"json_url": "_static/switcher.json", "version_match": version},
+    "check_switcher": True,
+    "navbar_start": ["navbar-logo", "version-switcher"],
+    "navbar_center": ["navbar-nav"],
+    "navbar_end": ["theme-switcher"],
 }
 
 # Plausible Analytics
@@ -118,10 +127,10 @@ html_theme_options["analytics"] = {
 
 # Sphinx remove the sidebar from some pages
 html_sidebars = {
-  "install": [],
-  "getting_started": [],
-  "user_guide": [],
-  "contributor_guide": [],
+    "install": [],
+    "getting_started": [],
+    "user_guide": [],
+    "contributor_guide": [],
 }
 
 # Sphinx-Copybutton configuration
