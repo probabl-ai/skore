d630048a091e6e2e704a98339e7c4f32cf3aa657
Auguste Baum <auguste@probabl.ai>
2024-11-18 15:24:33 +0100

    add test with no y

--- skore/tests/unit/sklearn/test_train_test_split.py
+++ skore/tests/unit/sklearn/test_train_test_split.py
@@ -34,6 +34,13 @@ def test_train_test_split_warns():
         train_test_split([[1]] * 4, [0, 1, 1, 1])
 
 
+def test_train_test_split_no_y():
+    """When calling `train_test_split` with one array argument,
+    this array is assumed to be `X` and not `y`."""
+    with contextlib.nullcontext():
+        train_test_split([[1]] * 4)
+
+
 def test_train_test_split_no_warn():
     with contextlib.nullcontext():
         train_test_split([[1]] * 4, [0, 0, 1, 1])
