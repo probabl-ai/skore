e1da9888e814f897d0c2b0d601ac4e326ec5bf85
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-18 16:02:36 +0100

    review

--- skore/src/skore/__main__.py
+++ skore/src/skore/__main__.py
@@ -4,10 +4,16 @@ import sys
 
 from skore.cli.cli import cli
 
-if __name__ == "__main__":
+
+def main():
+    """Entry point to Skore CLI."""
     import rich.traceback
 
     # Display error tracebacks with Rich
     rich.traceback.install(show_locals=True)
 
-    sys.exit(cli())
+    sys.exit(cli(sys.argv[1:]))
+
+
+if __name__ == "__main__":
+    main()
