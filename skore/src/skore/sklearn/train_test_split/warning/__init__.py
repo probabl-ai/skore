498663565d31aca5efe9e84aecb191b32fe6bc64
Auguste Baum <auguste@probabl.ai>
2024-11-19 11:51:27 +0100

    Add new warning

--- skore/src/skore/sklearn/train_test_split/warning/__init__.py
+++ skore/src/skore/sklearn/train_test_split/warning/__init__.py
@@ -4,10 +4,14 @@ This module implements the warnings that can be reported to the user when using
 `train_test_split`.
 """
 
+from .high_class_imbalance_too_few_examples_warning import (
+    HighClassImbalanceTooFewExamplesWarning,
+)
 from .high_class_imbalance_warning import (
     HighClassImbalanceWarning,
 )
 
 __all__ = [
+    HighClassImbalanceTooFewExamplesWarning.__name__,
     HighClassImbalanceWarning.__name__,
 ]
--- skore/tests/unit/sklearn/train_test_split/test_train_test_split.py
+++ skore/tests/unit/sklearn/train_test_split/test_train_test_split.py
@@ -10,6 +10,18 @@ from skore.sklearn.train_test_split.warning import (
 )
 
 
+def test_check_high_class_imbalance_too_few_examples():
+    y_test = [0] * 100
+    y_labels = [0, 1]
+    check_passed = HighClassImbalanceTooFewExamplesWarning.check(
+        y_test=y_test,
+        y_labels=y_labels,
+        stratify=None,
+        ml_task="multiclass-classification",
+    )
+
+    assert not check_passed
+
 
 def test_train_test_split_warns():
     with pytest.warns(HighClassImbalanceWarning, match=HighClassImbalanceWarning.MSG):
