dee08a8cada8c57741bd46afcbad4fe572cd4d8a
Auguste Baum <52001167+augustebaum@users.noreply.github.com>
2024-11-20 16:26:08 +0100

    feat(train_test_split): Add "stratify" warning (#780)
    Addresses #684

---
+++ skore/src/skore/sklearn/train_test_split/warning/stratify_is_set.py
@@ -0,0 +1,42 @@
+"""Stratify warning.
+
+This warning is shown when `stratify` is set.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Optional
+
+from skore.sklearn.train_test_split.warning.train_test_split_warning import (
+    TrainTestSplitWarning,
+)
+
+if TYPE_CHECKING:
+    ArrayLike = Any
+
+
+class StratifyWarning(TrainTestSplitWarning):
+    """Check whether `stratify` is set."""
+
+    MSG = (
+        "We recommend against using `stratify` if you have a problem where your events "
+        "occur over time (even if they are independent). Using `stratify` will result "
+        "in inflated model performance evaluation because the natural drift of the "
+        "target will not be taken into account."
+    )
+
+    @staticmethod
+    def check(stratify: Optional[ArrayLike], **kwargs) -> bool:
+        """Check whether stratify is set.
+
+        Parameters
+        ----------
+        stratify : array-like or None
+            An 1-dimensional target array to be used for stratification.
+
+        Returns
+        -------
+        bool
+            True if the check passed, False otherwise.
+        """
+        return not stratify
