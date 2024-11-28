14e8bf68280a289ef401834765c8d0e7f9b9a58a
Matt J <matthieu@probabl.ai>
2024-11-28 15:09:46 +0100

    App toolbar scaffolding

--- skore-ui/src/App.vue
+++ skore-ui/src/App.vue
@@ -1,36 +1,45 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 
+import AppToolbar from "@/components/AppToolbar.vue";
 import LoadingBars from "@/components/LoadingBars.vue";
 import ModalDialog from "@/components/ModalDialog.vue";
 import ToastNotificationArea from "@/components/ToastNotificationArea.vue";
 </script>
 
 <template>
-  <RouterView v-slot="{ Component }">
-    <template v-if="Component">
-      <Transition name="fade">
-        <Suspense>
-          <component :is="Component" />
-          <template #fallback>
-            <div class="loader">
-              <LoadingBars />
-            </div>
-          </template>
-        </Suspense>
-      </Transition>
-    </template>
-  </RouterView>
+  <div class="skore">
+    <AppToolbar />
+    <RouterView v-slot="{ Component }">
+      <template v-if="Component">
+        <Transition name="fade">
+          <Suspense>
+            <component :is="Component" />
+            <template #fallback>
+              <div class="loader">
+                <LoadingBars />
+              </div>
+            </template>
+          </Suspense>
+        </Transition>
+      </template>
+    </RouterView>
+  </div>
   <ToastNotificationArea />
   <ModalDialog />
 </template>
 
 <style scoped>
-.loader {
+.skore {
   display: flex;
-  width: 100dvw;
-  height: 100dvh;
-  align-items: center;
-  justify-content: center;
+  flex-direction: row;
+
+  .loader {
+    display: flex;
+    width: 100dvw;
+    height: 100dvh;
+    align-items: center;
+    justify-content: center;
+  }
 }
 </style>
