20cac5f997e0b65b05b624c119a7e6d14697ae3b
Auguste Baum <auguste@probabl.ai>
2024-12-02 11:59:47 +0100

    add to top-level imports

--- skore/src/skore/sklearn/__init__.py
+++ skore/src/skore/sklearn/__init__.py
@@ -1,9 +1,11 @@
 """Enhance `sklearn` functions."""
 
 from skore.sklearn.cross_validate import cross_validate
+from skore.sklearn.cross_validation_reporter import CrossValidationReporter
 from skore.sklearn.train_test_split.train_test_split import train_test_split
 
 __all__ = [
     "cross_validate",
     "train_test_split",
+    "CrossValidationReporter",
 ]
