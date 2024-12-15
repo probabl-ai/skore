76c7375dc58371d7ecb4847cbe7ac2b547a11cba
Guillaume Lemaitre <guillaume@probabl.ai>
2024-12-15 21:48:23 +0100

    fix: Do not show right-hand sidebar for contributing page

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -139,7 +139,7 @@ html_sidebars = {
     "install": [],
     "getting_started": [],
     "user_guide": [],
-    "contributor_guide": [],
+    "contributor_link": [],
 }
 
 # Sphinx-Copybutton configuration
