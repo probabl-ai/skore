c30ce1a4f2383aad6ebe7edabd2608cdaa387462
Auguste Baum <auguste@probabl.ai>
2024-12-17 14:50:13 +0100

    fix: Correct serialization of CrossValidationItem for UI
    Fixes a bug introduced in https://github.com/probabl-ai/skore/pull/964/files where the plot was outputted to a non-serializable object

--- skore/src/skore/ui/project_routes.py
+++ skore/src/skore/ui/project_routes.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import base64
 import copy
 import importlib
+import json
 import operator
 import re
 import statistics
@@ -136,7 +137,7 @@ def __cross_validation_item_as_serializable(item: CrossValidationItem) -> dict:
         "plots": [
             {
                 "name": "cross-validation results",
-                "value": item.plot,
+                "value": json.loads(item.plot_bytes.decode("utf-8")),
             }
         ],
         "sections": [

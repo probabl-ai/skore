probabl-ai/skore

commit 32e6c83cae74a745e9c096483efd4f525d7b02ce

test(cross-validation UI): Add integration test (#964)
As requested in
https://github.com/probabl-ai/skore/pull/871#discussion_r1880487305

skore/src/skore/ui/project_routes.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 import base64
 import copy
 import importlib
-import json
 import operator
 import re
 import statistics
@@ -137,7 +136,7 @@ def __cross_validation_item_as_serializable(item: CrossValidationItem) -> dict:
         "plots": [
             {
                 "name": "cross-validation results",
-                "value": json.loads(item.plot_bytes.decode("utf-8")),
+                "value": item.plot,
             }
         ],
         "sections": [

skore/tests/integration/ui/test_ui.py
@@ -7,6 +7,8 @@ import pytest
 from fastapi.testclient import TestClient
 from PIL import Image
 from sklearn.linear_model import Lasso
+from sklearn.model_selection import KFold
+from skore import CrossValidationReporter
 from skore.item.media_item import MediaItem
 from skore.ui.app import create_app
 from skore.view.view import View
@@ -144,6 +146,121 @@ def test_serialize_media_item(client, in_memory_project):
     assert "image" in project["items"]["img"][0]["media_type"]
     assert project["items"]["html"][0]["value"] == html
     assert project["items"]["media html"][0]["value"] == html
+
+
+@pytest.fixture
+def fake_cross_validate(monkeypatch):
+    def _fake_cross_validate(*args, **kwargs):
+        result = {
+            "test_score": numpy.array([1.0] * 5),
+            "score_time": numpy.array([1.0] * 5),
+            "fit_time": numpy.array([1.0] * 5),
+        }
+        if kwargs.get("return_estimator"):
+            result["estimator"] = numpy.array([])
+        if kwargs.get("return_indices"):
+            result["indices"] = {
+                "train": numpy.array([[1.0] * 5] * 5),
+                "test": numpy.array([[1.0] * 5] * 5),
+            }
+        if kwargs.get("return_train_score"):
+            result["train_score"] = numpy.array([1.0] * 5)
+        return result
+
+    monkeypatch.setattr("sklearn.model_selection.cross_validate", _fake_cross_validate)
+
+
+def test_serialize_cross_validation_item(
+    client,
+    in_memory_project,
+    monkeypatch,
+    MockDatetime,
+    mock_nowstr,
+    fake_cross_validate,
+):
+    monkeypatch.setattr("skore.item.item.datetime", MockDatetime)
+    monkeypatch.setattr("skore.ui.project_routes.CrossValidationItem.plot", {})
+
+    def prepare_cv():
+        from sklearn import datasets, linear_model
+
+        diabetes = datasets.load_diabetes()
+        X = diabetes.data[:100]
+        y = diabetes.target[:100]
+        lasso = linear_model.Lasso(alpha=2.5)
+        return lasso, X, y
+
+    model, X, y = prepare_cv()
+    reporter = CrossValidationReporter(model, X, y, cv=KFold(3))
+    in_memory_project.put("cv", reporter)
+
+    response = client.get("/api/project/items")
+    assert response.status_code == 200
+
+    project = response.json()
+    expected = {
+        "name": "cv",
+        "media_type": "application/vnd.skore.cross_validation+json",
+        "value": {
+            "scalar_results": [
+                {"name": "Mean test score", "value": 1.0, "stddev": 0.0},
+                {"name": "Mean score time (seconds)", "value": 1.0, "stddev": 0.0},
+                {"name": "Mean fit time (seconds)", "value": 1.0, "stddev": 0.0},
+            ],
+            "tabular_results": [
+                {
+                    "name": "Cross validation results",
+                    "columns": ["test_score", "score_time", "fit_time"],
+                    "data": [
+                        [1.0, 1.0, 1.0],
+                        [1.0, 1.0, 1.0],
+                        [1.0, 1.0, 1.0],
+                        [1.0, 1.0, 1.0],
+                        [1.0, 1.0, 1.0],
+                    ],
+                }
+            ],
+            "plots": [{"name": "cross-validation results", "value": {}}],
+            "sections": [
+                {
+                    "title": "Model",
+                    "icon": "icon-square-cursor",
+                    "items": [
+                        {
+                            "name": "Estimator parameters",
+                            "description": "Core model configuration used for training",
+                            "value": (
+                                '<a href="https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Lasso.html" target="_blank"><code>Lasso</code></a>\n'  # noqa: E501
+                                "- alpha: *2.5*\n"
+                                "- copy_X: True (default)\n"
+                                "- fit_intercept: True (default)\n"
+                                "- max_iter: 1000 (default)\n"
+                                "- positive: False (default)\n"
+                                "- precompute: False (default)\n"
+                                "- random_state: None (default)\n"
+                                "- selection: 'cyclic' (default)\n"
+                                "- tol: 0.0001 (default)\n"
+                                "- warm_start: False (default)"
+                            ),
+                        },
+                        {
+                            "name": "Cross-validation parameters",
+                            "description": "Controls how data is split and validated",
+                            "value": (
+                                "n_splits: *3*, "
+                                "shuffle: *False*, "
+                                "random_state: *None*"
+                            ),
+                        },
+                    ],
+                }
+            ],
+        },
+        "updated_at": mock_nowstr,
+        "created_at": mock_nowstr,
+    }
+    actual = project["items"]["cv"][0]
+    assert expected == actual
 
 
 def test_activity_feed(monkeypatch, client, in_memory_project):

commit c30ce1a4f2383aad6ebe7edabd2608cdaa387462

fix: Correct serialization of CrossValidationItem for UI
Fixes a bug introduced in https://github.com/probabl-ai/skore/pull/964/files where the plot was outputted to a non-serializable object

skore/src/skore/ui/project_routes.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import base64
 import copy
 import importlib
+import json
 import operator
 import re
 import statistics
@@ -136,7 +137,7 @@ def __cross_validation_item_as_serializable(item: CrossValidationItem) -> dict:
         "plots": [
             {
                 "name": "cross-validation results",
-                "value": item.plot,
+                "value": json.loads(item.plot_bytes.decode("utf-8")),
             }
         ],
         "sections": [

skore/tests/integration/ui/test_ui.py
@@ -179,7 +179,6 @@ def test_serialize_cross_validation_item(
     fake_cross_validate,
 ):
     monkeypatch.setattr("skore.item.item.datetime", MockDatetime)
-    monkeypatch.setattr("skore.ui.project_routes.CrossValidationItem.plot", {})
 
     def prepare_cv():
         from sklearn import datasets, linear_model
@@ -194,12 +193,17 @@ def test_serialize_cross_validation_item(
     reporter = CrossValidationReporter(model, X, y, cv=KFold(3))
     in_memory_project.put("cv", reporter)
 
+    # Mock the item to make the plot empty
+    item = in_memory_project.get_item("cv")
+    item.plot_bytes = b"{}"
+    in_memory_project.put_item("cv_mocked", item)
+
     response = client.get("/api/project/items")
     assert response.status_code == 200
 
     project = response.json()
     expected = {
-        "name": "cv",
+        "name": "cv_mocked",
         "media_type": "application/vnd.skore.cross_validation+json",
         "value": {
             "scalar_results": [
@@ -259,7 +263,7 @@ def test_serialize_cross_validation_item(
         "updated_at": mock_nowstr,
         "created_at": mock_nowstr,
     }
-    actual = project["items"]["cv"][0]
+    actual = project["items"]["cv_mocked"][0]
     assert expected == actual
 
 