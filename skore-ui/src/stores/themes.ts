abbf12e6afaf1757a4dfc77154f12dad892de1f9
Matt J <matthieu@probabl.ai>
2024-12-18 12:31:12 +0100

    persist theme in local storage

--- skore-ui/src/stores/themes.ts
+++ skore-ui/src/stores/themes.ts
@@ -5,8 +5,12 @@ import { isUserInDarkMode } from "@/services/utils";
 
 export type Theme = "light" | "dark";
 
+const LOCAL_STORAGE_KEY = "skore-theme";
+
 export const useThemesStore = defineStore("themes", () => {
-  const currentTheme = ref<Theme>(isUserInDarkMode() ? "dark" : "light");
+  const preferredTheme = isUserInDarkMode() ? "dark" : "light";
+  const storedTheme = localStorage.getItem(LOCAL_STORAGE_KEY);
+  const currentTheme = ref<Theme>((storedTheme ?? preferredTheme) as Theme);
   const currentThemeClass = computed(() => `skore-${currentTheme.value}`);
 
   /**
@@ -15,6 +19,7 @@ export const useThemesStore = defineStore("themes", () => {
    */
   function setTheme(t: Theme) {
     currentTheme.value = t;
+    localStorage.setItem(LOCAL_STORAGE_KEY, t);
   }
 
   // listen to browser preferences changes
