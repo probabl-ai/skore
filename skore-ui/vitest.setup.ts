512d3f34008f731909cd1a2e427f28181947dba5
Matt J <matthieu@probabl.ai>
2024-12-18 19:38:41 +0100

    fix tests

--- skore-ui/vitest.setup.ts
+++ skore-ui/vitest.setup.ts
@@ -11,4 +11,19 @@ vi.hoisted(() => {
   const mockObjectURL = vi.fn();
   window.URL.createObjectURL = mockObjectURL;
   window.URL.revokeObjectURL = mockObjectURL;
+
+  // mock window.matchMedia
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
 });
