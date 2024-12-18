aeaab369899545f99640c8c2b09333f6fdc9f9ec
Matt J <matthieu@probabl.ai>
2024-12-18 18:45:55 +0100

    switch dark/ligh in vscode aswell

--- skore-ui/src/StandaloneWidget.vue
+++ skore-ui/src/StandaloneWidget.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
-import MediaWidgetSelector from "./components/MediaWidgetSelector.vue";
-import { deserializeProjectItemDto } from "./models";
+import { onBeforeUnmount, onMounted, ref } from "vue";
+
+import MediaWidgetSelector from "@/components/MediaWidgetSelector.vue";
+import { deserializeProjectItemDto } from "@/models";
+import { isUserInDarkMode } from "@/services/utils";
 
 const dataId = `skore-item-data-${window.skoreWidgetId}`;
 const itemData = JSON.parse(document.getElementById(dataId)?.innerText || "{}");
 const item = deserializeProjectItemDto(itemData);
-console.info(itemData);
+const theme = ref(isUserInDarkMode() ? "skore-dark" : "skore-light");
+
+function switchTheme(m: MediaQueryListEvent) {
+  theme.value = m.matches ? "skore-dark" : "skore-light";
+}
+
+const preferredColorScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+onMounted(() => {
+  preferredColorScheme.addEventListener("change", switchTheme);
+});
+
+onBeforeUnmount(() => {
+  preferredColorScheme.removeEventListener("change", switchTheme);
+});
 </script>
 
 <template>
-  <div class="widget">
+  <div class="widget" :class="theme">
     <MediaWidgetSelector :item="item" />
   </div>
 </template>
