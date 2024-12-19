1beae940845ae3b8cd08e2fabd1e0b60c7e6b9fb
Auguste Baum <auguste@probabl.ai>
2024-12-19 09:53:28 +0100

    feat: Add `open` command

--- skore/src/skore/project/__init__.py
+++ skore/src/skore/project/__init__.py
@@ -2,10 +2,12 @@
 
 from .create import create
 from .load import load
+from .open import open
 from .project import Project
 
 __all__ = [
     "create",
     "load",
+    "open",
     "Project",
 ]
