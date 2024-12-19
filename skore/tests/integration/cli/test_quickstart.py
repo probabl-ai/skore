d980600e5cd40747c5f0ff45a5fbee8be15714a8
Auguste Baum <auguste@probabl.ai>
2024-12-19 10:57:02 +0100

    test(cli): Add integration test for `quickstart` command

---
+++ skore/tests/integration/cli/test_quickstart.py
@@ -0,0 +1,18 @@
+from skore.cli.cli import cli
+
+
+def test_quickstart(tmp_path, monkeypatch):
+    def fake_launch(project_name, port, open_browser):
+        pass
+
+    monkeypatch.setattr("skore.cli.quickstart_command.__launch", fake_launch)
+
+    cli(f"quickstart --working-dir {tmp_path}".split())
+    assert (tmp_path / "project.skore").exists()
+
+    # calling the same command without overwriting should succeed
+    # (as the creation step is skipped)
+    cli(f"quickstart --working-dir {tmp_path}".split())
+
+    # calling the same command with overwriting should succeed
+    cli(f"quickstart --working-dir {tmp_path} --overwrite".split())
