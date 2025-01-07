8936f18cd6f81817a463d00804db5d9bc6058870
Auguste Baum <auguste@probabl.ai>
2025-01-07 16:38:13 +0100

    refactor(tests): Move `os.chdir` out of fixture

--- skore/tests/integration/cli/test_create.py
+++ skore/tests/integration/cli/test_create.py
@@ -5,26 +5,22 @@ from skore.cli.cli import cli
 from skore.exceptions import ProjectCreationError
 
 
-@pytest.fixture
-def fake_working_dir(tmp_path):
-    """Change directory to a temporary one, and return its path."""
+def test_create_project_cli_default_argument(tmp_path):
     os.chdir(tmp_path)
-    return tmp_path
-
-
-def test_create_project_cli_default_argument(fake_working_dir):
     cli("create".split())
-    assert (fake_working_dir / "project.skore").exists()
+    assert (tmp_path / "project.skore").exists()
 
 
-def test_create_project_cli_absolute_path(fake_working_dir):
-    cli(f"create {fake_working_dir / 'hello.skore'}".split())
-    assert (fake_working_dir / "hello.skore").exists()
+def test_create_project_cli_absolute_path(tmp_path):
+    os.chdir(tmp_path)
+    cli(f"create {tmp_path / 'hello.skore'}".split())
+    assert (tmp_path / "hello.skore").exists()
 
 
-def test_create_project_cli_ends_in_skore(fake_working_dir):
+def test_create_project_cli_ends_in_skore(tmp_path):
+    os.chdir(tmp_path)
     cli("create hello.skore".split())
-    assert (fake_working_dir / "hello.skore").exists()
+    assert (tmp_path / "hello.skore").exists()
 
 
 def test_create_project_cli_invalid_name():
@@ -32,10 +28,11 @@ def test_create_project_cli_invalid_name():
         cli("create hello.txt".split())
 
 
-def test_create_project_cli_overwrite(fake_working_dir):
+def test_create_project_cli_overwrite(tmp_path):
     """Check the behaviour of the `overwrite` flag/parameter."""
+    os.chdir(tmp_path)
     cli("create".split())
-    assert (fake_working_dir / "project.skore").exists()
+    assert (tmp_path / "project.skore").exists()
 
     # calling the same command without overwriting should fail
     with pytest.raises(FileExistsError):
@@ -43,4 +40,4 @@ def test_create_project_cli_overwrite(fake_working_dir):
 
     # calling the same command with overwriting should succeed
     cli("create --overwrite".split())
-    assert (fake_working_dir / "project.skore").exists()
+    assert (tmp_path / "project.skore").exists()
--- skore/tests/integration/cli/test_quickstart.py
+++ skore/tests/integration/cli/test_quickstart.py
@@ -5,13 +5,6 @@ from skore.cli.cli import cli
 
 
 @pytest.fixture
-def fake_working_dir(tmp_path):
-    """Change directory to a temporary one, and return its path."""
-    os.chdir(tmp_path)
-    return tmp_path
-
-
-@pytest.fixture
 def fake_launch(monkeypatch):
     def _fake_launch(project_name, port, open_browser, verbose):
         pass
@@ -19,9 +12,10 @@ def fake_launch(monkeypatch):
     monkeypatch.setattr("skore.cli.quickstart_command.__launch", _fake_launch)
 
 
-def test_quickstart(fake_working_dir, fake_launch):
+def test_quickstart(tmp_path, fake_launch):
+    os.chdir(tmp_path)
     cli("quickstart".split())
-    assert (fake_working_dir / "project.skore").exists()
+    assert (tmp_path / "project.skore").exists()
 
     # calling the same command without overwriting should succeed
     # (as the creation step is skipped)
