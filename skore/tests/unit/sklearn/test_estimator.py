86801669aa7299da427274fd7f10e17c5ffe662a
Guillaume Lemaitre <guillaume@probabl.ai>
2025-01-14 20:00:31 +0100

    another assert for help method name

--- skore/tests/unit/sklearn/test_estimator.py
+++ skore/tests/unit/sklearn/test_estimator.py
@@ -286,6 +286,7 @@ def test_estimator_report_repr(binary_classification_data):
 
     repr_str = repr(report)
     assert "skore.EstimatorReport" in repr_str
+    assert "reporter.help()" in repr_str
 
 
 @pytest.mark.parametrize(
@@ -345,6 +346,7 @@ def test_estimator_report_plot_repr(binary_classification_data):
 
     repr_str = repr(report.metrics.plot)
     assert "skore.EstimatorReport.metrics.plot" in repr_str
+    assert "reporter.metrics.plot.help()" in repr_str
 
 
 def test_estimator_report_plot_roc(binary_classification_data):
@@ -484,6 +486,7 @@ def test_estimator_report_metrics_repr(binary_classification_data):
 
     repr_str = repr(report.metrics)
     assert "skore.EstimatorReport.metrics" in repr_str
+    assert "reporter.metrics.help()" in repr_str
 
 
 @pytest.mark.parametrize(
