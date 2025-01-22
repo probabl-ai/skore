56f0b79aabed3568e9aadb4cad3783ee0be8d7ff
MarieS-WiMLDS <79304610+MarieS-WiMLDS@users.noreply.github.com>
2025-01-22 15:30:04 +0100

    Update examples/model_evaluation/plot_cross_validate.py
    Co-authored-by: Sylvain Combettes <48064216+sylvaincom@users.noreply.github.com>

--- examples/model_evaluation/plot_cross_validate.py
+++ examples/model_evaluation/plot_cross_validate.py
@@ -159,7 +159,7 @@ reporter.plots.scores
 # Cleanup the project
 # -------------------
 #
-# Let's clean the skore project to avoid conflict with other examples.
+# Let's clear the skore project (to avoid any conflict with other documentation examples).
 
 # %%
 my_project.clear()
