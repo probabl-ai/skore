36b1e60ccd78892ef32bc6cc1912c3a14706b08a
Marie <marie.sacksick@posteo.net>
2025-01-28 18:42:34 +0100

    improve spec - exemple

--- examples/model_evaluation/plot_est_compare.py
+++ examples/model_evaluation/plot_est_compare.py
@@ -1,9 +1,9 @@
 """
 .. _example_compare_estimators:
 
-===========
-Example driven development
-===========
+============================================
+Example driven development - Compare feature
+============================================
 """
 
 # %%
@@ -28,7 +28,7 @@ my_project = skore.open("my_project", create=True)
 # current working directory.
 
 # %%
-# Evaluate your model using skore's :class:`~skore.CrossValidationReport`:
+# Evaluate your model using skore's :class:`~skore.EstimatorReport`:
 
 # %%
 from sklearn.datasets import make_classification
@@ -55,13 +55,23 @@ est_report_rf = EstimatorReport(
 # %%
 # here starts the example to compare.
 # to create the comparison object:
-# comp = skore.Comparator(reports = [est_report_lr, est_report_rf])
-# comp = skore.Comparator(reports = project.get("est_rep", version = "all"))
+#
+#   .. code-block:: python
+#
+#       comp = skore.Comparator(reports=[est_report_lr, est_report_rf])
+#       comp = skore.Comparator(reports=project.get("est_rep", version="all"))
+#
+#
 # to check on init
+#
 # - that all estimators are in the same ml use case
+#
 # - that all X_train, y_train have the same hash (for estimator)
+#
 # - that all X_test, y_test have the same hash (for estimator)
+#
 # - that the reports are estimator reports (for now)
+#
 # - that all the reports have the same nature
 
 # %%
@@ -72,17 +82,29 @@ abs = [est.metrics.accuracy().index[0] for est in [est_report_lr, est_report_rf]
 fig = px.scatter(x=abs, y=ord)
 fig.update_traces(marker_size=10)
 
+# %%
 # the comparator object has a structure quite similar to the estimators it's comparing.
-# comp.metrics.plot(metric = "recall")
-fig.show()
-# with proper legend of course.
-# border cases:
+#
+#   .. code-block:: python
+#
+#       comp.metrics.plot(metric="recall")
+#
+fig.show()  # with proper legend of course.
+
+# %%
+# border case:
 # - what if several estimators have the same model name? > in v0, we rename ourselves by adding #1
 
 
 # %%
 # for a further iteration
-# comp.metrics.plot(metric = "recall", with_time = True) # with_time is False by default
+#
+#   .. code-block:: python
+#
+#       comp.metrics.plot(metric="recall", with_time=True)
+#
+# with_time is False by default
+#
 # idea: we would have the training time in abscissa, and the metric in ordinates.
 # cf for instance the second plot of this reddit post: https://www.reddit.com/r/LocalLLaMA/comments/191u1j9/visualising_llm_training_compute_correlating_to/#lightbox
 
@@ -99,12 +121,23 @@ df_metrics = pd.concat(
     [est_report_lr.metrics.report_metrics(), est_report_rf.metrics.report_metrics()],
     ignore_index=False,
 )
+# %%
 # border cases:
-# - what happen if we have computed a score for one model, and not for the other? > actually not a problem.
-# - what if several estimators have the same model name? > cf above, adding #n (1, 2, 3...)
+#
+# - what happen if we have computed a score for one model, and not for the other?
+# > actually not a problem.
+#
+# - what if several estimators have the same model name?
+# > cf above, adding #n (1, 2, 3...)
 
-# comp.metrics.metric()
-# comp.metrics.custom_metric(scoring = my_func)
+# %%
+#
+#   .. code-block:: python
+#
+#       comp.metrics.metric()
+#       comp.metrics.custom_metric(scoring=my_func)
+#
+
 df_metrics.style.apply(highlight_max, axis=0)
 
 # %%
