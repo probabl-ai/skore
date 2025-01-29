374a623f6a4439e66bdf15bb8edb8f13d57fd114
Thomas S <thomas@probabl.ai>
2025-01-29 10:50:07 +0100

    Fix launch on existing project

--- skore/src/skore/ui/launch.py
+++ skore/src/skore/ui/launch.py
@@ -36,7 +36,7 @@ def launch(
     from skore import console  # avoid circular import
 
     with logger_context(logger, verbose):
-        project = Project(project_name)
+        project = Project(project_name, exist_ok=True)
 
         @asynccontextmanager
         async def lifespan(app: FastAPI):
