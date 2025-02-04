4a19b4888204b47b68f5324f2b3f5e8210a9029e
Auguste Baum <auguste@probabl.ai>
2025-02-04 17:09:53 +0100

    add failing test

--- skore/tests/unit/sklearn/test_cross_validation.py
+++ skore/tests/unit/sklearn/test_cross_validation.py
@@ -737,3 +737,39 @@ def test_cross_validation_report_custom_metric(binary_classification_data):
     )
     assert result.shape == (2, 1)
     assert result.columns == ["accuracy_score"]
+
+
+@pytest.fixture
+def fake_clone(monkeypatch):
+    """Mock `clone` function which raises an Exception when called too many times.
+
+    This allows us to interrupt the cross-validation process early, since each
+    cross-validation split is performed on a different `clone`d estimator.
+    """
+    nb_calls = 0
+
+    def failing_clone(estimator):
+        from sklearn.base import clone
+
+        nonlocal nb_calls
+        nb_calls += 1
+        if nb_calls > 2:
+            raise Exception("clone called more than twice")
+
+        return clone(estimator)
+
+    monkeypatch.setattr("skore.sklearn._cross_validation.report.clone", failing_clone)
+
+
+def test_cross_validation_report_interrupted(fake_clone, binary_classification_data):
+    """Check that we can interrupt cross-validation without losing all
+    data."""
+    estimator, X, y = binary_classification_data
+    report = CrossValidationReport(estimator, X, y, cv_splitter=10)
+
+    result = report.metrics.custom_metric(
+        metric_function=accuracy_score,
+        response_method="predict",
+    )
+    assert result.shape == (1, 1)
+    assert result.columns == ["accuracy_score"]
