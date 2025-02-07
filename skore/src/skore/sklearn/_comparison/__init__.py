cca35c98dd3713a34cdbe49e478bc8b7cda40cae
Thomas S <thomas@probabl.ai>
2025-02-07 11:53:58 +0100

    Fix minor typing mistake

--- skore/src/skore/sklearn/_comparison/__init__.py
+++ skore/src/skore/sklearn/_comparison/__init__.py
@@ -3,7 +3,7 @@ from skore.externals._pandas_accessors import _register_accessor
 from .metrics_accessor import _MetricsAccessor, _PlotMetricsAccessor
 from .report import ComparisonReport
 
-# add the metrics accessor to the estimator report
+# add the metrics accessor to the ComparisonReport
 _register_accessor("metrics", ComparisonReport)(_MetricsAccessor)
 
 # add the plot accessor to the metrics accessor
