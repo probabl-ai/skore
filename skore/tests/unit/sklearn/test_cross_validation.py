6cbf3cd30b7155da2ef0af2ebbe2589e9b1a2a2e
Guillaume Lemaitre <guillaume@probabl.ai>
2025-02-07 16:57:33 +0100

    make a well behaving classifier

--- skore/tests/unit/sklearn/test_cross_validation.py
+++ skore/tests/unit/sklearn/test_cross_validation.py
@@ -4,7 +4,7 @@ import joblib
 import numpy as np
 import pandas as pd
 import pytest
-from sklearn.base import BaseEstimator, clone
+from sklearn.base import BaseEstimator, ClassifierMixin, clone
 from sklearn.datasets import make_classification, make_regression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.exceptions import NotFittedError
@@ -752,7 +752,7 @@ def test_cross_validation_report_interrupted(
     """Check that we can interrupt cross-validation without losing all
     data."""
 
-    class MockEstimator(BaseEstimator):
+    class MockEstimator(ClassifierMixin, BaseEstimator):
         def __init__(self, n_call=0, fail_after_n_clone=3):
             self.n_call = n_call
             self.fail_after_n_clone = fail_after_n_clone
@@ -760,6 +760,7 @@ def test_cross_validation_report_interrupted(
         def fit(self, X, y):
             if self.n_call > self.fail_after_n_clone:
                 raise error
+            self.classes_ = np.unique(y)
             return self
 
         def __sklearn_clone__(self):
