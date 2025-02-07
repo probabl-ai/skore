a21f0fbddb7cf9adac0ec66a715741bcd2b0e75a
Guillaume Lemaitre <guillaume@probabl.ai>
2025-02-07 17:06:52 +0100

    iter

--- skore/conftest.py
+++ skore/conftest.py
@@ -1,31 +1,12 @@
 import sklearn
-import pytest
-from _pytest.doctest import DoctestItem
 from sklearn.utils.fixes import parse_version
 
-def pytest_collection_modifyitems(config, items):
-    """Called after collect is completed.
+def pytest_collect_file(parent, path):
+    if path.strpath.startswith('src/'):
+        sklearn_version = parse_version(sklearn.__version__)
+        required_version = parse_version('1.6')
 
-    Parameters
-    ----------
-    config : pytest config
-    items : list of collected items
-    """
-    skip_doctests = False
-    if parse_version(sklearn.__version__) < parse_version("1.6"):
-        reason = "We only check docstrings with the latest version of scikit-learn"
-        skip_doctests = True
+        if sklearn_version < required_version:
+            return None
 
-    # Normally doctest has the entire module's scope. Here we set globs to an empty dict
-    # to remove the module's scope:
-    # https://docs.python.org/3/library/doctest.html#what-s-the-execution-context
-    for item in items:
-        if isinstance(item, DoctestItem):
-            item.dtest.globs = {}
-
-    if skip_doctests:
-        skip_marker = pytest.mark.skip(reason=reason)
-
-        for item in items:
-            if isinstance(item, DoctestItem):
-                item.add_marker(skip_marker)
+    return None
