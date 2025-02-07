d0a4853acb027523e494e1f4e62f505665b9af29
Thomas S <thomas@probabl.ai>
2025-02-07 11:39:50 +0100

    Clean ComparisonReport tests

--- skore/tests/unit/sklearn/test_comparison.py
+++ skore/tests/unit/sklearn/test_comparison.py
@@ -1,193 +1,93 @@
-import warnings
 from io import BytesIO
+from typing import Literal, Optional
 
 import joblib
 import numpy as np
 import pandas as pd
 import pytest
 from sklearn.datasets import make_classification, make_regression
-from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split
 from skore import ComparisonReport, EstimatorReport
 
 
-@pytest.fixture
-def logistic_regression_report():
-    X, y = make_classification(n_classes=2)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    estimator = LogisticRegression()
+def usecase(
+    type: Literal["binary-logisitic-regression", "linear-regression"],
+    random_state: Optional[int] = 42,
+):
+    match type:
+        case "binary-logistic-regression":
+            X, y = make_classification(n_classes=2, random_state=random_state)
+            estimator = LogisticRegression()
+        case "linear-regression":
+            X, y = make_regression(random_state=random_state)
+            estimator = LinearRegression()
+        case _:
+            raise ValueError
 
-    return EstimatorReport(
-        estimator,
-        X_train=X_train,
-        y_train=y_train,
-        X_test=X_test,
-        y_test=y_test,
-    )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=random_state)
 
-
-@pytest.fixture
-def linear_regression_report():
-    X, y = make_regression()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    estimator = LinearRegression()
-
-    return EstimatorReport(
-        estimator,
-        X_train=X_train,
-        y_train=y_train,
-        X_test=X_test,
-        y_test=y_test,
-    )
+    return estimator, X_train, X_test, y_train, y_test
 
 
-def test_comparison_report(logistic_regression_report):
-    comp = ComparisonReport([logistic_regression_report, logistic_regression_report])
+def test_comparison_report_init_wrong_parameters():
+    """If the input is not valid, raise."""
 
-    comp.metrics.help()
-    comp.metrics.report_metrics()
-    comp.metrics.brier_score()
-    comp.metrics.plot.roc()
-    print(comp.metrics.accuracy())
-    print(comp.metrics.accuracy(aggregate="mean"))
-    print(comp.metrics.accuracy(aggregate=["mean", "std"]))
-
-
-def test_comparison_report_different_estimators():
-    X, y = make_classification(random_state=42)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    logistic_regression = LogisticRegression().fit(X_train, y_train)
-    dummy_classifier = DummyClassifier().fit(X_train, y_train)
-
-    from skore import ComparisonReport
-
-    comp = ComparisonReport(
-        [
-            EstimatorReport(logistic_regression, X_test=X_test, y_test=y_test),
-            EstimatorReport(dummy_classifier, X_test=X_test, y_test=y_test),
-        ]
-    )
-
-    comp.metrics.help()
-    with warnings.catch_warnings(action="ignore"):
-        comp.metrics.report_metrics()
-    comp.metrics.brier_score()
-    comp.metrics.plot.roc()
-    print(comp.metrics.accuracy())
-    print(comp.metrics.accuracy(aggregate="mean"))
-    print(comp.metrics.accuracy(aggregate=["mean", "std"]))
-
-
-def test_example(tmp_path):
-    from sklearn.datasets import make_classification
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.linear_model import LogisticRegression
-    from skore import ComparisonReport, EstimatorReport, Project, train_test_split
-
-    X, y = make_classification(n_classes=2)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-    log_reg = LogisticRegression()
-    est_report_lr = EstimatorReport(
-        log_reg, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
-    )
-
-    rf = RandomForestClassifier(max_depth=2, random_state=0)
-    est_report_rf = EstimatorReport(
-        rf, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
-    )
-
-    project = Project(tmp_path / "my-project.skore")
-    project.put("est_rep", est_report_lr)
-    project.put("est_rep", est_report_rf)
-
-    comp1 = ComparisonReport([est_report_lr, est_report_rf])
-    comp2 = ComparisonReport(project.get("est_rep", version="all"))
-
-    print(comp1.metrics.accuracy())
-    print(comp2.metrics.accuracy())
-
-
-def test_no_train_data():
-    """If EstimatorReports do not contain training data, computing metrics
-    with `data_source="train"` should fail."""
-    X, y = make_classification(n_classes=2)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-    log_reg = LogisticRegression()
-    log_reg.fit(X_train, y_train)
-    est_report = EstimatorReport(log_reg, X_test=X_test, y_test=y_test)
-
-    comp = ComparisonReport([est_report, est_report])
-
-    with pytest.raises(ValueError, match="No train data"):
-        comp.metrics.accuracy(data_source="train")
-
-
-def test_comparison_report_init_len(logistic_regression_report):
-    """If we have less than 2 estimator reports to compare, the comparison report raises
-    an exception."""
-
-    with pytest.raises(
-        ValueError, match="At least 2 instances of EstimatorReport are needed"
-    ):
-        ComparisonReport([logistic_regression_report])
-
-
-def test_comparison_report_init_wrong_type(logistic_regression_report):
-    """If the input is not a list of `EstimatorReport`s, raise."""
+    estimator, _, _, _, _ = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(estimator, fit=False)
 
     with pytest.raises(
         TypeError, match="object of type 'EstimatorReport' has no len()"
     ):
-        ComparisonReport(logistic_regression_report)
+        ComparisonReport(estimator_report)
+
+    with pytest.raises(
+        ValueError, match="At least 2 instances of EstimatorReport are needed"
+    ):
+        ComparisonReport([estimator_report])
 
     with pytest.raises(
         TypeError, match="Only instances of EstimatorReport are allowed"
     ):
-        ComparisonReport([logistic_regression_report, None])
+        ComparisonReport([estimator_report, None])
 
     with pytest.raises(
         TypeError, match="Only instances of EstimatorReport are allowed"
     ):
-        ComparisonReport([None, logistic_regression_report])
+        ComparisonReport([None, estimator_report])
 
 
-def test_comparison_report_init_deepcopy(logistic_regression_report):
+def test_comparison_report_init_deepcopy():
     """If an estimator report is modified outside of the comparator, it is not modified
     inside the comparator."""
-
-    comp = ComparisonReport([logistic_regression_report, logistic_regression_report])
+    estimator, _, _, _, _ = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(estimator, fit=False)
+    comp = ComparisonReport([estimator_report, estimator_report])
 
     # check if the deepcopy work well
-    assert comp.estimator_reports_[0]._hash == logistic_regression_report._hash
+    assert comp.estimator_reports_[0]._hash == estimator_report._hash
 
     # modify the estimator report outside of the comparator
-    my_new_hash_value = 0
-    logistic_regression_report._hash = my_new_hash_value
+    estimator_report._hash = 0
 
     # check if there is no impact on the estimator report in the comparator (its value
     # has not changed)
-    assert comp.estimator_reports_[0]._hash != my_new_hash_value
+    assert comp.estimator_reports_[0]._hash != 0
 
 
 def test_comparison_report_init_MissingTrainingDataWarning(capsys):
     """Raise a warning if there is no training data (`None`) for any estimator
     report."""
 
-    X, y = make_classification(n_classes=2)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    estimator = LogisticRegression()
-    estimator.fit(X_train, y_train)
-
-    est_report = EstimatorReport(
+    estimator, _, X_test, _, y_test = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(
         estimator,
+        fit=False,
         X_test=X_test,
         y_test=y_test,
     )
 
-    ComparisonReport([est_report, est_report])
+    ComparisonReport([estimator_report, estimator_report])
 
     captured = capsys.readouterr()
 
@@ -198,28 +98,31 @@ def test_comparison_report_init_MissingTestDataWarning(capsys):
     """Raise a warning if there is no test data (`None`) for any estimator
     report."""
 
-    X, y = make_classification(n_classes=2)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    estimator = LogisticRegression()
-    estimator.fit(X_train, y_train)
-
-    est_report = EstimatorReport(
+    estimator, X_train, _, y_train, _ = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(
         estimator,
+        fit=False,
         X_train=X_train,
         y_train=y_train,
     )
 
-    ComparisonReport([est_report, est_report])
+    ComparisonReport([estimator_report, estimator_report])
 
     captured = capsys.readouterr()
 
     assert "MissingTestDataWarning" in captured.out
 
 
-def test_comparison_report_init_different_ml_usecases(
-    linear_regression_report,
-    logistic_regression_report,
-):
+def test_comparison_report_init_different_ml_usecases():
+    linear_regression_estimator, _, _, _, _ = usecase("linear-regression")
+    linear_regression_report = EstimatorReport(linear_regression_estimator, fit=False)
+
+    logistic_regression_estimator, _, _, _, _ = usecase("binary-logistic-regression")
+    logistic_regression_report = EstimatorReport(
+        logistic_regression_estimator,
+        fit=False,
+    )
+
     with pytest.raises(
         ValueError, match="Not all estimators are in the same ML usecase"
     ):
@@ -227,87 +130,87 @@ def test_comparison_report_init_different_ml_usecases(
 
 
 def test_comparison_report_init_different_training_data():
-    X, y = make_classification(n_classes=2, random_state=0)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-
-    report_1 = EstimatorReport(
-        LogisticRegression(),
-        X_train=X_train,
-        y_train=y_train,
-        X_test=X_test,
-        y_test=y_test,
-    )
-
-    # Different random state, so the train and test data are different
-    X_train, _, y_train, _ = train_test_split(X, y, random_state=2)
-
-    report_2 = EstimatorReport(
-        LogisticRegression(),
-        X_train=X_train,
-        y_train=y_train,
-        X_test=X_test,
-        y_test=y_test,
-    )
+    estimator, X_train, _, y_train, _ = usecase("binary-logistic-regression")
 
     with pytest.raises(
         ValueError, match="Not all estimators have the same training data"
     ):
-        ComparisonReport([report_1, report_2])
+        ComparisonReport(
+            [
+                EstimatorReport(
+                    estimator,
+                    fit=False,
+                    X_train=X_train,
+                    y_train=y_train,
+                ),
+                EstimatorReport(
+                    estimator,
+                    fit=False,
+                    X_train=X_train[:-1],
+                    y_train=y_train[:-1],
+                ),
+            ]
+        )
 
 
 def test_comparison_report_init_different_test_data():
-    X, y = make_classification(n_classes=2, random_state=0)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-
-    report_1 = EstimatorReport(
-        LogisticRegression(),
-        X_train=X_train,
-        y_train=y_train,
-        X_test=X_test,
-        y_test=y_test,
-    )
-
-    # Different random state, so the train and test data are different
-    _, X_test, _, y_test = train_test_split(X, y, random_state=2)
-
-    report_2 = EstimatorReport(
-        LogisticRegression(),
-        X_train=X_train,
-        y_train=y_train,
-        X_test=X_test,
-        y_test=y_test,
-    )
+    estimator, _, X_test, _, y_test = usecase("binary-logistic-regression")
 
     with pytest.raises(
         ValueError, match="Not all estimators have the same testing data"
     ):
-        ComparisonReport([report_1, report_2])
+        ComparisonReport(
+            [
+                EstimatorReport(
+                    estimator,
+                    fit=False,
+                    X_test=X_test,
+                    y_test=y_test,
+                ),
+                EstimatorReport(
+                    estimator,
+                    fit=False,
+                    X_test=X_test[:-1],
+                    y_test=y_test[:-1],
+                ),
+            ]
+        )
 
 
-def test_comparison_report_init_without_report_names(logistic_regression_report):
+def test_comparison_report_init_with_report_names():
+    estimator, X_train, X_test, y_train, y_test = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(
+        estimator,
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
+
     comp = ComparisonReport(
-        [logistic_regression_report, logistic_regression_report],
-        report_names=["report1", "report2"],
+        [estimator_report, estimator_report], report_names=["r1", "r2"]
     )
-    accuracy = comp.metrics.accuracy()
 
-    assert comp.report_names_ == ["report1", "report2"]
     pd.testing.assert_index_equal(
-        accuracy.index,
-        pd.MultiIndex.from_tuples(
-            enumerate(["report1", "report2"]),
-            names=[None, "Estimator"],
-        ),
+        comp.metrics.accuracy().index,
+        pd.MultiIndex.from_tuples(enumerate(["r1", "r2"]), names=[None, "Estimator"]),
     )
 
 
-def test_comparison_report_init_with_report_names(logistic_regression_report):
-    comp = ComparisonReport([logistic_regression_report, logistic_regression_report])
-    accuracy = comp.metrics.accuracy()
+def test_comparison_report_init_without_report_names():
+    estimator, X_train, X_test, y_train, y_test = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(
+        estimator,
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
 
-    assert comp.report_names_ == ["LogisticRegression", "LogisticRegression"]
+    comp = ComparisonReport([estimator_report, estimator_report])
+
     pd.testing.assert_index_equal(
-        accuracy.index,
+        comp.metrics.accuracy().index,
         pd.MultiIndex.from_tuples(
             enumerate(["LogisticRegression", "LogisticRegression"]),
             names=[None, "Estimator"],
@@ -315,40 +218,42 @@ def test_comparison_report_init_with_report_names(logistic_regression_report):
     )
 
 
-def test_comparison_report_init_with_invalid_report_names(logistic_regression_report):
+def test_comparison_report_init_with_invalid_report_names():
+    estimator, _, _, _, _ = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(estimator, fit=False)
+
     with pytest.raises(
         ValueError, match="There should be as many report names as there are reports"
     ):
-        ComparisonReport(
-            [logistic_regression_report, logistic_regression_report],
-            report_names=["report1", "report2", "report3"],
-        )
+        ComparisonReport([estimator_report, estimator_report], report_names=["r1"])
 
 
-def test_comparison_report_help(capsys, logistic_regression_report):
-    """Check that the help method writes to the console."""
-    report = ComparisonReport([logistic_regression_report, logistic_regression_report])
-    report.help()
+def test_comparison_report_help(capsys):
+    estimator, _, _, _, _ = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(estimator, fit=False)
+
+    ComparisonReport([estimator_report, estimator_report]).help()
 
     assert "Tools to compare estimators" in capsys.readouterr().out
 
 
-def test_comparison_report_repr(logistic_regression_report):
-    """Check that __repr__ returns a string starting with the expected prefix."""
-    report = ComparisonReport([logistic_regression_report, logistic_regression_report])
-    repr_str = repr(report)
+def test_comparison_report_repr():
+    estimator, _, _, _, _ = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(estimator, fit=False)
+
+    repr_str = repr(ComparisonReport([estimator_report, estimator_report]))
 
     assert "skore.ComparisonReport" in repr_str
     assert "help()" in repr_str
 
 
-def test_comparison_report_pickle(tmp_path, logistic_regression_report):
+def test_comparison_report_pickle(tmp_path):
     """Check that we can pickle a comparison report."""
-    with BytesIO() as f:
-        joblib.dump(
-            ComparisonReport([logistic_regression_report, logistic_regression_report]),
-            f,
-        )
+    estimator, _, _, _, _ = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(estimator, fit=False)
+
+    with BytesIO() as stream:
+        joblib.dump(ComparisonReport([estimator_report, estimator_report]), stream)
 
 
 @pytest.mark.parametrize(
@@ -375,17 +280,16 @@ def test_estimator_report_metrics_binary_classification(
         dummy,
     )
 
-    X, y = make_classification(n_classes=2)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    logistic_regression_report = EstimatorReport(
-        LogisticRegression(),
+    estimator, X_train, X_test, y_train, y_test = usecase("binary-logistic-regression")
+    estimator_report = EstimatorReport(
+        estimator,
         X_train=X_train,
         y_train=y_train,
         X_test=X_test,
         y_test=y_test,
     )
 
-    comp = ComparisonReport([logistic_regression_report, logistic_regression_report])
+    comp = ComparisonReport([estimator_report, estimator_report])
 
     # ensure metric is valid
     result = getattr(comp.metrics, metric_name)()
@@ -403,7 +307,7 @@ def test_estimator_report_metrics_binary_classification(
         ("r2", 2),
     ],
 )
-def test_estimator_report_metrics_regression(
+def test_estimator_report_metrics_linear_regression(
     monkeypatch,
     metric_name,
     metric_value,
@@ -416,10 +320,9 @@ def test_estimator_report_metrics_regression(
         dummy,
     )
 
-    X, y = make_regression()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    estimator, X_train, X_test, y_train, y_test = usecase("linear-regression")
     estimator_report = EstimatorReport(
-        LinearRegression(),
+        estimator,
         X_train=X_train,
         y_train=y_train,
         X_test=X_test,
