ec2851a9e6daa5c57300c7d8f3737ee93f26457b
Guillaume Lemaitre <guillaume@probabl.ai>
2025-02-07 17:17:09 +0100

    adapt to -e install

--- skore/conftest.py
+++ skore/conftest.py
@@ -10,7 +10,7 @@ def pytest_collection_modifyitems(items):
     if sklearn_version < min_required:
         skip_marker = pytest.mark.skip(reason="Requires scikit-learn >= 1.6")
         for item in items:
-            if isinstance(item, pytest.DoctestItem) and "site-packages/skore" in str(
-                item.path
+            if isinstance(item, pytest.DoctestItem) and (
+                "site-packages/skore" in str(item.path) or "src/skore" in str(item.path)
             ):
                 item.add_marker(skip_marker)
