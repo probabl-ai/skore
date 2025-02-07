2e586629e8169e78d8d814c594500f70222ba627
Guillaume Lemaitre <guillaume@probabl.ai>
2025-02-07 17:12:49 +0100

    iter

--- skore/conftest.py
+++ skore/conftest.py
@@ -1,12 +1,14 @@
+import pytest
 import sklearn
 from sklearn.utils.fixes import parse_version
 
-def pytest_collect_file(parent, path):
-    if path.strpath.startswith('src/'):
-        sklearn_version = parse_version(sklearn.__version__)
-        required_version = parse_version('1.6')
 
-        if sklearn_version < required_version:
-            return None
+def pytest_collection_modifyitems(items):
+    sklearn_version = parse_version(sklearn.__version__)
+    min_required = parse_version("1.6")
 
-    return None
+    if sklearn_version < min_required:
+        skip_marker = pytest.mark.skip(reason="Requires scikit-learn >= 1.6")
+        for item in items:
+            if isinstance(item, pytest.DoctestItem) and item.path.parts[0] == "src":
+                item.add_marker(skip_marker)
