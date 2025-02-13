3f64276394c277ee6c87356efd236747b658ee82
Thomas S <thomas@probabl.ai>
2025-02-13 17:10:07 +0100

    Add test to ensure plot is valid

--- skore/tests/unit/sklearn/comparison/test_roc_curve_display.py
+++ skore/tests/unit/sklearn/comparison/test_roc_curve_display.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+from matplotlib.testing.compare import compare_images
 from numpy import array
 from numpy.testing import assert_equal
 from skore.sklearn._comparison.roc_curve_display import RocCurveDisplay
+
+CWD = Path(__file__).parent.resolve()
 
 
 class FakeEstimator:
@@ -43,7 +49,7 @@ class TestRocCurveDisplay:
             y_true=y_true,
             y_pred=y_pred,
             estimators=estimators,
-            estimator_names=["E1", "E2"],
+            estimator_names=["MC-E1", "MC-E2"],
             ml_task="multiclass-classification",
             data_source="test",
         )
@@ -65,10 +71,35 @@ class TestRocCurveDisplay:
             },
         )
         assert_equal(display.roc_auc, {0: [1.0, 0.0], 1: [1.0, 0.0], 2: [1.0, 0.0]})
-        assert_equal(display.estimator_names, ["E1", "E2"])
+        assert_equal(display.estimator_names, ["MC-E1", "MC-E2"])
         assert_equal(display.ml_task, "multiclass-classification")
         assert_equal(display.pos_label, None)
         assert_equal(display.data_source, "test")
 
-    def test_plot_binary_classification(self): ...
+    def test_plot_binary_classification(self, tmp_path):
+        classes = (0, 1)
+        y_true = (array((0, 1)), array((0, 1)))
+        y_pred = (array((0.2, 0.8)), array((0.8, 0.2)))
+        estimators = (FakeEstimator(classes), FakeEstimator(classes))
+        display = RocCurveDisplay._from_predictions(
+            y_true=y_true,
+            y_pred=y_pred,
+            estimators=estimators,
+            estimator_names=["BC-E1", "BC-E2"],
+            ml_task="binary-classification",
+            data_source="test",
+        )
+
+        display.plot()
+        plt.gcf().savefig(tmp_path / "rc-binary-classification.png")
+
+        assert (
+            compare_images(
+                tmp_path / "rc-binary-classification.png",
+                CWD / "rc-binary-classification.png",
+                0,
+            )
+            is None
+        )
+
     def test_plot_multiclass_classification(self): ...
