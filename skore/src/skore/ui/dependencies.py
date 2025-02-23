928170e6910c0be2d1463e43697c2044ea2b65df
Guillaume Lemaitre <guillaume@probabl.ai>
2025-02-23 19:16:05 +0100

    iter

--- skore/src/skore/ui/dependencies.py
+++ skore/src/skore/ui/dependencies.py
@@ -5,6 +5,6 @@ from pathlib import Path
 __UI_MODULE_PATH = Path(__file__).resolve().parent
 
 
-def get_static_path():
+def get_static_path() -> Path:
     """Injectable static path."""
     return __UI_MODULE_PATH / "static"
