f5804ab63722a80f2193329ae0325c017dae2a10
Guillaume Lemaitre <guillaume@probabl.ai>
2025-02-25 14:10:26 +0100

    Add protocol

---
+++ skore/src/skore/sklearn/_plot/base.py
@@ -0,0 +1,12 @@
+from typing import Any, Optional, Protocol, runtime_checkable
+
+from matplotlib.axes import Axes
+
+
+@runtime_checkable
+class Display(Protocol):
+    """Protocol for the public API of display objects."""
+
+    def plot(self, ax: Optional[Axes] = None, **kwargs: Any) -> None:
+        """Plot the display."""
+        ...
--- skore/src/skore/sklearn/_plot/metrics/roc_curve.py
+++ skore/src/skore/sklearn/_plot/metrics/roc_curve.py
@@ -20,6 +20,26 @@ from skore.sklearn._plot.utils import (
 )
 from skore.sklearn.types import MLTask
 
+LINESTYLE = [
+    ("solid", "solid"),
+    ("dotted", "dotted"),
+    ("dashed", "dashed"),
+    ("dashdot", "dashdot"),
+    ("loosely dotted", (0, (1, 10))),
+    ("dotted", (0, (1, 5))),
+    ("densely dotted", (0, (1, 1))),
+    ("long dash with offset", (5, (10, 3))),
+    ("loosely dashed", (0, (5, 10))),
+    ("dashed", (0, (5, 5))),
+    ("densely dashed", (0, (5, 1))),
+    ("loosely dashdotted", (0, (3, 10, 1, 10))),
+    ("dashdotted", (0, (3, 5, 1, 5))),
+    ("densely dashdotted", (0, (3, 1, 1, 1))),
+    ("dashdotdotted", (0, (3, 5, 1, 5, 1, 5))),
+    ("loosely dashdotdotted", (0, (3, 10, 1, 10, 1, 10))),
+    ("densely dashdotdotted", (0, (3, 1, 1, 1, 1, 1))),
+]
+
 
 class RocCurveDisplay(HelpDisplayMixin, _ClassifierCurveDisplayMixin):
     """ROC Curve visualization.
