d35d161d98dc46b8f136d43d6045244b2feca93d
Auguste Baum <auguste@probabl.ai>
2025-02-26 14:52:47 +0100

    make dataframe index use feature names if they exist

--- skore/tests/unit/sklearn/estimator/feature_importance/test_permutation_importance.py
+++ skore/tests/unit/sklearn/estimator/feature_importance/test_permutation_importance.py
@@ -20,6 +20,17 @@ def regression_data():
     return data
 
 
+def regression_data_dataframe():
+    data = regression_data()
+    data["X_train"] = pd.DataFrame(
+        data["X_train"], columns=["my_feature_0", "my_feature_1", "my_feature_2"]
+    )
+    data["X_test"] = pd.DataFrame(
+        data["X_test"], columns=["my_feature_0", "my_feature_1", "my_feature_2"]
+    )
+    return data
+
+
 def case_default_args_numpy():
     data = regression_data()
 
@@ -78,6 +89,64 @@ def case_several_scoring_numpy():
     return data, kwargs, expected
 
 
+def case_default_args_dataframe():
+    data = regression_data_dataframe()
+
+    kwargs = {"random_state": 42}
+
+    expected = pd.DataFrame(
+        data=np.zeros((3, 5)),
+        index=pd.Index((f"my_feature_{i}" for i in range(3)), name="Feature"),
+        columns=pd.Index((f"Repeat #{i}" for i in range(5)), name="Repeat"),
+    )
+    return data, kwargs, expected
+
+
+def case_r2_dataframe():
+    data = regression_data_dataframe()
+
+    kwargs = {"scoring": make_scorer(r2_score), "random_state": 42}
+
+    expected = pd.DataFrame(
+        data=np.zeros((3, 5)),
+        index=pd.Index((f"my_feature_{i}" for i in range(3)), name="Feature"),
+        columns=pd.Index((f"Repeat #{i}" for i in range(5)), name="Repeat"),
+    )
+    return data, kwargs, expected
+
+
+def case_train_dataframe():
+    data = regression_data_dataframe()
+
+    kwargs = {"data_source": "train", "random_state": 42}
+
+    expected = pd.DataFrame(
+        data=np.zeros((3, 5)),
+        index=pd.Index((f"my_feature_{i}" for i in range(3)), name="Feature"),
+        columns=pd.Index((f"Repeat #{i}" for i in range(5)), name="Repeat"),
+    )
+    return data, kwargs, expected
+
+
+def case_several_scoring_dataframe():
+    data = regression_data_dataframe()
+
+    kwargs = {"scoring": ["r2", "neg_root_mean_squared_error"], "random_state": 42}
+
+    expected = pd.DataFrame(
+        data=np.zeros((2 * 3, 5)),
+        index=pd.MultiIndex.from_product(
+            [
+                ["r2", "neg_root_mean_squared_error"],
+                (f"my_feature_{i}" for i in range(3)),
+            ],
+            names=("Metric", "Feature"),
+        ),
+        columns=pd.Index((f"Repeat #{i}" for i in range(5)), name="Repeat"),
+    )
+    return data, kwargs, expected
+
+
 @pytest.mark.parametrize(
     "estimator",
     [
@@ -94,6 +163,10 @@ def case_several_scoring_numpy():
         case_r2_numpy,
         case_train_numpy,
         case_several_scoring_numpy,
+        case_default_args_dataframe,
+        case_r2_dataframe,
+        case_train_dataframe,
+        case_several_scoring_dataframe,
     ],
 )
 def test_estimator_report_feature_permutation(estimator, params):
