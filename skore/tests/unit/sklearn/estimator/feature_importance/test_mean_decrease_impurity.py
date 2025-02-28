9e75d1a92da8b4803e470953a9baf80fc25e6b38
Auguste Baum <auguste@probabl.ai>
2025-02-28 17:13:59 +0100

    test all sklearn estimators

--- skore/tests/unit/sklearn/estimator/feature_importance/test_mean_decrease_impurity.py
+++ skore/tests/unit/sklearn/estimator/feature_importance/test_mean_decrease_impurity.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pytest
-from sklearn.base import is_classifier, is_regressor
+import sklearn
+from sklearn.base import is_regressor
 from sklearn.datasets import make_classification, make_regression
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import train_test_split
@@ -124,91 +125,47 @@ def test_pandas_dataframe(estimator):
     assert result.columns.tolist() == ["Mean decrease impurity"]
 
 
+def _make_estimator_param(estimator):
+    return pytest.param(estimator, id=estimator.__class__.__name__)
+
+
 @pytest.mark.parametrize(
     "estimator",
     [
-        # sklearn.ensemble.AdaBoostClassifier.feature_importances_ (Python property, in AdaBoostClassifier)
-        # sklearn.ensemble.AdaBoostRegressor.feature_importances_ (Python property, in AdaBoostRegressor)
-        # sklearn.ensemble.ExtraTreesClassifier.feature_importances_ (Python property, in ExtraTreesClassifier)
-        # sklearn.ensemble.ExtraTreesRegressor.feature_importances_ (Python property, in ExtraTreesRegressor)
-        # sklearn.ensemble.GradientBoostingClassifier.feature_importances_ (Python property, in GradientBoostingClassifier)
-        # sklearn.ensemble.GradientBoostingRegressor.feature_importances_ (Python property, in GradientBoostingRegressor)
-        # sklearn.ensemble.RandomForestClassifier.feature_importances_ (Python property, in RandomForestClassifier)
-        # sklearn.ensemble.RandomForestRegressor.feature_importances_ (Python property, in RandomForestRegressor)
-        # sklearn.ensemble.RandomTreesEmbedding.feature_importances_ (Python property, in RandomTreesEmbedding)
-        # sklearn.tree.DecisionTreeClassifier.feature_importances_ (Python property, in DecisionTreeClassifier)
-        # sklearn.tree.DecisionTreeRegressor.feature_importances_ (Python property, in DecisionTreeRegressor)
-        # sklearn.tree.ExtraTreeClassifier.feature_importances_ (Python property, in ExtraTreeClassifier)
-        # sklearn.tree.ExtraTreeRegressor.feature_importances_ (Python property, in ExtraTreeRegressor)
-        # 1.11. Ensembles: Gradient boosting, random forests, bagging, voting, stacking
-        # ...ce evaluation for more details). The feature importance scores of a fit gradient boosting model can be accessed via the feature_importances_ property: >>> from sklearn.datasets import make_hastie_10_2 >>> from sklearn.ensemble import Gradie...
-        # 1.13. Feature selection
-        # ...al set of features and the importance of each feature is obtained either through any specific attribute (such as coef_, feature_importances_) or callable. Then, the least important features are pruned from current set of features. That proc...
-        # AdaBoostClassifier
-        # ...boosted ensemble. estimator_errors_ndarray of floatsClassification error for each estimator in the boosted ensemble. feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. n_features_in_intNumber of fe...
-        # AdaBoostRegressor
-        # ...the boosted ensemble. estimator_errors_ndarray of floatsRegression error for each estimator in the boosted ensemble. feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. n_features_in_intNumber of fe...
-        # DecisionTreeClassifier
-        # ...list of ndarrayThe classes labels (single output problem), or a list of arrays of class labels (multi-output problem). feature_importances_ndarray of shape (n_features,)Return the feature importances. max_features_intThe inferred value of...
-        # DecisionTreeRegressor
-        # ...egressions trained on data with missing values. Read more in the User Guide. Added in version 1.4. Attributes: feature_importances_ndarray of shape (n_features,)Return the feature importances. max_features_intThe inferred value of...
-        # ExtraTreeClassifier
-        # ...(for single output problems), or a list containing the number of classes for each output (for multi-output problems). feature_importances_ndarray of shape (n_features,)Return the feature importances. n_features_in_intNumber of features s...
-        # ExtraTreeRegressor
-        # ...Names of features seen during fit. Defined only when X has feature names that are all strings. Added in version 1.0. feature_importances_ndarray of shape (n_features,)Return the feature importances. n_outputs_intThe number of outputs wh...
-        # ExtraTreesClassifier
-        # ...of classes (single output problem), or a list containing the number of classes for each output (multi-output problem). feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. n_features_in_intNumber of fe...
-        # ExtraTreesRegressor
-        # ...timator_ was renamed to estimator_. estimators_list of DecisionTreeRegressorThe collection of fitted sub-estimators. feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. n_features_in_intNumber of fe...
-        # Feature importances with a forest of trees
-        # ...=0) Feature importance based on mean decrease in impurity Feature importances are provided by the fitted attribute feature_importances_ and they are computed as the mean and standard deviation of accumulation of the impurity decrease w...
-        # Glossary of Common Terms and API Elements
-        # ...used for prediction or transformation; transductive outputs such as labels_ or embedding_; or diagnostic data, such as feature_importances_. Common attributes are listed below. A public attribute may have the same name as a constructor par...
-        # Gradient Boosting regression
-        # ...are less predictive and the error bars of the permutation plot show that they overlap with 0. feature_importance = reg.feature_importances_ sorted_idx = np.argsort(feature_importance) pos = np.arange(sorted_idx.shape[0]) + 0.5 fig = plt.fi...
-        # GradientBoostingClassifier
-        # ...number of trees that are built at each iteration. For binary classifiers, this is always 1. Added in version 1.4.0. feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. oob_improvement_ndarray of sh...
-        # GradientBoostingRegressor
-        # ...n_intThe number of trees that are built at each iteration. For regressors, this is always 1. Added in version 1.4.0. feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. oob_improvement_ndarray of sh...
-        # Model-based and sequential feature selection
-        # ...however works with any model, while SelectFromModel requires the underlying estimator to expose a coef_ attribute or a feature_importances_ attribute. The forward SFS is faster than the backward SFS because it only needs to perform n_featu...
-        # Permutation Importance vs Random Forest Feature Importance (MDI)
-        # ...portance. import pandas as pd feature_names = rf[:-1].get_feature_names_out() mdi_importances = pd.Series( rf[-1].feature_importances_, index=feature_names ).sort_values(ascending=True) ax = mdi_importances.plot.barh() ax.set_title(...
-        # Permutation Importance with Multicollinear or Correlated Features
-        # ...uring training. import matplotlib.pyplot as plt import numpy as np import pandas as pd mdi_importances = pd.Series(clf.feature_importances_, index=X_train.columns) tree_importance_sorted_idx = np.argsort(clf.feature_importances_) fig, (ax...
-        # RandomForestClassifier
-        # ...feature names that are all strings. Added in version 1.0. n_outputs_intThe number of outputs when fit is performed. feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. oob_score_floatScore of the t...
-        # RandomForestRegressor
-        # ...timator_ was renamed to estimator_. estimators_list of DecisionTreeRegressorThe collection of fitted sub-estimators. feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. n_features_in_intNumber of fe...
-        # RandomTreesEmbedding
-        # ..._ was renamed to estimator_. estimators_list of ExtraTreeRegressor instancesThe collection of fitted sub-estimators. feature_importances_ndarray of shape (n_features,)The impurity-based feature importances. n_features_in_intNumber of fe...
-        # RFE
-        # ...stanceA supervised learning estimator with a fit method that provides information about feature importance (e.g. coef_, feature_importances_). n_features_to_selectint or float, default=NoneThe number of features to select. If None, half of...
-        # RFECV
-        # ...ator with a fit method that provides information about feature importance either through a coef_ attribute or through a feature_importances_ attribute. stepint or float, default=1If greater than or equal to 1, then step corresponds to the...
-        # SelectFromModel
-        # ...r is built. This can be both a fitted (if prefit is set to True) or a non-fitted estimator. The estimator should have a feature_importances_ or coef_ attribute after fitting. Otherwise, the importance_getter parameter should be used. thres...
+        _make_estimator_param(estimator)
+        for estimator in [
+            sklearn.ensemble.AdaBoostClassifier(),
+            sklearn.ensemble.AdaBoostRegressor(),
+            sklearn.ensemble.ExtraTreesClassifier(),
+            sklearn.ensemble.ExtraTreesRegressor(),
+            sklearn.ensemble.GradientBoostingClassifier(),
+            sklearn.ensemble.GradientBoostingRegressor(),
+            sklearn.ensemble.RandomForestClassifier(),
+            sklearn.ensemble.RandomForestRegressor(),
+            sklearn.ensemble.RandomTreesEmbedding(),
+            sklearn.tree.DecisionTreeClassifier(),
+            sklearn.tree.DecisionTreeRegressor(),
+            sklearn.tree.ExtraTreeClassifier(),
+            sklearn.tree.ExtraTreeRegressor(),
+        ]
     ],
 )
 def test_all_sklearn_estimators(
     request, estimator, regression_data, classification_data
 ):
     """Check that `mean_decrease_impurity` is supported for every sklearn estimator."""
-    if is_classifier(estimator):
-        X, y = classification_data
-    elif is_regressor(estimator):
+    if is_regressor(estimator):
         X, y = regression_data
     else:
-        raise Exception("Estimator is neither a classifier nor a regressor")
+        X, y = classification_data
 
     estimator.fit(X, y)
 
     report = EstimatorReport(estimator)
     result = report.feature_importance.mean_decrease_impurity()
 
-    assert result.shape == (6, 1)
+    assert result.shape == (5, 1)
     assert result.index.tolist() == [
-        "Intercept",
         "Feature #0",
         "Feature #1",
         "Feature #2",
