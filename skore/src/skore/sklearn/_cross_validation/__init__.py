484a863872bdb709b5a4c211c7130dc639b56303
Auguste Baum <auguste@probabl.ai>
2025-03-03 17:53:12 +0100

    assign accessor without `_register_accessor`

--- skore/src/skore/sklearn/_cross_validation/__init__.py
+++ skore/src/skore/sklearn/_cross_validation/__init__.py
@@ -1,7 +1,7 @@
-from skore.externals._pandas_accessors import _register_accessor
+from skore.externals._pandas_accessors import Accessor
 from skore.sklearn._cross_validation.metrics_accessor import _MetricsAccessor
 from skore.sklearn._cross_validation.report import CrossValidationReport
 
-_register_accessor("metrics", CrossValidationReport)(_MetricsAccessor)
+CrossValidationReport.metrics = Accessor("metrics", _MetricsAccessor)
 
 __all__ = ["CrossValidationReport"]
