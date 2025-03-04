5432a40e330dcab9cab7889317e51ced59df9757
Sylvain Combettes <48064216+sylvaincom@users.noreply.github.com>
2025-03-04 16:57:41 +0100

    feat: Display the model coefficients for linear models (#1339)
    Co-authored-by: Auguste Baum <auguste@probabl.ai>
    Co-authored-by: Marie Sacksick <79304610+MarieSacksick@users.noreply.github.com>
    Co-authored-by: Guillaume Lemaitre <guillaume@probabl.ai>

--- skore/src/skore/utils/_accessor.py
+++ skore/src/skore/utils/_accessor.py
@@ -1,5 +1,7 @@
 from typing import Any, Callable
 
+from sklearn.pipeline import Pipeline
+
 
 def _check_supported_ml_task(supported_ml_tasks: list[str]) -> Callable:
     def check(accessor: Any) -> bool:
@@ -14,5 +16,24 @@ def _check_supported_ml_task(supported_ml_tasks: list[str]) -> Callable:
             )
 
         return True
+
+    return check
+
+
+def _check_has_coef() -> Callable:
+    def check(accessor: Any) -> bool:
+        """Check if the estimator has a `coef_` attribute."""
+        parent_estimator = accessor._parent.estimator_
+        estimator = (
+            parent_estimator.steps[-1][1]
+            if isinstance(parent_estimator, Pipeline)
+            else parent_estimator
+        )
+        if hasattr(estimator, "coef_"):
+            return True
+        raise AttributeError(
+            f"Estimator {accessor._parent.estimator_} is not a supported estimator by "
+            "the function called."
+        )
 
     return check
