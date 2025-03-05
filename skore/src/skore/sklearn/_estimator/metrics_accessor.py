095876ff1cc851e1c4030bd3be32bb55f8788d69
Auguste Baum <auguste@probabl.ai>
2025-03-05 11:25:18 +0100

    update Scoring type

--- skore/src/skore/sklearn/_estimator/metrics_accessor.py
+++ skore/src/skore/sklearn/_estimator/metrics_accessor.py
@@ -25,6 +25,17 @@ from skore.utils._index import flatten_multi_index
 
 DataSource = Literal["test", "train", "X_y"]
 
+Metric = Literal[
+    "accuracy",
+    "precision",
+    "recall",
+    "brier_score",
+    "roc_auc",
+    "log_loss",
+    "r2",
+    "rmse",
+]
+
 
 class _MetricsAccessor(_BaseAccessor["EstimatorReport"], DirNamesMixin):
     """Accessor for metrics-related operations.
