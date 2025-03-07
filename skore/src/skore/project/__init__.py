ebc429da973fc2f61d3e51bac630fa726751b142
Thomas S <thomas@probabl.ai>
2025-03-07 17:03:12 +0100

    [ci skip] Use project classes as plugins

--- skore/src/skore/project/__init__.py
+++ skore/src/skore/project/__init__.py
@@ -1,5 +1,21 @@
-"""Alias top level function and class of the project submodule."""
+import sys
+import typing
 
-from .project import Project
 
-__all__ = ["Project"]
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
+
+
+if not (PLUGINS := entry_points(group="skore.plugins.projects")):
+    raise SystemError("No project plugin founded, please install at least one.")
+
+
+def open(mode: typing.Literal[*PLUGINS.names], /, *args, **kwargs):
+    if mode not in PLUGINS.names:
+        raise ValueError(
+            f"Unknown mode '{mode}'. Available modes {', '.join(PLUGINS.names)}."
+        )
+
+    return PLUGINS[mode].load()(*args, **kwargs)
