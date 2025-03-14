fbeaee42f2dbc04b1a7d518ded226aa563eda164
Thomas S <thomas@probabl.ai>
2025-03-14 17:20:50 +0100

    Remove monkeypatch_tmpdir from skore/tests/conftest.py

--- skore/tests/conftest.py
+++ skore/tests/conftest.py
@@ -69,24 +69,3 @@ def pyplot():
     pyplot.close("all")
     yield pyplot
     pyplot.close("all")
-
-
-@pytest.fixture(autouse=True)
-def monkeypatch_tmpdir(monkeypatch, tmp_path):
-    """A pytest fixture that temporarily changes the system's temporary directory.
-
-    This fixture modifies the TMPDIR environment variable to point to a temporary
-    path provided by pytest's tmp_path fixture. It also handles the cached nature
-    of tempfile.gettempdir() by reloading the tempfile module.
-    """
-    import tempfile
-    from importlib import reload
-
-    with monkeypatch.context() as mp:
-        mp.setenv("TMPDIR", str(tmp_path))
-
-        # The first call of `tempfile.gettempdir` being cached, reload the module.
-        # https://docs.python.org/3/library/tempfile.html#tempfile.gettempdir
-        reload(tempfile)
-
-        yield
