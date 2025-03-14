f873e8f546247a818ee0f96631f9b518601a7f09
Auguste Baum <auguste@probabl.ai>
2025-03-14 14:42:02 +0100

    change _hash behaviour without passing arguments, using config

--- skore/src/skore/utils/_hash.py
+++ skore/src/skore/utils/_hash.py
@@ -1,5 +1,12 @@
+from typing import Any
+
 from skore.externals._joblib_hash import hash
+
+# A hashlib hash object, that has a `update` and a `digest` method
+HashObject = Any
 
 
 def _hash(obj, coerce_mmap=False):
-    return hash(obj, hash_name="md5", coerce_mmap=coerce_mmap)
+    from skore import get_config
+
+    return hash(obj, hash_name=get_config()["hash_func"], coerce_mmap=coerce_mmap)
--- skore/tests/unit/utils/test_hash.py
+++ skore/tests/unit/utils/test_hash.py
@@ -1,5 +1,6 @@
 import numpy
 import pytest
+from skore._config import config_context
 from skore.utils._hash import _hash
 
 
@@ -13,3 +14,20 @@ from skore.utils._hash import _hash
 )
 def test_hash(obj):
     _hash(obj)
+
+
+@pytest.mark.parametrize(
+    "obj",
+    [
+        3,
+        numpy.array([0.3]),
+        (numpy.array([0.9]), numpy.array([0.3])),
+    ],
+)
+def test_hash_different_config(obj):
+    default = _hash(obj)
+
+    with config_context(hash_func="sha1"):
+        sha1 = _hash(obj)
+
+    assert default != sha1
