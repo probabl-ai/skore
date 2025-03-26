7c64586aaf70a8bb7a69d84886446ba2d5a132f5
Auguste Baum <auguste@probabl.ai>
2025-03-26 14:45:12 +0100

    wip

--- skore/tests/unit/sklearn/comparison/test_compare_cross_validation_reports.py
+++ skore/tests/unit/sklearn/comparison/test_compare_cross_validation_reports.py
@@ -3,10 +3,13 @@ from pandas.testing import assert_frame_equal
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from skore import CrossValidationComparisonReport, CrossValidationReport
+from skore._config import set_config
 
 
 def test_metrics_binary_classification():
     """Check the metrics work."""
+    set_config(show_progress=False)
+    
     X, y = make_classification(random_state=42)
     cv_report = CrossValidationReport(LogisticRegression(), X, y)
 
