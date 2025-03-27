5c75803a046f690e4ebe7cbb51fa44e27bc8f2c1
Thomas S <thomas@probabl.ai>
2025-03-27 20:19:18 +0100

    [ci skip] WIP

--- skore-remote-project/src/skore_remote_project/project/project.py
+++ skore-remote-project/src/skore_remote_project/project/project.py
@@ -103,35 +103,3 @@ class Project:
             return dto(response[version])
 
         raise ValueError('`version` should be -1, "all", or a positive integer')
-
-
-import pandas as pd
-
-
-class Metadata(pd.DataFrame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if isinstance(self.index, pd.RangeIndex):
-            self.set_index("report_id", inplace=True)
-
-        self.index.name = None
-
-    @property
-    def _constructor(self):
-        return Metadata
-
-    def reports(self):
-        return [f"REPORT {index}" for index in self.index]
-
-
-if __name__ == "__main__":
-    df = Metadata(
-        {
-            "A": [1, 2, 3],
-            "B": [4, 5, 6],
-            "C": [7, 8, 9],
-            "report_id": ["RID-1", "RID-2", "RID-3"],
-        }
-    )
-    df.iloc[1:2].reports()
