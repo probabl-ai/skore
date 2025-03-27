08f50aacba10324fbea6078f2f1b325677a3b000
Auguste Baum <auguste@probabl.ai>
2025-03-27 11:54:17 +0100

    another attempt at making the doctest less flaky

--- skore/src/skore/utils/_measure_time.py
+++ skore/src/skore/utils/_measure_time.py
@@ -9,7 +9,7 @@ class MeasureTime:
     >>> from time import sleep
     >>> with MeasureTime() as time_taken:
     ...     sleep(0.01)
-    >>> assert 0 <= time_taken() <= 0.1 # Note: time_taken is a callable
+    >>> assert 0 <= time_taken() <= 1 # Note: time_taken is a callable
     """
 
     def __enter__(self):
