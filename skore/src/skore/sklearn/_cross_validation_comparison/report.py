2dca29973f1035ed2dae1b520f92f1a77c8483e9
Auguste Baum <auguste@probabl.ai>
2025-04-01 17:10:41 +0200

    fix mypy, remove X and y from args

--- skore/src/skore/sklearn/_cross_validation_comparison/report.py
+++ skore/src/skore/sklearn/_cross_validation_comparison/report.py
@@ -218,9 +218,7 @@ class CrossValidationComparisonReport(_BaseReport, DirNamesMixin):
         for report in self.reports_:
             # Pass the progress manager to child tasks
             report._parent_progress = progress
-            report.cache_predictions(
-                response_methods=response_methods, n_jobs=n_jobs
-            )
+            report.cache_predictions(response_methods=response_methods, n_jobs=n_jobs)
             progress.update(main_task, advance=1, refresh=True)
 
     ####################################################################################
