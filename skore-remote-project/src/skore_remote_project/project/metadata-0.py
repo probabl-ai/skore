04d200cef60613360d47684c5da3fb3ff2b9d965
Thomas S <thomas@probabl.ai>
2025-04-02 12:25:15 +0200

    [ci skip] WIP

--- skore-remote-project/src/skore_remote_project/project/metadata-0.py
+++ skore-remote-project/src/skore_remote_project/project/metadata-0.py
@@ -3,28 +3,33 @@ import pandas as pd
 
 class Metadata(pd.DataFrame):
     def __init__(self, *args, **kwargs):
+        if not (args or kwargs):  # retrieve metadata from hub
+            kwargs = dict(copy=False)
+            args = [
+                pd.DataFrame(
+                    {
+                        "A": [1, 2, 3],
+                        "B": [4, 5, 6],
+                        "C": [7, 8, 9],
+                    },
+                    index=pd.MultiIndex.from_arrays(
+                        [
+                            pd.RangeIndex(3),
+                            pd.Index(["RID-1", "RID-2", "RID-3"], name="id"),
+                        ]
+                    ),
+                )
+            ]
+
         super().__init__(*args, **kwargs)
-
-        if isinstance(self.index, pd.RangeIndex):
-            self.set_index("report_id", inplace=True)
-
-        self.index.name = None
 
     @property
     def _constructor(self):
         return Metadata
 
     def reports(self):
-        return [f"REPORT {index}" for index in self.index]
+        return [f"REPORT {id}" for id in self.index.get_level_values("id")]
 
 
-df = Metadata(
-    {
-        "A": [1, 2, 3],
-        "B": [4, 5, 6],
-        "C": [7, 8, 9],
-        "report_id": ["RID-1", "RID-2", "RID-3"],
-    }
-)
-
+df = Metadata()
 df.iloc[1:2].reports()
