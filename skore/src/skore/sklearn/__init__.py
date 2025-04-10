bf0f7ffdd5272f6a3f8467655d4917b5eb4005d4
Auguste Baum <auguste@probabl.ai>
2025-04-10 16:06:07 +0200

    remove CrossValidationComparison

--- skore/src/skore/sklearn/__init__.py
+++ skore/src/skore/sklearn/__init__.py
@@ -2,7 +2,6 @@
 
 from skore.sklearn._comparison import ComparisonReport
 from skore.sklearn._cross_validation import CrossValidationReport
-from skore.sklearn._cross_validation_comparison import CrossValidationComparisonReport
 from skore.sklearn._estimator import EstimatorReport
 from skore.sklearn._plot import (
     PrecisionRecallCurveDisplay,
@@ -13,7 +12,6 @@ from skore.sklearn.train_test_split.train_test_split import train_test_split
 
 __all__ = [
     "ComparisonReport",
-    "CrossValidationComparisonReport",
     "CrossValidationReport",
     "EstimatorReport",
     "PrecisionRecallCurveDisplay",
