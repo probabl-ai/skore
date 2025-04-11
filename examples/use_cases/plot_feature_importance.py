49359cb28155a43d275c019a803e18a73a63971f
Sylvain Combettes <48064216+sylvaincom@users.noreply.github.com>
2025-04-11 15:06:45 +0200

    Update examples/use_cases/plot_feature_importance.py

--- examples/use_cases/plot_feature_importance.py
+++ examples/use_cases/plot_feature_importance.py
@@ -253,7 +253,6 @@ ridge_report.metrics.report_metrics()
 
 # %%
 ridge_report.metrics.prediction_error().plot(kind="actual_vs_predicted")
-plt.tight_layout()
 
 # %%
 # We can observe that the model has issues predicting large house prices, due to the
