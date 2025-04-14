28e80fd121469a5e0bd4408dbe24e5d43b069625
waridrox <mohdwarid4@gmail.com>
2025-04-14 21:35:25 +0530

    feat: Export ConfusionMatrixDisplay in metrics submodule public API
    Signed-off-by: waridrox <mohdwarid4@gmail.com>

--- skore/src/skore/sklearn/_plot/metrics/__init__.py
+++ skore/src/skore/sklearn/_plot/metrics/__init__.py
@@ -1,3 +1,4 @@
+from skore.sklearn._plot.metrics.confusion_matrix import ConfusionMatrixDisplay
 from skore.sklearn._plot.metrics.precision_recall_curve import (
     PrecisionRecallCurveDisplay,
 )
@@ -5,6 +6,7 @@ from skore.sklearn._plot.metrics.prediction_error import PredictionErrorDisplay
 from skore.sklearn._plot.metrics.roc_curve import RocCurveDisplay
 
 __all__ = [
+    "ConfusionMatrixDisplay",
     "PrecisionRecallCurveDisplay",
     "PredictionErrorDisplay",
     "RocCurveDisplay",
