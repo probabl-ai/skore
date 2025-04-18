3771a04d473b0f4942680a05636fb00cf6b1594f
Thomas S <thomas@probabl.ai>
2025-04-18 13:58:31 +0200

    [ci skip] Fix `project.metadata` when project doesnt exist yet

--- skore-remote-project/tests/unit/project/test_project.py
+++ skore-remote-project/tests/unit/project/test_project.py
@@ -2,8 +2,11 @@ from json import dumps
 from urllib.parse import urljoin
 
 from httpx import Client, Response
+from pandas import Categorical, DataFrame, RangeIndex, MultiIndex, Index
+from pandas.testing import assert_frame_equal
 from pytest import fixture, mark, raises
 from skore_remote_project import Project
+from skore_remote_project.project.metadata import Metadata
 
 
 class FakeClient(Client):
@@ -24,6 +27,10 @@ class TestProject:
             "skore_remote_project.project.project.AuthenticatedClient",
             FakeClient,
         )
+        monkeypatch.setattr(
+            "skore_remote_project.project.metadata.AuthenticatedClient",
+            FakeClient,
+        )
 
     def test_tenant(self):
         assert Project("<tenant>", "<name>").tenant == "<tenant>"
@@ -32,15 +39,61 @@ class TestProject:
         assert Project("<tenant>", "<name>").name == "<name>"
 
     @mark.respx(assert_all_called=True)
-    def test_run_id(self, monkeypatch, respx_mock):
+    def test_run_id(self, monkeypatch, nowstr, respx_mock):
         respx_mock.post("projects/<tenant>/<name>/runs").mock(
             Response(200, json={"id": "<id>"})
         )
 
         assert Project("<tenant>", "<name>").run_id == "<id>"
 
-    def test_metadata(self):
-        raise NotImplementedError
+    @mark.respx(assert_all_called=True)
+    def test_metadata(self, monkeypatch, nowstr, respx_mock):
+        url = "projects/<tenant>/<name>/runs"
+        respx_mock.post(url).mock(Response(200, json={"id": "<id>"}))
+
+        url = "projects/<tenant>/<name>/experiments/estimator-reports"
+        respx_mock.get(url).mock(
+            Response(
+                200,
+                json=[
+                    {
+                        "id": 0,
+                        "run_id": 0,
+                        "ml_task": "<ml_task>",
+                        "estimator_class_name": "<estimator_class_name>",
+                        "dataset_fingerprint": "<dataset_fingerprint>",
+                        "created_at": nowstr,
+                        "metrics": [],
+                    },
+                ],
+            )
+        )
+
+        project = Project("<tenant>", "<name>")
+        metadata = project.metadata()
+
+        assert isinstance(metadata, DataFrame)
+        assert isinstance(metadata, Metadata)
+        assert metadata.project == project
+
+        assert_frame_equal(
+            metadata,
+            DataFrame(
+                {
+                    "run_id": [0],
+                    "ml_task": ["<ml_task>"],
+                    "learner": Categorical(["<estimator_class_name>"]),
+                    "dataset": ["<dataset_fingerprint>"],
+                    "date": [nowstr],
+                },
+                MultiIndex.from_arrays(
+                    [
+                        RangeIndex(1),
+                        Index(["0"], name="id", dtype=str),
+                    ]
+                ),
+            ),
+        )
 
     def test_put_exception(self):
         with raises(TypeError, match="Key must be a string"):
@@ -49,6 +102,7 @@ class TestProject:
         with raises(TypeError, match="Note must be a string"):
             Project("<tenant>", "<name>").put("<key>", "<value>", note=0)
 
+    @mark.respx(assert_all_called=True)
     def test_put(self, monkeypatch, respx_mock, now):
         respx_mock.post("projects/<tenant>/<name>/runs").mock(
             Response(200, json={"id": "<id>"})
