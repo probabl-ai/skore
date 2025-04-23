f087a5d862a5b33603b2c606e8336214e6dba8c5
Aditi Juneja <1509aditi@gmail.com>
2025-04-24 00:56:36 +0530

    using get_tags

--- skore/tests/unit/sklearn/estimator/feature_importance/test_coefficients.py
+++ skore/tests/unit/sklearn/estimator/feature_importance/test_coefficients.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import StandardScaler
 from skore import EstimatorReport
-from skore.externals._sklearn_compat import is_clusterer
+from skore.externals._sklearn_compat import get_tags, is_clusterer
 
 
 @pytest.mark.parametrize(
@@ -210,13 +210,7 @@ def test_all_sklearn_estimators(
     if is_classifier(estimator):
         X, y = classification_data
     elif is_regressor(estimator):
-        if isinstance(
-            estimator,
-            (
-                sklearn.linear_model.GammaRegressor,
-                sklearn.linear_model.PoissonRegressor,
-            ),
-        ):
+        if get_tags(estimator).target_tags.positive_only:
             X, y = positive_regression_data
         elif isinstance(
             estimator,
