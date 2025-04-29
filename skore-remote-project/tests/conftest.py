ea261736b180348235c8cc290f74caf1cbf44471
Thomas S <thomas@probabl.ai>
2025-04-29 17:26:13 +0200

    fix: Set `matplotlib.backend` to `agg` during artifacts generation

--- skore-remote-project/tests/conftest.py
+++ skore-remote-project/tests/conftest.py
@@ -1,62 +1,62 @@
-from datetime import datetime, timezone
-
 from pytest import fixture
+
+
+@fixture(autouse=True)
+def setup(monkeypatch, tmp_path):
+    import importlib
+    import tempfile
+
+    import matplotlib
+
+    with monkeypatch.context() as mp:
+        # Change ``TMPDIR`` used by ``tempfile.gettempdir()`` to point to ``tmp_path``,
+        # so that it is automatically deleted after use, with no impact on the user's
+        # environment.
+        mp.setenv("TMPDIR", str(tmp_path))
+
+        # Force the reload of the ``tempfile`` module to change the cached return of
+        # ``tempfile.gettempdir()``.
+        #
+        # https://docs.python.org/3/library/tempfile.html#tempfile.gettempdir
+        importlib.reload(tempfile)
+
+        # Make `skrub.TableReport.html_snippet()` reproducible
+        #
+        # https://github.com/skrub-data/skrub/blob/35f573ce586fe61ef2c72f4c0c4b188ebf2e664b/skrub/_reporting/_html.py#L153
+        mp.setattr("secrets.token_hex", lambda: "<token>")
+
+        # Make `matplotlib.Figure.savefig(format="svg")` reproducible
+        #
+        # https://matplotlib.org/stable/users/prev_whats_new/whats_new_2.1.0.html#reproducible-ps-pdf-and-svg-output
+        # https://matplotlib.org/stable/users/prev_whats_new/whats_new_3.10.0.html#svg-id-rcparam
+        mp.setenv("SOURCE_DATE_EPOCH", "0")
+
+        matplotlib_rcparams = matplotlib.rcParams.copy()
+
+        try:
+            matplotlib.rcParams["svg.hashsalt"] = "<hashsalt>"
+
+            if "svg.id" in matplotlib.rcParams:
+                matplotlib.rcParams["svg.id"] = "<id>"
+
+            # Use a non-interactive ``matplotlib.backend`` that can only write to files.
+            #
+            # https://github.com/matplotlib/matplotlib/issues/29119
+            # https://matplotlib.org/stable/users/explain/figure/backends.html#selecting-a-backend
+            matplotlib.rcParams["backend"] = "agg"
+
+            yield
+        finally:
+            matplotlib.rcParams = matplotlib_rcparams
 
 
 @fixture
 def now():
-    return datetime.now(tz=timezone.utc)
+    import datetime
+
+    return datetime.datetime.now(tz=datetime.timezone.utc)
 
 
 @fixture
 def nowstr(now):
     return now.isoformat()
-
-
-@fixture(autouse=True)
-def monkeypatch_tmpdir(monkeypatch, tmp_path):
-    """
-    A pytest fixture that temporarily changes the system's temporary directory.
-
-    It modifies the ``TMPDIR`` environment variable to point to a temporary path managed
-    by pytest's ``tmp_path`` fixture. Thus, it will be automatically deleted after use
-    and will not impact the user's environment.
-
-    It also forces the reload of the ``tempfile`` module to change the cached return of
-    ``tempfile.gettempdir()``.
-    """
-    import tempfile
-    from importlib import reload
-
-    with monkeypatch.context() as mp:
-        mp.setenv("TMPDIR", str(tmp_path))
-
-        # The first call of `tempfile.gettempdir` being cached, reload the module.
-        # https://docs.python.org/3/library/tempfile.html#tempfile.gettempdir
-        reload(tempfile)
-
-        yield
-
-
-@fixture
-def reproducible(monkeypatch):
-    import matplotlib
-
-    # Make `skrub.TableReport.html_snippet()` reproducible
-    # https://github.com/skrub-data/skrub/blob/35f573ce586fe61ef2c72f4c0c4b188ebf2e664b/skrub/_reporting/_html.py#L153
-    monkeypatch.setattr("secrets.token_hex", lambda: "<token>")
-
-    # Make `matplotlib.Figure.savefig(format="svg")` reproducible
-    # https://matplotlib.org/stable/users/prev_whats_new/whats_new_2.1.0.html#reproducible-ps-pdf-and-svg-output
-    # https://matplotlib.org/stable/users/prev_whats_new/whats_new_3.10.0.html#svg-id-rcparam
-    monkeypatch.setenv("SOURCE_DATE_EPOCH", "0")
-
-    try:
-        matplotlib.rcParams["svg.hashsalt"] = "<hashsalt>"
-
-        if "svg.id" in matplotlib.rcParams:
-            matplotlib.rcParams["svg.id"] = "<id>"
-
-        yield
-    finally:
-        matplotlib.rcdefaults()
