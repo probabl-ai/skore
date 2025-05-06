4d487734380a82ba31787f1f4c05e925855a4ca4
Auguste Baum <auguste@probabl.ai>
2025-05-06 14:16:03 +0200

    Move NotImplementedError statement

--- skore/src/skore/sklearn/_comparison/metrics_accessor.py
+++ skore/src/skore/sklearn/_comparison/metrics_accessor.py
@@ -1260,9 +1260,6 @@ class _MetricsAccessor(_BaseAccessor, DirNamesMixin):
         display : display_class
             The display.
         """
-        if self._parent._reports_type == "CrossValidationReport":
-            raise NotImplementedError()
-
         if "seed" in display_kwargs and display_kwargs["seed"] is None:
             cache_key = None
         else:
@@ -1285,50 +1282,56 @@ class _MetricsAccessor(_BaseAccessor, DirNamesMixin):
             y_true: list[YPlotData] = []
             y_pred: list[YPlotData] = []
 
-            for report, report_name in zip(
-                self._parent.reports_, self._parent.report_names_
-            ):
-                report_X, report_y, _ = report.metrics._get_X_y_and_data_source_hash(
-                    data_source=data_source,
-                    X=X,
-                    y=y,
-                )
-
-                y_true.append(
-                    YPlotData(
-                        estimator_name=report_name,
-                        split_index=None,
-                        y=report_y,
-                    )
-                )
-                y_pred.append(
-                    YPlotData(
-                        estimator_name=report_name,
-                        split_index=None,
-                        y=_get_cached_response_values(
-                            cache=report._cache,
-                            estimator_hash=report._hash,
-                            estimator=report._estimator,
-                            X=report_X,
-                            response_method=response_method,
+            if self._parent._reports_type == "EstimatorReport":
+                for report, report_name in zip(
+                    self._parent.reports_, self._parent.report_names_
+                ):
+                    report_X, report_y, _ = (
+                        report.metrics._get_X_y_and_data_source_hash(
                             data_source=data_source,
-                            data_source_hash=None,
-                            pos_label=display_kwargs.get("pos_label"),
-                        ),
+                            X=X,
+                            y=y,
+                        )
                     )
-                )
-                progress.update(main_task, advance=1, refresh=True)
 
-            display = display_class._compute_data_for_display(
-                y_true=y_true,
-                y_pred=y_pred,
-                report_type="comparison-estimator",
-                estimators=[report.estimator_ for report in self._parent.reports_],
-                estimator_names=self._parent.report_names_,
-                ml_task=self._parent._ml_task,
-                data_source=data_source,
-                **display_kwargs,
-            )
+                    y_true.append(
+                        YPlotData(
+                            estimator_name=report_name,
+                            split_index=None,
+                            y=report_y,
+                        )
+                    )
+                    y_pred.append(
+                        YPlotData(
+                            estimator_name=report_name,
+                            split_index=None,
+                            y=_get_cached_response_values(
+                                cache=report._cache,
+                                estimator_hash=report._hash,
+                                estimator=report._estimator,
+                                X=report_X,
+                                response_method=response_method,
+                                data_source=data_source,
+                                data_source_hash=None,
+                                pos_label=display_kwargs.get("pos_label"),
+                            ),
+                        )
+                    )
+                    progress.update(main_task, advance=1, refresh=True)
+
+                display = display_class._compute_data_for_display(
+                    y_true=y_true,
+                    y_pred=y_pred,
+                    report_type="comparison-estimator",
+                    estimators=[report.estimator_ for report in self._parent.reports_],
+                    estimator_names=self._parent.report_names_,
+                    ml_task=self._parent._ml_task,
+                    data_source=data_source,
+                    **display_kwargs,
+                )
+
+            else:
+                raise NotImplementedError()
 
             if cache_key is not None:
                 # Unless seed is an int (i.e. the call is deterministic),
