163107a12877764d6953f59ac1938cea0c3cd38d
Thomas S <thomas@probabl.ai>
2025-05-11 22:07:58 +0200

    Adapt test from skore-remote-project/ to skore/

--- skore/tests/unit/project/test_metadata.py
+++ skore/tests/unit/project/test_metadata.py
@@ -1,109 +1,79 @@
-from datetime import timedelta
-from urllib.parse import urljoin
+from types import SimpleNamespace
 
-from httpx import Client, Response
 from pandas import DataFrame, Index, MultiIndex, RangeIndex
 from pandas.testing import assert_index_equal
-from pytest import fixture, mark
-from skore_remote_project.item.skore_estimator_report_item import (
-    Metadata as SkoreEstimatorReportItemMetadata,
-)
-from skore_remote_project.item.skore_estimator_report_item import (
-    SkoreEstimatorReportItem,
-)
-from skore_remote_project.project.project import Metadata
+from pytest import fixture
+from sklearn.datasets import make_classification, make_regression
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
+from skore import EstimatorReport
+from skore.project.metadata import Metadata
 
 
-class Namespace:
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+@fixture
+def regression():
+    X, y = make_regression(random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    return EstimatorReport(
+        LinearRegression(),
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
 
 
-class FakeClient(Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__()
+@fixture
+def binary_classification():
+    X, y = make_classification(random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-    def request(self, method, url, **kwargs):
-        response = super().request(method, urljoin("http://localhost", url), **kwargs)
-        response.raise_for_status()
+    return EstimatorReport(
+        LogisticRegression(),
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
 
-        return response
+
+class FakeProject:
+    def __init__(self, *reports):
+        self.__reports = reports
+
+    @property
+    def reports(self):
+        def get(id: str):
+            return self.__reports[int(id)]
+
+        def metadata():
+            return [
+                {
+                    "id": index,
+                    "run_id": None,
+                    "key": None,
+                    "date": None,
+                    "learner": None,
+                    "dataset": None,
+                    "ml_task": report._ml_task,
+                    "rmse": None,
+                    "log_loss": None,
+                    "roc_auc": None,
+                    "fit_time": None,
+                    "predict_time": None,
+                }
+                for index, report in enumerate(self.__reports)
+            ]
+
+        return SimpleNamespace(metadata=metadata, get=get)
 
 
 class TestMetadata:
-    @fixture(autouse=True)
-    def monkeypatch_client(self, monkeypatch):
-        monkeypatch.setattr(
-            "skore_remote_project.project.metadata.AuthenticatedClient",
-            FakeClient,
-        )
-
-    @fixture(scope="class")
-    def regression(self):
-        from sklearn.datasets import make_regression
-        from sklearn.linear_model import LinearRegression
-        from sklearn.model_selection import train_test_split
-        from skore import EstimatorReport
-
-        X, y = make_regression()
-        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-        return EstimatorReport(
-            LinearRegression(),
-            X_train=X_train,
-            y_train=y_train,
-            X_test=X_test,
-            y_test=y_test,
-        )
-
-    @fixture(scope="class")
-    def binary_classification(self):
-        from sklearn.datasets import make_classification
-        from sklearn.linear_model import LogisticRegression
-        from sklearn.model_selection import train_test_split
-        from skore import EstimatorReport
-
-        X, y = make_classification()
-        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-        return EstimatorReport(
-            LogisticRegression(),
-            X_train=X_train,
-            y_train=y_train,
-            X_test=X_test,
-            y_test=y_test,
-        )
-
-    @mark.respx(assert_all_called=True)
-    def test_factory(self, now, respx_mock, regression, binary_classification):
-        project = Namespace(tenant="<tenant>", name="<name>")
-        url = "projects/<tenant>/<name>/experiments/estimator-reports"
-
-        respx_mock.get(url).mock(
-            Response(
-                200,
-                json=[
-                    dict(
-                        (
-                            *SkoreEstimatorReportItemMetadata(regression),
-                            ("id", 0),
-                            ("run_id", 0),
-                            ("created_at", now.isoformat()),
-                        ),
-                    ),
-                    dict(
-                        (
-                            *SkoreEstimatorReportItemMetadata(binary_classification),
-                            ("id", 1),
-                            ("run_id", 1),
-                            ("created_at", (now + timedelta(1)).isoformat()),
-                        ),
-                    ),
-                ],
-            )
-        )
-
+    def test_factory(self, regression, binary_classification):
+        project = FakeProject(regression, binary_classification)
         metadata = Metadata.factory(project)
 
         assert isinstance(metadata, DataFrame)
@@ -120,142 +90,64 @@ class TestMetadata:
         )
         assert list(metadata.columns) == [
             "run_id",
-            "ml_task",
+            "key",
+            "date",
             "learner",
             "dataset",
-            "date",
-            "r2",
+            "ml_task",
             "rmse",
-            "fit_time",
-            "predict_time_test",
-            "accuracy",
-            "brier_score",
             "log_loss",
             "roc_auc",
+            "fit_time",
+            "predict_time",
         ]
 
-    @mark.respx(assert_all_called=True)
-    def test_constructor(self, now, respx_mock, regression):
-        project = Namespace(tenant="<tenant>", name="<name>")
-        url = "projects/<tenant>/<name>/experiments/estimator-reports"
-
-        respx_mock.get(url).mock(
-            Response(
-                200,
-                json=[
-                    dict(
-                        (
-                            *SkoreEstimatorReportItemMetadata(regression),
-                            ("id", 0),
-                            ("run_id", 0),
-                            ("created_at", now.isoformat()),
-                        ),
-                    ),
-                ],
-            )
-        )
-
+    def test_constructor(self, regression):
+        project = FakeProject(regression)
         metadata = Metadata.factory(project)
-        metadata2 = metadata.query("run_id==1")
 
-        assert not DataFrame.equals(metadata2, metadata)
+        # Test with a bad query, with empty result
+        metadata2 = metadata.query("ml_task=='<ml_task>'")
+
         assert isinstance(metadata2, DataFrame)
         assert isinstance(metadata2, Metadata)
+        assert len(metadata2) == 0
         assert metadata2.project == project
 
-    @mark.respx(assert_all_called=True)
-    def test_reports_with_filter(
-        self, monkeypatch, now, respx_mock, regression, binary_classification
-    ):
-        project = Namespace(tenant="<tenant>", name="<name>")
-        item = SkoreEstimatorReportItem.factory(regression)
-        url = "projects/<tenant>/<name>/experiments/estimator-reports"
+        # Test with a valid query, with identical result
+        metadata3 = metadata.query("ml_task=='regression'")
 
-        respx_mock.get(url).mock(
-            Response(
-                200,
-                json=[
-                    dict(
-                        (
-                            *SkoreEstimatorReportItemMetadata(regression),
-                            ("id", 0),
-                            ("run_id", 0),
-                            ("created_at", now.isoformat()),
-                        ),
-                    ),
-                    dict(
-                        (
-                            *SkoreEstimatorReportItemMetadata(binary_classification),
-                            ("id", 1),
-                            ("run_id", 1),
-                            ("created_at", (now + timedelta(1)).isoformat()),
-                        ),
-                    ),
-                ],
-            )
-        )
+        assert isinstance(metadata3, DataFrame)
+        assert isinstance(metadata3, Metadata)
+        assert DataFrame.equals(metadata3, metadata)
+        assert metadata3.project == project
 
-        url = "projects/<tenant>/<name>/experiments/estimator-reports/0"
+    def test_reports_with_filter(self, monkeypatch, regression, binary_classification):
+        project = FakeProject(regression, binary_classification)
+        metadata = Metadata.factory(project)
 
-        respx_mock.get(url).mock(
-            Response(
-                200,
-                json={"raw": item.__parameters__["parameters"]},
-            )
-        )
+        assert len(metadata) == 2
+        assert metadata.reports() == [regression, binary_classification]
 
         monkeypatch.setattr(
-            "skore_remote_project.project.metadata.Metadata.query_string_selection",
+            "skore.project.metadata.Metadata.query_string_selection",
             lambda self: "ml_task == 'regression'",
         )
 
+        assert metadata.reports() == [regression]
+
+    def test_reports_without_filter(
+        self, monkeypatch, regression, binary_classification
+    ):
+        project = FakeProject(regression, binary_classification)
         metadata = Metadata.factory(project)
-        reports = metadata.reports()
 
         assert len(metadata) == 2
-        assert isinstance(reports, list)
-        assert len(reports) == 1
-        assert type(reports[0]) is type(regression)
-        assert reports[0].estimator_name_ == regression.estimator_name_
-        assert reports[0].ml_task == regression.ml_task
+        assert metadata.reports(filter=False) == [regression, binary_classification]
 
-    @mark.respx(assert_all_called=True)
-    def test_reports_without_filter(self, now, respx_mock, regression):
-        project = Namespace(tenant="<tenant>", name="<name>")
-        item = SkoreEstimatorReportItem.factory(regression)
-        url = "projects/<tenant>/<name>/experiments/estimator-reports"
-
-        respx_mock.get(url).mock(
-            Response(
-                200,
-                json=[
-                    dict(
-                        (
-                            *SkoreEstimatorReportItemMetadata(regression),
-                            ("id", 0),
-                            ("run_id", 0),
-                            ("created_at", now.isoformat()),
-                        ),
-                    ),
-                ],
-            )
+        monkeypatch.setattr(
+            "skore.project.metadata.Metadata.query_string_selection",
+            lambda self: "ml_task == 'regression'",
         )
 
-        url = "projects/<tenant>/<name>/experiments/estimator-reports/0"
-
-        respx_mock.get(url).mock(
-            Response(
-                200,
-                json={"raw": item.__parameters__["parameters"]},
-            )
-        )
-
-        metadata = Metadata.factory(project)
-        reports = metadata.reports(filter=False)
-
-        assert len(metadata) == 1
-        assert isinstance(reports, list)
-        assert len(reports) == 1
-        assert type(reports[0]) is type(regression)
-        assert reports[0].estimator_name_ == regression.estimator_name_
-        assert reports[0].ml_task == regression.ml_task
+        assert metadata.reports(filter=False) == [regression, binary_classification]
