81dbe07c01475ee16d1f5e6e2e231c6682ef75c7
Guillaume Lemaitre <guillaume@probabl.ai>
2025-05-21 23:08:31 +0200

    linter

--- skore/src/skore/sklearn/_base.py
+++ skore/src/skore/sklearn/_base.py
@@ -333,10 +333,10 @@ def _get_cached_response_values(
 ) -> list[tuple[tuple[Any, ...], Any, bool]]:
     """Compute or load from local cache the response values.
 
-    Be aware that the predictions will be loaded from the cache if present, but they will not be added
-    to it. The reason is that we want to be able to run this function in parallel
-    settings in a thread-safe manner. The update should be done outside of this
-    function.
+    Be aware that the predictions will be loaded from the cache if present, but they
+    will not be added to it. The reason is that we want to be able to run this function
+    in parallel settings in a thread-safe manner. The update should be done outside of
+    this function.
 
     Parameters
     ----------
