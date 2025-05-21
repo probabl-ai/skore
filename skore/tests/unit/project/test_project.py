56d0a2a2a101e5af3c193fdb3ade3cc6ff3ee4cb
Thomas S <thomas@probabl.ai>
2025-05-21 10:19:26 +0200

    Improve Project's tests

--- skore/tests/unit/project/test_project.py
+++ skore/tests/unit/project/test_project.py
@@ -1,7 +1,12 @@
 from sys import version_info
+from types import SimpleNamespace
+from unittest.mock import Mock
 
 from pytest import fixture, raises
-from skore import Project
+from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from skore import EstimatorReport, Project
 
 if version_info < (3, 10):
     from importlib_metadata import EntryPoint, EntryPoints
@@ -9,11 +14,14 @@ else:
     from importlib.metadata import EntryPoint, EntryPoints
 
 
-class FakeLocalProject:
-    def __init__(self, *args, **kwargs): ...
+class FakeLocalProject(Mock):
+    def __init__(self, *args, **kwargs):
+        super().__init__(constructor_args=args, constructor_kwargs=kwargs)
 
 
-FakeHubProject = FakeLocalProject
+class FakeHubProject(Mock):
+    def __init__(self, *args, **kwargs):
+        super().__init__(constructor_args=args, constructor_kwargs=kwargs)
 
 
 class TestProject:
@@ -38,21 +46,43 @@ class TestProject:
             "skore.project.project.entry_points", lambda **kwargs: entrypoints
         )
 
+    @fixture(scope="class")
+    def regression(self):
+        X, y = make_regression(random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
+
+        return EstimatorReport(
+            LinearRegression(),
+            X_train=X_train,
+            y_train=y_train,
+            X_test=X_test,
+            y_test=y_test,
+        )
+
     def test_init_local(self):
         project = Project("<name>")
 
         assert isinstance(project, Project)
-        assert isinstance(project._Project__project, FakeLocalProject)
         assert project._Project__mode == "local"
         assert project._Project__name == "<name>"
+        assert isinstance(project._Project__project, FakeLocalProject)
+        assert not project._Project__project.constructor_args
+        assert project._Project__project.constructor_kwargs == {"name": "<name>"}
 
     def test_init_hub(self):
         project = Project("hub://<tenant>/<name>")
 
         assert isinstance(project, Project)
-        assert isinstance(project._Project__project, FakeHubProject)
         assert project._Project__mode == "hub"
         assert project._Project__name == "<name>"
+        assert isinstance(project._Project__project, FakeHubProject)
+        assert not project._Project__project.constructor_args
+        assert project._Project__project.constructor_kwargs == {
+            "tenant": "<tenant>",
+            "name": "<name>",
+        }
 
     def test_init_exception_no_plugin(self, monkeypatch, tmp_path):
         monkeypatch.undo()
@@ -95,11 +125,32 @@ class TestProject:
         assert Project("<name>").name == "<name>"
         assert Project("hub://<tenant>/<name>").name == "<name>"
 
-    def test_put(self): ...
+    def test_put(self, regression):
+        project = Project("<name>")
 
-    def test_put_exception(self): ...
+        project.put("<key>", regression)
 
-    def test_reports(self): ...
+        assert isinstance(project._Project__project, FakeLocalProject)
+        assert project._Project__project.put.called
+        assert not project._Project__project.put.call_args.args
+        assert project._Project__project.put.call_args.kwargs == {
+            "key": "<key>",
+            "report": regression,
+        }
+
+    def test_put_exception(self):
+        with raises(TypeError, match="Key must be a string"):
+            Project("<name>").put(None, "<value>")
+
+        with raises(TypeError, match="Report must be a `skore.EstimatorReport`"):
+            Project("<name>").put("<key>", "<value>")
+
+    def test_reports(self):
+        project = Project("<name>")
+
+        assert isinstance(project.reports, SimpleNamespace)
+        assert hasattr(project.reports, "get")
+        assert hasattr(project.reports, "metadata")
 
     def test_reports_get(self): ...
 
