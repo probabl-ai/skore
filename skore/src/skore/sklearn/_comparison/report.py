6e3475cd7598ddb66be23189180d7c4dc3397827
pauline-banye <paulinebanye@gmail.com>
2025-05-21 21:43:36 +0100

    pull latest changes from upstream main

--- skore/src/skore/sklearn/_comparison/report.py
+++ skore/src/skore/sklearn/_comparison/report.py
@@ -27,12 +27,11 @@ class ComparisonReport(_BaseReport, DirNamesMixin):
     This object can be used to compare several :class:`skore.EstimatorReport` instances,
     or several :class:`~skore.CrossValidationReport` instances.
 
-    .. caution::
-        Reports passed to `ComparisonReport` are not copied. If you pass a report
-        to `ComparisonReport`, and then modify the report outside later, it will
-        affect the report stored inside the `ComparisonReport` as well, which can
-        lead to inconsistent results. For this reason, modifying reports after
-        creation is strongly discouraged.
+    .. caution:: Reports passed to `ComparisonReport` are not copied. If you pass
+       a report to `ComparisonReport`, and then modify the report outside later, it
+       will affect the report stored inside the `ComparisonReport` as well, which
+       can lead to inconsistent results. For this reason, modifying reports after
+       creation is strongly discouraged.
 
     Parameters
     ----------
