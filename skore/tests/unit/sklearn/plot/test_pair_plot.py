37ddfd97b4dcc19770e0795660c6beeed7b01ee7
Marie <marie.sacksick@posteo.net>
2025-05-23 10:57:23 +0200

    fix tests

--- skore/tests/unit/sklearn/plot/test_pair_plot.py
+++ skore/tests/unit/sklearn/plot/test_pair_plot.py
@@ -2,7 +2,8 @@ import pytest
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from skore import EstimatorReport
+from sklearn.tree import DecisionTreeClassifier
+from skore import ComparisonReport, EstimatorReport
 from skore.sklearn._plot import PairPlotDisplay
 
 
@@ -11,44 +12,6 @@ def binary_classification_data():
     X, y = make_classification(class_sep=0.1, random_state=42)
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
     return (
-        LogisticRegression().fit(X_train, y_train),
-        X_train,
-        X_test,
-        y_train,
-        y_test,
-    )
-
-
-@pytest.fixture
-def multiclass_classification_data():
-    X, y = make_classification(
-        class_sep=0.1,
-        n_classes=3,
-        n_clusters_per_class=1,
-        random_state=42,
-    )
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    return (
-        LogisticRegression().fit(X_train, y_train),
-        X_train,
-        X_test,
-        y_train,
-        y_test,
-    )
-
-
-@pytest.fixture
-def regression_data():
-    X, y = make_classification(
-        n_samples=100,
-        n_features=10,
-        n_informative=5,
-        n_redundant=2,
-        random_state=42,
-    )
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    return (
-        LogisticRegression().fit(X_train, y_train),
         X_train,
         X_test,
         y_train,
@@ -60,15 +23,27 @@ def test_pairplot_test_set(
     pyplot,
     binary_classification_data,
 ):
-    estimator, X_train, X_test, y_train, y_test = binary_classification_data
-    report = EstimatorReport(
-        estimator,
+    X_train, X_test, y_train, y_test = binary_classification_data
+    est_1 = LogisticRegression()
+    est_2 = DecisionTreeClassifier()
+    report1 = EstimatorReport(
+        est_1,
         X_train=X_train,
         y_train=y_train,
         X_test=X_test,
         y_test=y_test,
     )
-    display = report.metrics.pairwise_plot(
+    report2 = EstimatorReport(
+        est_2,
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
+    comparison = ComparisonReport(
+        {"Logistic Regression": report1, "Decision Tree": report2}
+    )
+    display = comparison.pairwise_plot(
         perf_metric_x="fit_time",
         perf_metric_y="predict_time",
         data_source="train",
@@ -85,16 +60,28 @@ def test_pairplot_train_set(
     pyplot,
     binary_classification_data,
 ):
-    estimator, X_train, X_test, y_train, y_test = binary_classification_data
-    report = EstimatorReport(
-        estimator,
+    X_train, X_test, y_train, y_test = binary_classification_data
+    est_1 = LogisticRegression()
+    est_2 = DecisionTreeClassifier()
+    report1 = EstimatorReport(
+        est_1,
         X_train=X_train,
         y_train=y_train,
         X_test=X_test,
         y_test=y_test,
     )
+    report2 = EstimatorReport(
+        est_2,
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
+    comparison = ComparisonReport(
+        {"Logistic Regression": report1, "Decision Tree": report2}
+    )
 
-    display = report.metrics.pairwise_plot(
+    display = comparison.pairwise_plot(
         perf_metric_x="fit_time",
         perf_metric_y="roc_auc",
         data_source="train",
@@ -112,17 +99,29 @@ def test_pairplot_missing_col(
     pyplot,
     binary_classification_data,
 ):
-    estimator, X_train, X_test, y_train, y_test = binary_classification_data
-    report = EstimatorReport(
-        estimator,
+    X_train, X_test, y_train, y_test = binary_classification_data
+    est_1 = LogisticRegression()
+    est_2 = DecisionTreeClassifier()
+    report1 = EstimatorReport(
+        est_1,
         X_train=X_train,
         y_train=y_train,
         X_test=X_test,
         y_test=y_test,
     )
+    report2 = EstimatorReport(
+        est_2,
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
+    comparison = ComparisonReport(
+        {"Logistic Regression": report1, "Decision Tree": report2}
+    )
 
     with pytest.raises(ValueError):
-        report.metrics.pairwise_plot(
+        comparison.pairwise_plot(
             perf_metric_x="fit_time",
             perf_metric_y="an_invented_column",
             data_source="train",
@@ -134,17 +133,29 @@ def test_pairplot_missing_pos_label(
     pyplot,
     binary_classification_data,
 ):
-    estimator, X_train, X_test, y_train, y_test = binary_classification_data
-    report = EstimatorReport(
-        estimator,
+    X_train, X_test, y_train, y_test = binary_classification_data
+    est_1 = LogisticRegression()
+    est_2 = DecisionTreeClassifier()
+    report1 = EstimatorReport(
+        est_1,
         X_train=X_train,
         y_train=y_train,
         X_test=X_test,
         y_test=y_test,
     )
+    report2 = EstimatorReport(
+        est_2,
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
+    comparison = ComparisonReport(
+        {"Logistic Regression": report1, "Decision Tree": report2}
+    )
 
     with pytest.raises(ValueError):
-        report.metrics.pairwise_plot(
+        comparison.pairwise_plot(
             perf_metric_x="fit_time",
             perf_metric_y="recall",
             data_source="train",
