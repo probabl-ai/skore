5a0f94d661704ed0374538325694efde7d47b40f
Sylvain Combettes <48064216+sylvaincom@users.noreply.github.com>
2025-05-27 11:01:28 +0200

    Update examples/getting_started/plot_skore_getting_started.py
    Co-authored-by: Marie Sacksick <79304610+MarieSacksick@users.noreply.github.com>

--- examples/getting_started/plot_skore_getting_started.py
+++ examples/getting_started/plot_skore_getting_started.py
@@ -265,7 +265,7 @@ _ = skore.train_test_split(
 # =======================
 #
 # Another key feature of skore is its :class:`~skore.Project` that allows us to store
-# and retrieve skore reports.
+# and retrieve EstimatorReports.
 
 # %%
 # Setup: creating and loading a skore project
