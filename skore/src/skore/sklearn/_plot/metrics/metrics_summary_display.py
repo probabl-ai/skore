86d02b89004d333d99effed07a0d342c10217dfe
Marie <marie.sacksick@posteo.net>
2025-06-11 15:40:01 +0200

    introduce temporarily _SCORE_OR_LOSS_INFO in class

--- skore/src/skore/sklearn/_plot/metrics/metrics_summary_display.py
+++ skore/src/skore/sklearn/_plot/metrics/metrics_summary_display.py
@@ -12,6 +12,22 @@ class MetricsSummaryDisplay(HelpDisplayMixin, StyleDisplayMixin):
     This class should not be instantiated directly.
     """
 
+    # should be removed once transformed into a utils
+    _SCORE_OR_LOSS_INFO: dict[str, dict[str, str]] = {
+        "fit_time": {"name": "Fit time (s)", "icon": "(↘︎)"},
+        "predict_time": {"name": "Predict time (s)", "icon": "(↘︎)"},
+        "accuracy": {"name": "Accuracy", "icon": "(↗︎)"},
+        "precision": {"name": "Precision", "icon": "(↗︎)"},
+        "recall": {"name": "Recall", "icon": "(↗︎)"},
+        "brier_score": {"name": "Brier score", "icon": "(↘︎)"},
+        "roc_auc": {"name": "ROC AUC", "icon": "(↗︎)"},
+        "log_loss": {"name": "Log loss", "icon": "(↘︎)"},
+        "r2": {"name": "R²", "icon": "(↗︎)"},
+        "rmse": {"name": "RMSE", "icon": "(↘︎)"},
+        "custom_metric": {"name": "Custom metric", "icon": ""},
+        "report_metrics": {"name": "Report metrics", "icon": ""},
+    }
+
     def __init__(
         self, *, summarize_data, report_type: ReportType, data_source: str = "test"
     ):
