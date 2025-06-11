953106a865dda1e3b51c0d450a0a46ec699cc517
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2025-06-11 13:11:08 +0400

    fix(train_test_split): improved error msg for as_dict=True when pos args>2
    Co-authored-by: Auguste Baum <auguste@probabl.ai>

--- skore/src/skore/sklearn/train_test_split/train_test_split.py
+++ skore/src/skore/sklearn/train_test_split/train_test_split.py
@@ -176,8 +176,10 @@ def train_test_split(
             keys.extend(["X", "y"])
         elif len(arrays) > 2:
             raise ValueError(
-                "With as_dict=True, more than two arguments must be keyword arguments."
-                "\nEx: train_test_split(X=X, y=y, z=z, sw=sample_weight, as_dict=True)"
+                "With as_dict=True, expected no more than two positional arguments "
+                "(which will be interpreted as X and y). "
+                "The remaining arrays must be passed by keyword, "
+                "e.g. train_test_split(X, y, z=z, sw=sample_weights, as_dict=True)."
             )
 
     if keyword_arrays:
