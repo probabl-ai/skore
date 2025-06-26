ed24cb1db1302355a1de20f5419fd91908283ead
Sylvain Combettes <48064216+sylvaincom@users.noreply.github.com>
2025-06-26 11:26:31 +0200

    Updating the current file given the new scope

--- examples/use_cases/plot_example_skorch.py
+++ examples/use_cases/plot_example_skorch.py
@@ -1,45 +1,66 @@
 """
-.. _example_use_case_skorch:
+.. _example_sklearn_api:
 
-====================================================
-Using skore with the skorch deep learning framework
-====================================================
+===========================================================
+Using skore for models compatible with the scikit-learn API
+===========================================================
 
-This example shows how to leverage skore's capabilities with a deep learning framework like skorch.
+This example shows how to leverage skore's capabilities with scikit-learn compatible
+estimators, including:
+
+- libraries like ``xgboost``, ``catboost``, and ``lightgbm``,
+- deep learning frameworks such as ``skorch`` (a wrapper for PyTorch) and ``keras``,
+- foundation models such as `TabICL <https://github.com/soda-inria/tabicl>`_ and
+  `TabPFN <https://github.com/PriorLabs/TabPFN>`_.
 """
 
 # %%
-# Loading the Breast Cancer Winconsin (Diagnostic) Dataset
-# ========================================================
-# In this example, we tackle the Breast Cancer Winconsin dataset where the goal is a binary classification task, i.e. predicting whether a tumor is malignant or benign.
-# We shall first perform some preprocessing using `skrub` and then create a shallow neural network to demonstrate `skore`'s ability to support deep learning frameworks such as `skorch` which is a `sklearn`-like API over `PyTorch`.
+# Loading a binary classification dataset
+# =======================================
+#
+# In this example, we tackle the Breast Cancer Winconsin dataset where the goal is a
+# binary classification task, i.e. predicting whether a tumor is malignant or benign.
+# We use this dataset to keep this example simple.
 
 # %%
 from sklearn.datasets import load_breast_cancer
 
-cancer = load_breast_cancer(as_frame=True)
-cancer.frame.head(2)
+X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+X.head(2)
 
 # %%
-# The `documentation <https://sklearn.org/stable/modules/generated/sklearn.datasets.load_breast_cancer.html>`_ shows that there are 2 classes `malignant` (M) and `benign` (B) with a slight imbalance 212 `M` samples and 357 `B` samples. Before we approach the balancing problem, let us explore the dataset for any require preprocessing.
-# We shall explore the dataset using `skrub` library's `TableReport`.
+# The `documentation <https://sklearn.org/stable/modules/generated/sklearn.datasets.load_breast_cancer.html>`_
+# shows that there are 2 classes `malignant` (M) and `benign` (B) with a slight
+# imbalance 212 `M` samples and 357 `B` samples.
+# Before we approach the balancing problem, let us explore the dataset.
 #
-# Table report
-# ============
+# Exploratory data analysis
+# -------------------------
+#
+# We shall explore the dataset using `skrub` library's `TableReport`.
 
 # %%
 from skrub import TableReport
 
-TableReport(cancer.frame, max_plot_columns=31)
+TableReport(X)
 
 # %%
-# Table report inferences
-# -----------------------
-#
+import pandas as pd
+
+TableReport(pd.DataFrame(y))
+
+# %%
+pd.DataFrame(y).value_counts(normalize=True).round(2)
+
+# %%
+# Preprocessing
+# -------------
+
+# %%
 # From the table report, we can make a few inferences:
 #
 # - The *Stats* tab shows there are no null values.
-# - The *Distribution* tab shows us there is moderate level of imbalance: 62.7% benign from the mean value and 37.3% malignant values. While we can balance this or add class weights in our neural network, it is important to note that we're modeling the real world
+# - The *Distribution* tab shows us there is moderate level of imbalance: 63% benign from the mean value and 37% malignant values. While we can balance this or add class weights in our neural network, it is important to note that we're modeling the real world
 #   and not to achieve an artificial balance!
 # - The *Distribution* tab shows a few features that have some outliers, namely: ``radius error``, ``texture error``, ``perimeter error``, ``area error``, ``smoothness error``, ``compactness error``, ``concavity error``, ``concave points error``, ``symmetry error``, ``fractal dimension error``, ``worst area``, ``worst symmetry``, ``worst fractal dimensions``.
 #   However, it is important to note that the outliers range from 1-8 in all the different columns which is not huge to cause problems in our modeling.
@@ -62,34 +83,41 @@ cols_to_keep = [
     "mean concave points",
     "worst concavity",
     "mean concavity",
-    "target",
 ]
 
-dataset = cancer.frame[cols_to_keep]
-X, y = (
-    dataset.drop("target", axis=1).values.astype(np.float32),
-    dataset["target"].values,
-)
-dataset.head(2)
+X = X[cols_to_keep]
+X_numpy = X.values.astype(np.float32)
 
 # %%
 # Splitting the data
-# ==================
+# ------------------
+#
 # We shall split the data using `skore`'s `train_test_split`.
 
 # %%
 from skore import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+X_train_numpy, X_test_numpy, y_train, y_test = train_test_split(
+    X_numpy, y, random_state=42
+)
 
 # %%
 # We can see how `skore` gives us 2 warnings on split to help us with our modeling approach.
 #
 # - Adhering to the `HighClassImbalanceTooFewExamplesWarning`, we shall employ cross validation in our modeling approach.
 # - The `ShuffleTrueWarning` can be ignored as there are no temporal dependencies in our medical dataset. Each example is IID.
-# Creating model using PyTorch
-# ----------------------------
-# We shall create our neural network model using PyTorch. We consider a neural network with 2 hidden layers and 1 output layer with ReLU activations.
+
+# %%
+# Deep learning with neural networks
+# ==================================
+#
+# PyTorch using skorch
+# --------------------
+#
+# We shall create our neural network model using PyTorch.
+# We consider a neural network with 2 hidden layers and 1 output layer with ReLU
+# activations.
 
 # %%
 from torch import nn
@@ -111,7 +139,8 @@ class MyNeuralNet(nn.Module):
 
 
 # %%
-# Since we want to use this with `skorch` that provides a sklearn like API interface that `skore` can utilize, we shall wrap this in `skorch`'s NeuralNetClassifier.
+# Since we want to use this with `skorch` that provides a sklearn like API interface
+# that `skore` can utilize, we shall wrap this in `skorch`'s NeuralNetClassifier.
 
 # %%
 import torch
@@ -138,8 +167,6 @@ model = make_pipeline(StandardScaler(), nnet)
 model
 
 # %%
-# Model development using `CrossValidationReport`
-# -----------------------------------------------
 # We use our X_train and y_train set using some specific set of hyperparameters to evaluate our hyperparameter selection. For the sake of example, we do not test against multiple hyperparameters but a single one.
 #
 # You may want to look into `ComparisonReport` that can allow you to compare multiple `CrossValidationReport` instances.
@@ -147,25 +174,32 @@ model
 # %%
 from skore import CrossValidationReport
 
-cv_report = CrossValidationReport(estimator=model, X=X_train, y=y_train, cv_splitter=3)
+cv_report = CrossValidationReport(
+    estimator=model, X=X_train_numpy, y=y_train, cv_splitter=3
+)
 
 # %%
 # We can observe the different ROC curves and cumulative scores across different folds for our binary classification task.
 
 # %%
 cv_report.metrics.roc().plot()
-cv_report.metrics.report_metrics(aggregate=["mean", "std"], indicator_favorability=True)
+cv_report.metrics.summarize(
+    aggregate=["mean", "std"], indicator_favorability=True
+).frame()
 
 # %%
-# Model evaluation using `EstimatorReport`
-# ----------------------------------------
 # Once we have picked the best hyperparmeter configuration, we can create a EstimatorReport with a new model object having best hyperparameter settings along with the test sets to get an evaluation report.
 
 # %%
 from skore import EstimatorReport
 
 report = EstimatorReport(
-    model, fit=True, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
+    model,
+    fit=True,
+    X_train=X_train_numpy,
+    y_train=y_train,
+    X_test=X_test_numpy,
+    y_test=y_test,
 )
 
 # %%
@@ -173,4 +207,6 @@ report = EstimatorReport(
 
 # %%
 report.metrics.roc().plot()
-report.metrics.report_metrics(indicator_favorability=True)
+report.metrics.summarize(indicator_favorability=True).frame()
+
+# %%
