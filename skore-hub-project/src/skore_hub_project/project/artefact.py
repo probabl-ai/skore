37a3d02d96c3ed3c42232967332827d0b4c0d853
Thomas S <thomas@probabl.ai>
2025-07-22 15:44:03 +0200

    WIP - Refactor docstr part 2

--- skore-hub-project/src/skore_hub_project/project/artefact.py
+++ skore-hub-project/src/skore_hub_project/project/artefact.py
@@ -1,3 +1,5 @@
+"""Function definition of the artefact ``upload``."""
+
 from __future__ import annotations
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -38,7 +40,7 @@ Progress = partial(
 
 
 class Serializer:
-    """Serialize an object using ``joblib``, on disk to avoid RAM overhead."""
+    """Serialize an object using ``joblib``, on disk to reduce RAM footprint."""
 
     def __init__(self, o: Any):
         with self.filepath.open("wb") as file:
@@ -58,7 +60,18 @@ class Serializer:
 
     @cached_property
     def checksum(self) -> str:
-        """The checksum of the serialized object."""
+        """
+        The checksum of the serialized object.
+
+        Notes
+        -----
+        Depending on the size of the serialized object, the checksum can be computed on
+        one or more threads:
+
+            Note that this can be slower for inputs shorter than ~1 MB
+
+        https://github.com/oconnor663/blake3-py
+        """
         hasher = Blake3(max_threads=(1 if self.size < 1e6 else Blake3.AUTO))
         checksum = hasher.update_mmap(self.filepath).digest()
 
@@ -77,7 +90,32 @@ def upload_chunk(
     offset: int,
     length: int,
 ) -> str:
-    """"""
+    """
+    Upload a chunk of the serialized object to the artefacts storage.
+
+    Parameters
+    ----------
+    filepath : ``Path``
+        The path of the file containing the serialized object.
+    client : ``httpx.Client``
+        The client used to upload the chunk to the artefacts storage.
+    url : str
+        The url used to upload the chunk to the artefacts storage.
+    offset : int
+        The start of the chunk in the file containing the serialized object.
+    length: int
+        The length of the chunk in the file containing the serialized object.
+
+    Returns
+    -------
+    etag : str
+        The ETag assigned by the artefacts storage to the chunk, used to acknowledge the
+        upload.
+
+    Notes
+    -----
+    This function is in charge of reading its own chunk to reduce RAM footprint.
+    """
     with filepath.open("rb") as file:
         file.seek(offset)
 
@@ -92,7 +130,31 @@ def upload_chunk(
 
 
 def upload(project: Project, o: Any, type: str, chunk_size: int) -> str:
-    """"""
+    """
+    Upload an object to the artefacts storage.
+
+    Parameters
+    ----------
+    project : ``Project``
+        The project where to upload the object.
+    o : Any
+        The object to upload.
+    type : str
+        The type to associate to object in the artefacts storage.
+    chunk_size : int
+        The size of the object not to be exceeded before to be uploaded in chunks.
+        The size of the chunks to upload.
+
+    Returns
+    -------
+    checksum : str
+        The checksum of the object after upload to the artefacts storage, based on its
+        ``joblib`` serialization.
+
+    Notes
+    -----
+    An object that was already uploaded in its whole will be ignored.
+    """
     with (
         Serializer(o) as serializer,
         AuthenticatedClient(raises=True) as authenticated_client,
@@ -115,11 +177,11 @@ def upload(project: Project, o: Any, type: str, chunk_size: int) -> str:
         if urls := response.json():
             task_to_chunk_id = {}
 
-            # Upload each chunk of the pickled report to the artefacts storage, using
+            # Upload each chunk of the serialized object to the artefacts storage, using
             # disk temporary file.
             #
-            # Each task is in charge of reading its own file chunk at runtime, to avoid
-            # RAM overhead.
+            # Each task is in charge of reading its own file chunk at runtime, to reduce
+            # RAM footprint.
             #
             # Use `threading` over `asyncio` to ensure compatibility with Jupyter
             # notebooks, where the event loop is already running.
