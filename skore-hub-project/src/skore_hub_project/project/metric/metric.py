bc1f7fffe5b9316ea313458425ad768e712e6538
Thomas S <thomas@probabl.ai>
2025-07-29 18:03:37 +0200

    [ci skip] WIP - Add metrics

--- skore-hub-project/src/skore_hub_project/project/metric/metric.py
+++ skore-hub-project/src/skore_hub_project/project/metric/metric.py
@@ -1,23 +1,22 @@
-from dataclasses import dataclass, KW_ONLY, field, InitVar
-from typing import Any, Callable, Literal
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import InitVar, dataclass, field
 from functools import partial
+from typing import Any, Literal, TYPE_CHECKING
 
 
-def call(report, accessor, **kwargs) -> float | None:
-    if hasattr(report, accessor):
-        value = getattr(report, accessor)(**kwargs)
+dataclass = partial(dataclass, kw_only=True)
 
-        with suppress(TypeError):
-            if isfinite(value := float(value)):
-                return value
+if TYPE_CHECKING:
+    from skore import EstimatorReport, CrossValidationReport
 
-    return None
+    Report = EstimatorReport | CrossValidationReport
 
 
-@dataclass(kw_only=True)
-class Metric:
+@dataclass
+class Metric(ABC):
     report: InitVar[Report]
-    function: InitVar[Callable[[EstimatorReport], float]]
     value: float | None = field(init=False)
     name: str
     verbose_name: str
@@ -25,94 +24,83 @@ class Metric:
     greater_is_better: bool
     position: int | None = None
 
-    def __post_init__(
-        self, report: EstimatorReport, function: Callable[[Report], float]
-    ):
-        self.value = function(report)  # or None
+    @abstractmethod
+    def __post_init__(self, report: Report): ...
+
+    @property.setter
+    def value(self, value: Any):
+        with suppress(TypeError):
+            if isfinite(value := float(value)):
+                return value
+
+        return None
 
 
-@dataclass(kw_only=True)
-class Accuracy(Metric):
+@dataclass
+class Accuracy(ABC, Metric):
     name: Final[str] = "accuracy"
     verbose_name: Final[str] = "Accuracy"
     greater_is_better: Final[str] = True
 
+    def __post_init__(self, report: EstimatorReport):
+        self.value = (
+            report.metrics.accuracy(data_source=self.data_source)
+            if hasattr(report, "metrics.accuracy")
+            else None
+        )
 
-@dataclass(kw_only=True)
+
+@dataclass
 class AccuracyTrain(Metric):
     data_source: Final[str] = "train"
-    function: InitVar[Final[Callable[[EstimatorReport], float]]] = partial(
-        call,
-        accessor="metrics.accuracy",
-        kwargs={"data_source": "train"},
-    )
 
 
-@dataclass(kw_only=True)
+@dataclass
 class AccuracyTest(Metric):
     data_source: Final[str] = "test"
-    function: InitVar[Final[Callable[[EstimatorReport], float]]] = partial(
-        call,
-        accessor="metrics.accuracy",
-        kwargs={"data_source": "test"},
-    )
 
 
-@dataclass(kw_only=True)
-class AccuracyMean(Metric):
-    @staticmethod
-    def compute(report, data_source) -> float:
-        df = report.metrics.accuracy(data_source=data_source)
-        series = df[(report.estimator_name_, "mean")]
-
-        return float(series.iloc[0])
-
+@dataclass
+class AccuracyMean(ABC, Metric):
     name: Final[str] = "accuracy_mean"
     verbose_name: Final[str] = "Accuracy - MEAN"
     greater_is_better: Final[str] = True
 
+    def __post_init__(self, report: CrossValidationReport):
+        df = report.metrics.accuracy(data_source=self.data_source)
+        series = df[(report.estimator_name_, "mean")]
 
-@dataclass(kw_only=True)
+        self.value = series.iloc[0]
+
+
+@dataclass
 class AccuracyTrainMean(Metric):
     data_source: Final[str] = "train"
-    function: InitVar[Final[Callable[[CrossValidationReport], float]]] = partial(
-        AccuracyMean.compute, data_source="train"
-    )
 
 
-@dataclass(kw_only=True)
+@dataclass
 class AccuracyTestMean(Metric):
     data_source: Final[str] = "test"
-    function: InitVar[Final[Callable[[CrossValidationReport], float]]] = partial(
-        AccuracyMean.compute, data_source="test"
-    )
 
 
-@dataclass(kw_only=True)
-class AccuracySTD(Metric):
-    @staticmethod
-    def compute(report, data_source) -> float:
-        df = report.metrics.accuracy(data_source=data_source)
-        series = df[(report.estimator_name_, "std")]
-
-        return float(series.iloc[0])
-
+@dataclass
+class AccuracySTD(ABC, Metric):
     name: Final[str] = "accuracy_std"
     verbose_name: Final[str] = "Accuracy - STD"
     greater_is_better: Final[str] = False
 
+    def __post_init__(self, report: CrossValidationReport):
+        df = report.metrics.accuracy(data_source=self.data_source)
+        series = df[(report.estimator_name_, "std")]
 
-@dataclass(kw_only=True)
+        self.value = series.iloc[0]
+
+
+@dataclass
 class AccuracyTrainSTD(Metric):
     data_source: Final[str] = "train"
-    function: InitVar[Final[Callable[[CrossValidationReport], float]]] = partial(
-        AccuracySTD.compute, data_source="train"
-    )
 
 
-@dataclass(kw_only=True)
+@dataclass
 class AccuracyTestSTD(Metric):
     data_source: Final[str] = "test"
-    function: InitVar[Final[Callable[[CrossValidationReport], float]]] = partial(
-        AccuracySTD.compute, data_source="test"
-    )
