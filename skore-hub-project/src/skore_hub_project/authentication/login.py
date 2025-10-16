f21e859521a6b4aae5a5bd6ced7ef87b333eea39
Auguste Baum <auguste@probabl.ai>
2025-10-16 15:16:48 +0200

    fix(skore-hub-project): Make `skore-hub-login` more user-friendly (#2066)

--- skore-hub-project/src/skore_hub_project/authentication/login.py
+++ skore-hub-project/src/skore_hub_project/authentication/login.py
@@ -18,24 +18,30 @@ def login(*, timeout=600):
     """Login to ``skore hub``."""
     with suppress(HTTPError):
         if Token.exists() and (Token.access() is not None):
+            console.print(
+                Panel(
+                    Align(
+                        "Already logged in!",
+                        align="center",
+                    ),
+                    title="[cyan]Skore Hub[/cyan]",
+                    border_style="orange1",
+                    expand=False,
+                    padding=1,
+                    title_align="center",
+                )
+            )
             return
 
     url, device_code, user_code = api.get_oauth_device_login()
 
-    if not open_webbrowser(url):
-        console.print(
-            Panel(
-                Align(
-                    f"Please visit this [link={url}]page[/link] to login.",
-                    align="center",
-                ),
-                title="[cyan]Skore HUB[/cyan]",
-                border_style="orange1",
-                expand=False,
-                padding=1,
-                title_align="center",
-            )
-        )
+    console.rule("[cyan]Skore Hub[/cyan]")
+    console.print(
+        f"Opening browser; if this fails, please visit this URL to log in:\n{url}",
+        soft_wrap=True,
+    )
+
+    open_webbrowser(url)
 
     api.get_oauth_device_code_probe(device_code, timeout=timeout)
     api.post_oauth_device_callback(device_code, user_code)
