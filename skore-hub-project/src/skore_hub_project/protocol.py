8a1e762af794f8b3318b6437b50bf5f4ca95c51b
Thomas S <thomas@probabl.ai>
2025-10-23 14:39:38 +0200

    chore(skore-hub-project): Use `mypy` strict mode

--- skore-hub-project/src/skore_hub_project/protocol.py
+++ skore-hub-project/src/skore_hub_project/protocol.py
@@ -1,6 +1,9 @@
 """Protocols definition used to remove adherence to ``skore`` for type checking."""
 
-from typing import Any, Protocol, runtime_checkable
+from typing import Any, Literal, Protocol, runtime_checkable
+
+DataSource = Literal["train", "test"] | None
+Aggregate = Literal["mean", "std"]
 
 
 @runtime_checkable
@@ -22,6 +25,13 @@ class EstimatorReport(Protocol):
     fit: Any
 
 
+class EstimatorReportMetricFunction(Protocol):
+    """Protocol equivalent to a functions in ``skore.EstimatorReport.metrics``."""
+
+    def __call__(self, data_source: DataSource) -> float | None:  # noqa: D102
+        ...
+
+
 @runtime_checkable
 class CrossValidationReport(Protocol):
     """Protocol equivalent to ``skore.CrossValidationReport``."""
@@ -39,3 +49,10 @@ class CrossValidationReport(Protocol):
     y: Any
     splitter: Any
     split_indices: Any
+
+
+class CrossValidationReportMetricFunction(Protocol):
+    """Protocol equivalent to functions in ``skore.CrossValidationReport.metrics``."""
+
+    def __call__(self, data_source: DataSource, aggregate: Aggregate) -> Any:  # noqa: D102
+        ...
