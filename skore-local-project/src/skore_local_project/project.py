15bc478afaa55e1af5aa7f34f9956f44a62a90d1
Auguste Baum <auguste@probabl.ai>
2025-10-27 14:19:50 +0100

    fix(skore-local-project)!: Change default workspace location (#2086)
    From "cache dir" to "data dir"

--- skore-local-project/src/skore_local_project/project.py
+++ skore-local-project/src/skore_local_project/project.py
@@ -79,12 +79,12 @@ class Project:
         | The workspace can be shared between all the projects.
         | The workspace can be set using kwargs or the environment variable
           ``SKORE_WORKSPACE``.
-        | If not, it will be by default set to a ``skore/`` directory in the USER
+        | If not, it will be by default set to a ``skore/`` directory in the user
           cache directory:
 
         - on Windows, usually ``C:\Users\%USER%\AppData\Local\skore``,
-        - on Linux, usually ``${HOME}/.cache/skore``,
-        - on macOS, usually ``${HOME}/Library/Caches/skore``.
+        - on Linux, usually ``${HOME}/.local/share/skore``,
+        - on macOS, usually ``${HOME}/Library/Application Support/skore``.
     """
 
     @staticmethod
@@ -100,7 +100,7 @@ class Project:
             if "SKORE_WORKSPACE" in os.environ:
                 workspace = Path(os.environ["SKORE_WORKSPACE"])
             else:
-                workspace = Path(platformdirs.user_cache_dir()) / "skore"
+                workspace = Path(platformdirs.user_data_dir()) / "skore"
 
         for directory in ("projects", "metadata", "artifacts"):
             (workspace / directory).mkdir(parents=True, exist_ok=True)
