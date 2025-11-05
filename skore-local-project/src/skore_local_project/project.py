b4df2296b87f10114ee03f5e68f5dd97525131db
Thomas S <2565098+thomass-dev@users.noreply.github.com>
2025-11-05 10:36:03 +0100

    chore(skore-local-project):: Use `mypy` strict mode

--- skore-local-project/src/skore_local_project/project.py
+++ skore-local-project/src/skore_local_project/project.py
@@ -6,7 +6,7 @@ import io
 import os
 from functools import wraps
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, ParamSpec, Protocol, TypeVar, cast, runtime_checkable
 from uuid import uuid4
 
 import joblib
@@ -15,7 +15,12 @@ import platformdirs
 from .metadata import CrossValidationReportMetadata, EstimatorReportMetadata
 from .storage import DiskCacheStorage
 
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
 if TYPE_CHECKING:
+    from collections.abc import Callable
     from typing import TypedDict
 
     from skore import CrossValidationReport, EstimatorReport
@@ -40,17 +45,26 @@ if TYPE_CHECKING:
         predict_time_mean: float | None
 
 
-def ensure_project_is_not_deleted(method):
+def ensure_project_is_not_deleted(method: Callable[P, R]) -> Callable[P, R]:
     """Ensure project is not deleted, before executing any other operation."""
 
+    @runtime_checkable
+    class Project(Protocol):
+        name: str
+        _Project__projects_storage: DiskCacheStorage
+
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        if self.name not in self._Project__projects_storage:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        project = args[0]
+
+        assert isinstance(project, Project), "You can only wrap `Project` methods"
+
+        if project.name not in project._Project__projects_storage:
             raise RuntimeError(
-                f"Skore could not proceed because {self!r} does not exist anymore."
+                f"Skore could not proceed because {project!r} does not exist anymore."
             )
 
-        return method(self, *args, **kwargs)
+        return method(*args, **kwargs)
 
     return wrapper
 
@@ -183,7 +197,7 @@ class Project:
         return pickle_hash, pickle_bytes
 
     @ensure_project_is_not_deleted
-    def put(self, key: str, report: EstimatorReport | CrossValidationReport):
+    def put(self, key: str, report: EstimatorReport | CrossValidationReport) -> None:
         """
         Put a key-report pair to the local project.
 
@@ -238,7 +252,9 @@ class Project:
         """Get a persisted report by its id."""
         if id in self.__artifacts_storage:
             with io.BytesIO(self.__artifacts_storage[id]) as stream:
-                return joblib.load(stream)
+                return cast(
+                    "EstimatorReport | CrossValidationReport", joblib.load(stream)
+                )
 
         raise KeyError(id)
 
@@ -275,7 +291,7 @@ class Project:
         )
 
     @staticmethod
-    def delete(name: str, *, workspace: Path | None = None):
+    def delete(name: str, *, workspace: Path | None = None) -> None:
         r"""
         Delete a local project.
 
