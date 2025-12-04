c99bd227ac77481b0efb7861e8fe27793977d522
Thomas S <2565098+thomass-dev@users.noreply.github.com>
2025-12-04 12:11:58 +0100

    fix(skore-hub-project/login): Ignore token URI when using API key

--- skore-hub-project/src/skore_hub_project/authentication/uri.py
+++ skore-hub-project/src/skore_hub_project/authentication/uri.py
@@ -24,10 +24,36 @@ def persist(uri: str) -> None:
 
 
 def URI() -> str:
-    """URI used for ``skore hub`` authentication."""
+    """
+    URI used for ``skore hub`` authentication.
+
+    Notes
+    -----
+    It is discovered by following the rules in order:
+
+    1. If you have setup an API key using the envar ``SKORE_HUB_API_KEY``, returns the
+       value of the envar ``SKORE_HUB_URI``, or ``https://api.skore.probabl.ai``.
+
+    2. If you have an active token, extract the content of the envar ``SKORE_HUB_URI``
+       and the URI associated with the token:
+        2.1. If both the URIs from the environment and the one associated with the token
+             are empty, returns ``https://api.skore.probabl.ai``.
+        2.2. If the URI from the environment is empty and the one associated with the
+             token isn't, returns the URI associated with the token.
+        2.3. If the URI associated with the token is empty, and the one from the
+             environment isn't, returns the URI from the environment.
+        2.4. If both the URIs associated with the token and the one from the
+             environment are not empty:
+                2.4.1. If both are equal, returns one of them.
+                2.4.2. If both aren't equal, raises a conflicting exception.
+    """
+    uri_from_environment = environ.get(ENVARNAME)
+
+    if environ.get("SKORE_HUB_API_KEY"):
+        return uri_from_environment or DEFAULT
+
     filepath = Filepath()
     uri_from_persistence = filepath.read_text() if filepath.exists() else None
-    uri_from_environment = environ.get(ENVARNAME)
 
     if (
         uri_from_persistence
