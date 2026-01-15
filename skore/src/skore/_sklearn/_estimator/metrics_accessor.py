41a7167a8c1f821b6a109384477fd87233977b8b
Guillaume Lemaitre <guillaume@probabl.ai>
2026-01-15 21:36:21 +0100

    fix docstring

--- skore/src/skore/_sklearn/_estimator/metrics_accessor.py
+++ skore/src/skore/_sklearn/_estimator/metrics_accessor.py
@@ -1973,7 +1973,7 @@ class _MetricsAccessor(
         >>> classifier = LogisticRegression(max_iter=10_000)
         >>> report = EstimatorReport(classifier, **split_data)
         >>> display = report.metrics.precision_recall()
-        >>> display.plot(relplot_kwargs={"color": "tab:red"})
+        >>> display.set_style(relplot_kwargs={"color": "tab:red"}).plot()
         """
         if pos_label is _DEFAULT:
             pos_label = self._parent.pos_label
