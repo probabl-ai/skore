ed065458bb2250967ccf9b87958d8fc89ce6c2b2
Guillaume Lemaitre <guillaume@probabl.ai>
2026-01-15 21:53:52 +0100

    chore(skore): Moving table report kwargs into set_style

--- skore/tests/unit/displays/table_report/test_estimator.py
+++ skore/tests/unit/displays/table_report/test_estimator.py
@@ -134,7 +134,7 @@ def test_categorical_plots_1d(pyplot, display):
         0.75,
     )
 
-    display.plot(y="gender", histplot_kwargs={"color": "blue"})
+    display.set_style(histplot_kwargs={"color": "blue"}).plot(y="gender")
     assert display.ax_.get_xlabel() == "Count"
     assert display.ax_.get_ylabel() == "gender"
     # blue
@@ -145,7 +145,7 @@ def test_numeric_plots_1d(pyplot, estimator_report):
     """Check the plot output with numeric data in 1-d."""
     display = estimator_report.data.analyze(data_source="train")
     ## for integers numeric values
-    display.plot(x="year_first_hired", histplot_kwargs={"color": "red"})
+    display.set_style(histplot_kwargs={"color": "red"}).plot(x="year_first_hired")
     assert display.ax_.get_xlabel() == "year_first_hired"
     labels = display.ax_.get_xticklabels()
     assert labels[0].get_text() == "1970"
@@ -157,7 +157,7 @@ def test_numeric_plots_1d(pyplot, estimator_report):
     # red
     assert display.ax_.containers[0].patches[0].get_facecolor() == (1.0, 0.0, 0.0, 0.75)
 
-    display.plot(y="year_first_hired")
+    display.set_style().plot(y="year_first_hired")
     assert display.ax_.get_xlabel() == "Count"
     assert display.ax_.get_ylabel() == "year_first_hired"
 
