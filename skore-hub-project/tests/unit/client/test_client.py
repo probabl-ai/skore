probabl-ai/skore

commit 94ec184f4cd0dfb68bad7ef59fca7c3f409dbd47

raise exceptions from threads

skore-hub-project/src/skore_hub_project/artifact/upload.py
@@ -94,6 +94,7 @@ def upload(
     pool : TheadPoolExecutor
         The pool used to execute the `upload_chunk` threads.
     """
+    return
     assert filepath.stat().st_size, "`filepath` must not be empty"
 
     with HUBClient() as hub_client, Client() as standard_client:

skore-hub-project/tests/unit/report/test_cross_validation_report.py
@@ -372,6 +372,23 @@ class TestCrossValidationReportPayload:
             TableReport,
         ]
 
+    def test_medias_raises_exception(self, monkeypatch, payload):
+        """
+        Since medias compute is multi-threaded, ensure that any exceptions thrown in a
+        sub-thread are also thrown in the main thread.
+        """
+
+        def raise_exception(_):
+            raise Exception("test_medias_raises_exception")
+
+        monkeypatch.setattr(
+            "skore_hub_project.artifact.media.EstimatorHtmlRepr.compute",
+            raise_exception,
+        )
+
+        with raises(Exception, match="test_medias_raises_exception"):
+            list(map(type, payload.medias))
+
     @mark.filterwarnings(
         # ignore precision warning due to the low number of labels in
         # `small_cv_binary_classification`, raised by `scikit-learn`

skore-hub-project/tests/unit/report/test_estimator_report.py
@@ -148,6 +148,23 @@ class TestEstimatorReportPayload:
             TableReportTrain,
         ]
 
+    def test_medias_raises_exception(self, monkeypatch, payload):
+        """
+        Since medias compute is multi-threaded, ensure that any exceptions thrown in a
+        sub-thread are also thrown in the main thread.
+        """
+
+        def raise_exception(_):
+            raise Exception("test_medias_raises_exception")
+
+        monkeypatch.setattr(
+            "skore_hub_project.artifact.media.EstimatorHtmlRepr.compute",
+            raise_exception,
+        )
+
+        with raises(Exception, match="test_medias_raises_exception"):
+            list(map(type, payload.medias))
+
     @mark.usefixtures("monkeypatch_artifact_hub_client")
     @mark.usefixtures("monkeypatch_upload_routes")
     def test_model_dump(self, binary_classification, payload):

skore-hub-project/src/skore_hub_project/report/report.py
@@ -172,13 +172,12 @@ class ReportPayload(BaseModel, ABC, Generic[Report]):
                 for media in medias
             ]
 
-            deque(
-                progress.track(
-                    as_completed(tasks),
-                    description=f"Uploading {self.report.__class__.__name__} media",
-                    total=len(tasks),
-                )
-            )
+            for task in progress.track(
+                as_completed(tasks),
+                description=f"Uploading {self.report.__class__.__name__} media",
+                total=len(tasks),
+            ):
+                task.result()
 
         return [media for media in medias if media.checksum is not None]
 

commit 7e11fca18a0ccddfe9687006145b31f564214197

fix regression introduced in 94ec184f4cd0dfb68bad7ef59fca7c3f409dbd47 and fix tests to detect such regression

skore-hub-project/src/skore_hub_project/artifact/upload.py
@@ -94,7 +94,6 @@ def upload(
     pool : TheadPoolExecutor
         The pool used to execute the `upload_chunk` threads.
     """
-    return
     assert filepath.stat().st_size, "`filepath` must not be empty"
 
     with HUBClient() as hub_client, Client() as standard_client:

skore-hub-project/tests/unit/artifact/media/test_data.py
@@ -8,9 +8,6 @@ from skore_hub_project import Project
 from skore_hub_project.artifact.media import TableReportTest, TableReportTrain
 
 
-@mark.usefixtures("monkeypatch_artifact_hub_client")
-@mark.usefixtures("monkeypatch_upload_routes")
-@mark.usefixtures("monkeypatch_upload_with_mock")
 @mark.parametrize(
     "Media,data_source",
     (
@@ -49,6 +46,10 @@ class TestTableReport:
             "top_associations",
         }.issubset(dataframe.keys())
 
+    @mark.usefixtures("monkeypatch_artifact_hub_client")
+    @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_upload(
         self, tmp_path, binary_classification, Media, data_source, upload_mock
     ):
@@ -75,6 +76,10 @@ class TestTableReport:
             "pool": pool,
         }
 
+    @mark.usefixtures("monkeypatch_artifact_hub_client")
+    @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_model_dump(self, binary_classification, Media, data_source):
         project = Project("<tenant>", "<name>")
         media = Media(project=project, report=binary_classification)

skore-hub-project/tests/unit/artifact/media/test_feature_importance.py
@@ -44,9 +44,6 @@ def monkeypatch_permutation(monkeypatch):
     )
 
 
-@mark.usefixtures("monkeypatch_artifact_hub_client")
-@mark.usefixtures("monkeypatch_upload_routes")
-@mark.usefixtures("monkeypatch_upload_with_mock")
 @mark.parametrize(
     "Media,report,accessor,data_source",
     (
@@ -129,6 +126,10 @@ class TestFeatureImportance:
         assert media.computed is True
         assert media.filepath.stat().st_size == 0
 
+    @mark.usefixtures("monkeypatch_artifact_hub_client")
+    @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_upload_available_accessor(
         self, tmp_path, Media, report, accessor, data_source, upload_mock, request
     ):
@@ -192,6 +193,10 @@ class TestFeatureImportance:
         # ensure `upload` is not called
         assert not upload_mock.called
 
+    @mark.usefixtures("monkeypatch_artifact_hub_client")
+    @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_model_dump(self, Media, report, accessor, data_source, request):
         project = Project("<tenant>", "<name>")
         report = request.getfixturevalue(report)

skore-hub-project/tests/unit/artifact/media/test_model.py
@@ -19,9 +19,6 @@ def serialize(estimator_html_repr) -> (bytes, str):
     return estimator_html_repr_bytes, f"blake3-{bytes_to_b64_str(checksum)}"
 
 
-@mark.usefixtures("monkeypatch_artifact_hub_client")
-@mark.usefixtures("monkeypatch_upload_routes")
-@mark.usefixtures("monkeypatch_upload_with_mock")
 class TestEstimatorHtmlRepr:
     def test_init_exception(self):
         project = Project("<tenant>", "<name>")
@@ -42,6 +39,10 @@ class TestEstimatorHtmlRepr:
         assert media.computed is True
         assert media.filepath.read_bytes() == content
 
+    @mark.usefixtures("monkeypatch_artifact_hub_client")
+    @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_upload(self, tmp_path, binary_classification, upload_mock):
         project = Project("<tenant>", "<name>")
         _, checksum = serialize(estimator_html_repr(binary_classification.estimator_))
@@ -68,6 +69,10 @@ class TestEstimatorHtmlRepr:
             "pool": pool,
         }
 
+    @mark.usefixtures("monkeypatch_artifact_hub_client")
+    @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_model_dump(self, binary_classification):
         project = Project("<tenant>", "<name>")
         _, checksum = serialize(estimator_html_repr(binary_classification.estimator_))

skore-hub-project/tests/unit/artifact/media/test_performance.py
@@ -37,9 +37,6 @@ def serialize(display) -> (bytes, str):
     # which is a dependency of `skore`
     "ignore:The default of observed=False is deprecated.*:FutureWarning:seaborn",
 )
-@mark.usefixtures("monkeypatch_artifact_hub_client")
-@mark.usefixtures("monkeypatch_upload_routes")
-@mark.usefixtures("monkeypatch_upload_with_mock")
 @mark.parametrize(
     "Media,report,accessor,data_source",
     (
@@ -169,6 +166,10 @@ class TestPerformance:
         assert media.computed is True
         assert media.filepath.stat().st_size == 0
 
+    @mark.usefixtures("monkeypatch_artifact_hub_client")
+    @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_upload_available_accessor(
         self, tmp_path, Media, report, accessor, data_source, upload_mock, request
     ):
@@ -230,6 +231,10 @@ class TestPerformance:
         # ensure `upload` is not called
         assert not upload_mock.called
 
+    @mark.usefixtures("monkeypatch_artifact_hub_client")
+    @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_model_dump(self, Media, report, accessor, data_source, request):
         project = Project("<tenant>", "<name>")
         report = request.getfixturevalue(report)

skore-hub-project/tests/unit/authentication/test_login.py
@@ -16,6 +16,7 @@ from skore_hub_project.authentication.login import (
 
 
 @mark.parametrize("success_uri", [None, "toto"])
+@mark.respx()
 def test_get_oauth_device_login(respx_mock, success_uri):
     respx_mock.get(urljoin(uri.DEFAULT, "identity/oauth/device/login")).mock(
         Response(
@@ -39,6 +40,7 @@ def test_get_oauth_device_login(respx_mock, success_uri):
     assert user_code == "C"
 
 
+@mark.respx()
 def test_post_oauth_device_callback(respx_mock):
     route = respx_mock.post(
         urljoin(uri.DEFAULT, "identity/oauth/device/callback")
@@ -50,6 +52,7 @@ def test_post_oauth_device_callback(respx_mock):
     assert route.calls.last.request.read() == b"state=my_state&user_code=my_user_code"
 
 
+@mark.respx()
 def test_get_oauth_device_token(respx_mock):
     respx_mock.get(urljoin(uri.DEFAULT, "identity/oauth/device/token")).mock(
         Response(
@@ -74,6 +77,7 @@ def test_get_oauth_device_token(respx_mock):
     }
 
 
+@mark.respx()
 def test_get_oauth_device_code_probe(monkeypatch, respx_mock):
     monkeypatch.setattr("skore_hub_project.authentication.login.sleep", lambda _: None)
     respx_mock.get(urljoin(uri.DEFAULT, "identity/oauth/device/code-probe")).mock(
@@ -92,6 +96,7 @@ def test_get_oauth_device_code_probe(monkeypatch, respx_mock):
     }
 
 
+@mark.respx()
 def test_get_oauth_device_code_probe_exception(respx_mock):
     respx_mock.get(urljoin(uri.DEFAULT, "identity/oauth/device/code-probe")).mock(
         side_effect=[
@@ -111,6 +116,7 @@ def test_get_oauth_device_code_probe_exception(respx_mock):
     }
 
 
+@mark.respx()
 def test_get_oauth_device_code_probe_timeout(respx_mock):
     respx_mock.get(urljoin(uri.DEFAULT, "identity/oauth/device/code-probe")).mock(
         side_effect=[
@@ -155,6 +161,7 @@ def test_login_with_token(respx_mock):
     assert token.access(refresh=False) == "A"
 
 
+@mark.respx()
 def test_login_with_expired_token(respx_mock):
     respx_mock.post(REFRESH_URL).mock(
         Response(
@@ -180,6 +187,7 @@ def test_login_with_expired_token(respx_mock):
     assert token.access(refresh=False) == "D"
 
 
+@mark.respx()
 def test_login(monkeypatch, respx_mock):
     def open_webbrowser(url):
         open_webbrowser.url = url
@@ -233,6 +241,7 @@ def test_login(monkeypatch, respx_mock):
     assert uri.URI() == uri.DEFAULT
 
 
+@mark.respx()
 def test_login_timeout(monkeypatch, respx_mock):
     monkeypatch.setattr(
         "skore_hub_project.authentication.login.open_webbrowser",
@@ -251,19 +260,6 @@ def test_login_timeout(monkeypatch, respx_mock):
         )
     )
     respx_mock.get(PROBE_URL).mock(side_effect=repeat(Response(400)))
-    respx_mock.post(CALLBACK_URL).mock(Response(200))
-    respx_mock.get(TOKEN_URL).mock(
-        Response(
-            200,
-            json={
-                "token": {
-                    "access_token": "D",
-                    "refresh_token": "E",
-                    "expires_at": DATETIME_MAX,
-                }
-            },
-        )
-    )
 
     assert not token.Filepath().exists()
 
@@ -281,6 +277,7 @@ def test_login_timeout(monkeypatch, respx_mock):
     assert not token.Filepath().exists()
 
 
+@mark.respx()
 def test_login_successively_on_different_uri(monkeypatch, respx_mock):
     def open_webbrowser(url):
         open_webbrowser.url = url
@@ -291,7 +288,6 @@ def test_login_successively_on_different_uri(monkeypatch, respx_mock):
     )
 
     for u in ("https://my-1-uri", "https://my-2-uri"):
-        respx_mock.post(urljoin(u, REFRESH_URL)).mock(Response(404))
         respx_mock.get(urljoin(u, LOGIN_URL)).mock(
             Response(
                 200,

skore-hub-project/tests/unit/authentication/test_token.py
@@ -17,6 +17,7 @@ def filepath(tmp_path):
     return tmp_path / "skore.token"
 
 
+@mark.respx()
 def test_post_oauth_refresh_token(respx_mock):
     route = respx_mock.post(urljoin(URI, "identity/oauth/token/refresh")).mock(
         Response(
@@ -58,6 +59,7 @@ def test_access(filepath, respx_mock):
     assert not respx_mock.calls
 
 
+@mark.respx()
 def test_access_expired(filepath, respx_mock):
     respx_mock.post(urljoin(URI, "identity/oauth/token/refresh")).mock(
         Response(

skore-hub-project/tests/unit/client/test_client.py
@@ -20,6 +20,7 @@ REFRESH_URL = "identity/oauth/token/refresh"
 
 
 class TestClient:
+    @mark.respx()
     def test_request_without_retry(self, monkeypatch, respx_mock):
         timeouts = []
 
@@ -44,6 +45,7 @@ class TestClient:
         assert timeouts == []
         assert excinfo.value.response.status_code == 408
 
+    @mark.respx()
     def test_request_with_retry(self, monkeypatch, respx_mock):
         timeouts = []
 
@@ -70,6 +72,7 @@ class TestClient:
 
         assert timeouts == [0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0]
 
+    @mark.respx()
     def test_request_with_retry_and_retry_total(self, monkeypatch, respx_mock):
         timeouts = []
 
@@ -97,6 +100,7 @@ class TestClient:
         assert timeouts == [0.25, 0.5, 1.0, 2.0, 4.0]
         assert excinfo.value.response.status_code == 429
 
+    @mark.respx()
     def test_request_with_retry_and_unretryable_status(self, monkeypatch, respx_mock):
         timeouts = []
 
@@ -126,6 +130,7 @@ def test___semver():
 
 
 class TestHUBClient:
+    @mark.respx()
     def test_request_with_api_key(self, monkeypatch, respx_mock):
         monkeypatch.setenv("SKORE_HUB_API_KEY", "<api-key>")
         respx_mock.get(urljoin(uri.DEFAULT, "foo")).mock(Response(200))
@@ -136,6 +141,7 @@ class TestHUBClient:
         assert "authorization" not in respx_mock.calls.last.request.headers
         assert respx_mock.calls.last.request.headers["X-API-Key"] == "<api-key>"
 
+    @mark.respx()
     def test_request_with_token(self, respx_mock):
         respx_mock.get(urljoin(uri.DEFAULT, "foo")).mock(Response(200))
 
@@ -154,6 +160,7 @@ class TestHUBClient:
         assert "X-API-Key" not in respx_mock.calls.last.request.headers
         assert respx_mock.calls.last.request.headers["authorization"] == "Bearer A"
 
+    @mark.respx()
     def test_request_with_token_and_uri(self, respx_mock):
         respx_mock.get(urljoin(uri.DEFAULT, "foo")).mock(Response(200))
 
@@ -174,6 +181,7 @@ class TestHUBClient:
         assert "X-API-Key" not in respx_mock.calls.last.request.headers
         assert respx_mock.calls.last.request.headers["authorization"] == "Bearer A"
 
+    @mark.respx()
     def test_request_with_api_key_and_token_and_uri(self, monkeypatch, respx_mock):
         TOKEN_URI = "https://token.com"
         API_KEY_URI = "https://apikey.com"
@@ -235,6 +243,7 @@ class TestHUBClient:
         assert token.access(refresh=False) == "A"
         assert not respx_mock.calls
 
+    @mark.respx()
     def test_request_with_expired_token(self, tmp_path, respx_mock):
         respx_mock.get(urljoin(uri.DEFAULT, "foo")).mock(Response(200))
         respx_mock.post(REFRESH_URL).mock(
@@ -263,6 +272,7 @@ class TestHUBClient:
         assert "X-API-Key" not in respx_mock.calls.last.request.headers
         assert respx_mock.calls.last.request.headers["authorization"] == "Bearer D"
 
+    @mark.respx()
     def test_request_raises(self, tmp_path, respx_mock):
         respx_mock.get(urljoin(uri.DEFAULT, "foo")).mock(Response(404))
 
@@ -279,6 +289,7 @@ class TestHUBClient:
         assert token.Filepath().exists()
         assert token.access(refresh=False) == "A"
 
+    @mark.respx()
     def test_request_without_package_semver(self, respx_mock):
         from importlib.metadata import version
 
@@ -294,6 +305,7 @@ class TestHUBClient:
 
         assert "X-Skore-Client" not in respx_mock.calls.last.request.headers
 
+    @mark.respx()
     def test_request_with_package_semver(self, monkeypatch, respx_mock):
         monkeypatch.setattr("skore_hub_project.client.client.PACKAGE_SEMVER", "1.0.0")
         respx_mock.get(urljoin(uri.DEFAULT, "foo")).mock(Response(200))

skore-hub-project/tests/unit/project/test_project.py
@@ -97,6 +97,7 @@ class TestProject:
     def test_quoted_name(self):
         assert Project("my/ tenant", "my/ name").quoted_name == "my%2F%20name"
 
+    @mark.respx()
     def test_put_exception(self, respx_mock):
         respx_mock.post("projects/<tenant>/<name>").mock(Response(200))
 
@@ -109,6 +110,7 @@ class TestProject:
         ):
             Project("<tenant>", "<name>").put("<key>", "<value>")
 
+    @mark.respx()
     def test_put_estimator_report(self, monkeypatch, binary_classification, respx_mock):
         respx_mock.post("projects/<tenant>/<name>").mock(Response(200))
         respx_mock.get("projects/<tenant>/<name>/artifacts").mock(Response(200, json=1))
@@ -140,6 +142,7 @@ class TestProject:
         # `searborn`, which is a dependency of `skore`
         "ignore:The default of observed=False is deprecated.*:FutureWarning:seaborn",
     )
+    @mark.respx()
     def test_put_cross_validation_report(
         self, monkeypatch, small_cv_binary_classification, respx_mock
     ):
@@ -165,6 +168,7 @@ class TestProject:
         # Compare content with the desired output
         assert content == desired
 
+    @mark.respx()
     def test_get_estimator_report(self, respx_mock, regression):
         # Mock hub routes that will be called
         respx_mock.post("projects/<tenant>/<name>").mock(Response(200))
@@ -188,6 +192,7 @@ class TestProject:
         assert report.estimator_name_ == regression.estimator_name_
         assert report.ml_task == regression.ml_task
 
+    @mark.respx()
     def test_reports_get_cross_validation_report(self, respx_mock, cv_regression):
         # Mock hub routes that will be called
         respx_mock.post("projects/<tenant>/<name>").mock(Response(200))
@@ -211,6 +216,7 @@ class TestProject:
         assert report.estimator_name_ == cv_regression.estimator_name_
         assert report.ml_task == cv_regression.ml_task
 
+    @mark.respx()
     def test_summarize(self, nowstr, respx_mock):
         respx_mock.post("projects/<tenant>/<name>").mock(Response(200))
 
@@ -334,10 +340,12 @@ class TestProject:
             },
         ]
 
+    @mark.respx()
     def test_delete(self, respx_mock):
         respx_mock.delete("projects/<tenant>/<name>").mock(Response(204))
         Project.delete("<tenant>", "<name>")
 
+    @mark.respx()
     def test_delete_exception(self, respx_mock):
         respx_mock.delete("projects/<tenant>/<name>").mock(Response(403))
 

skore-hub-project/tests/unit/report/test_cross_validation_report.py
@@ -251,6 +251,7 @@ class TestCrossValidationReportPayload:
     @mark.usefixtures("monkeypatch_artifact_hub_client")
     @mark.usefixtures("monkeypatch_upload_routes")
     @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_estimators(self, project, payload, upload_mock):
         assert len(payload.estimators) == len(payload.report.estimator_reports_)
 
@@ -280,8 +281,9 @@ class TestCrossValidationReportPayload:
     @mark.usefixtures("monkeypatch_artifact_hub_client")
     @mark.usefixtures("monkeypatch_upload_routes")
     @mark.usefixtures("monkeypatch_upload_with_mock")
+    @mark.respx()
     def test_pickle(
-        self, small_cv_binary_classification, project, payload, upload_mock, respx_mock
+        self, small_cv_binary_classification, project, payload, upload_mock
     ):
         _, checksum = serialize(small_cv_binary_classification)
 
@@ -362,6 +364,7 @@ class TestCrossValidationReportPayload:
     )
     @mark.usefixtures("monkeypatch_artifact_hub_client")
     @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.respx()
     def test_medias(self, payload):
         assert list(map(type, payload.medias)) == [
             EstimatorHtmlRepr,
@@ -399,6 +402,7 @@ class TestCrossValidationReportPayload:
     )
     @mark.usefixtures("monkeypatch_artifact_hub_client")
     @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.respx()
     def test_model_dump_classification(self, small_cv_binary_classification, payload):
         _, checksum = serialize(small_cv_binary_classification)
 

skore-hub-project/tests/unit/report/test_estimator_report.py
@@ -77,9 +77,8 @@ class TestEstimatorReportPayload:
     @mark.usefixtures("monkeypatch_artifact_hub_client")
     @mark.usefixtures("monkeypatch_upload_routes")
     @mark.usefixtures("monkeypatch_upload_with_mock")
-    def test_pickle(
-        self, binary_classification, project, payload, upload_mock, respx_mock
-    ):
+    @mark.respx()
+    def test_pickle(self, binary_classification, project, payload, upload_mock):
         pickle, checksum = serialize(binary_classification)
 
         # Ensure checksum is well constructed
@@ -134,6 +133,7 @@ class TestEstimatorReportPayload:
 
     @mark.usefixtures("monkeypatch_artifact_hub_client")
     @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.respx()
     def test_medias(self, payload):
         assert list(map(type, payload.medias)) == [
             EstimatorHtmlRepr,
@@ -167,6 +167,7 @@ class TestEstimatorReportPayload:
 
     @mark.usefixtures("monkeypatch_artifact_hub_client")
     @mark.usefixtures("monkeypatch_upload_routes")
+    @mark.respx()
     def test_model_dump(self, binary_classification, payload):
         _, checksum = serialize(binary_classification)
 