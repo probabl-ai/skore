6421685004bb92de88c235c58192296f4d46d36c
Guillaume Lemaitre <guillaume@probabl.ai>
2026-01-17 23:11:56 +0100

    wip

--- skore/src/skore/_sklearn/_estimator/feature_importance_accessor.py
+++ skore/src/skore/_sklearn/_estimator/feature_importance_accessor.py
@@ -9,8 +9,6 @@ import pandas as pd
 from numpy.typing import ArrayLike
 from scipy.sparse import issparse
 from sklearn import metrics
-from sklearn.base import is_classifier, is_regressor
-from sklearn.inspection import permutation_importance
 from sklearn.metrics import make_scorer
 from sklearn.pipeline import Pipeline
 from sklearn.utils.metaestimators import available_if
@@ -24,12 +22,10 @@ from skore._sklearn._plot.feature_importance.permutation import (
     PermutationImportanceDisplay,
 )
 from skore._sklearn.feature_names import _get_feature_names
-from skore._sklearn.types import Aggregate
 from skore._utils._accessor import (
     _check_estimator_has_coef,
     _check_has_feature_importances,
 )
-from skore._utils._index import flatten_multi_index
 
 DataSource = Literal["test", "train", "X_y"]
 
@@ -466,230 +462,93 @@ class _FeatureImportanceAccessor(_BaseAccessor[EstimatorReport], DirNamesMixin):
         if seed is not None and not isinstance(seed, int):
             raise ValueError(f"seed must be an integer or None; got {type(seed)}")
 
-        if not isinstance(self._parent.estimator_, Pipeline) or at_step == 0:
-            feature_engineering, estimator = None, self._parent.estimator_
-            X_transformed = X_
+        # build the cache key components to finally create a tuple that will be used
+        # to check if the metric has already been computed
+        cache_key_parts: list[Any] = [
+            self._parent._hash,
+            "permutation_importance",
+            data_source,
+            at_step,
+        ]
+        cache_key_parts.append(data_source_hash)
 
+        if callable(metric) or isinstance(metric, list | dict):
+            cache_key_parts.append(joblib.hash(metric))
         else:
-            pipeline = self._parent.estimator_
-            if not isinstance(at_step, str | int):
-                raise ValueError(
-                    f"at_step must be an integer or a string; got {at_step!r}"
-                )
+            cache_key_parts.append(metric)
 
-            if isinstance(at_step, str):
-                # Make at_step an int and process it as usual
-                at_step = list(pipeline.named_steps.keys()).index(at_step)
+        # order arguments by key to ensure cache works n_jobs variable should not be in
+        # the cache
+        kwargs = {"n_repeats": n_repeats, "max_samples": max_samples, "seed": seed}
+        for _, v in sorted(kwargs.items()):
+            cache_key_parts.append(v)
 
-            if isinstance(at_step, int):
-                if abs(at_step) >= len(pipeline.steps):
+        cache_key = tuple(cache_key_parts)
+
+        if cache_key in self._parent._cache and seed is not None:
+            # NOTE: avoid to fetch from the cache if the seed is None because we want
+            # to trigger the computation in this case. We only have the permutation
+            # stored as a workaround for the serialization for skore-hub as explained
+            # earlier.
+            display = self._parent._cache[cache_key]
+        else:
+            if not isinstance(self._parent.estimator_, Pipeline) or at_step == 0:
+                feature_engineering, estimator = None, self._parent.estimator_
+                X_transformed = X_
+
+            else:
+                pipeline = self._parent.estimator_
+                if not isinstance(at_step, str | int):
                     raise ValueError(
-                        "at_step must be strictly smaller in magnitude than the "
-                        "number of steps in the Pipeline, which is "
-                        f"{len(pipeline.steps)}; got {at_step}"
+                        f"at_step must be an integer or a string; got {at_step!r}"
                     )
-                feature_engineering, estimator = (
-                    pipeline[:at_step],
-                    pipeline[at_step:],
-                )
-                X_transformed = feature_engineering.transform(X_)
 
-        feature_names = _get_feature_names(
-            estimator,
-            n_features=_num_features(X_transformed),
-            X=X_transformed,
-            transformer=feature_engineering,
-        )
+                if isinstance(at_step, str):
+                    # Make at_step an int and process it as usual
+                    at_step = list(pipeline.named_steps.keys()).index(at_step)
 
-        if issparse(X_transformed):
-            X_transformed = X_transformed.todense()
+                if isinstance(at_step, int):
+                    if abs(at_step) >= len(pipeline.steps):
+                        raise ValueError(
+                            "at_step must be strictly smaller in magnitude than the "
+                            "number of steps in the Pipeline, which is "
+                            f"{len(pipeline.steps)}; got {at_step}"
+                        )
+                    feature_engineering, estimator = (
+                        pipeline[:at_step],
+                        pipeline[at_step:],
+                    )
+                    X_transformed = feature_engineering.transform(X_)
 
-        return PermutationImportanceDisplay._compute_data_for_display(
-            data_source=data_source,
-            estimator=estimator,
-            X=X_transformed,
-            y=y_true,
-            feature_names=feature_names,
-            metric=metric,
-            n_repeats=n_repeats,
-            max_samples=max_samples,
-            n_jobs=n_jobs,
-            seed=seed,
-            report_type="estimator",
-        )
+            feature_names = _get_feature_names(
+                estimator,
+                n_features=_num_features(X_transformed),
+                X=X_transformed,
+                transformer=feature_engineering,
+            )
 
-    # def _feature_permutation(
-    #     self,
-    #     *,
-    #     data_source: DataSource,
-    #     data_source_hash: int | None,
-    #     X: ArrayLike | None,
-    #     y: ArrayLike | None,
-    #     aggregate: Aggregate | None,
-    #     metric: Metric | None,
-    #     n_repeats: int,
-    #     max_samples: float,
-    #     n_jobs: int | None,
-    #     seed: int | None,
-    #     flat_index: bool,
-    #     at_step: int | str,
-    # ) -> pd.DataFrame:
-    #     """Private interface of `feature_permutation` to pass `data_source_hash`.
+            if issparse(X_transformed):
+                X_transformed = X_transformed.todense()
 
-    #     `data_source_hash` is either an `int` when we already computed the hash
-    #     and are able to pass it around, or `None` and thus trigger its computation
-    #     in the underlying process.
-    #     """
-    #     checked_metric = _check_metric(metric)
+            display = PermutationImportanceDisplay._compute_data_for_display(
+                data_source=data_source,
+                estimator=estimator,
+                X=X_transformed,
+                y=y_true,
+                feature_names=feature_names,
+                metric=metric,
+                n_repeats=n_repeats,
+                max_samples=max_samples,
+                n_jobs=n_jobs,
+                seed=seed,
+                report_type="estimator",
+            )
 
-    #     if data_source_hash is None:
-    #         X_, y_true, data_source_hash = self._get_X_y_and_data_source_hash(
-    #             data_source=data_source, X=X, y=y
-    #         )
+            if cache_key is not None:
+                # NOTE: for the moment, we will always store the permutation importance
+                self._parent._cache[cache_key] = display
 
-    #     # NOTE: to temporary improve the `project.put` UX, we always store the
-    #     # permutation importance into the cache dictionary even when seed is None.
-    #     # Be aware that if seed is None, we still trigger the computation for all cases.
-    #     # We only store it such that when we serialize to send to the hub, we only
-    #     # fetch for the cache store instead of recomputing it because it is expensive.
-    #     # FIXME: the workaround above should be removed once we are able to trigger
-    #     # computation on the server side of skore-hub.
-
-    #     if seed is not None and not isinstance(seed, int):
-    #         raise ValueError(f"seed must be an integer or None; got {type(seed)}")
-
-    #     # build the cache key components to finally create a tuple that will be used
-    #     # to check if the metric has already been computed
-    #     cache_key_parts: list[Any] = [
-    #         self._parent._hash,
-    #         "permutation_importance",
-    #         data_source,
-    #         at_step,
-    #     ]
-    #     cache_key_parts.append(data_source_hash)
-
-    #     if callable(metric) or isinstance(metric, list | dict):
-    #         cache_key_parts.append(joblib.hash(metric))
-    #     else:
-    #         cache_key_parts.append(metric)
-
-    #     # aggregate is not included in the cache in order to trade off computation for
-    #     # storage
-    #     # order arguments by key to ensure cache works n_jobs variable should not be in
-    #     # the cache
-    #     kwargs = {"n_repeats": n_repeats, "max_samples": max_samples, "seed": seed}
-    #     for _, v in sorted(kwargs.items()):
-    #         cache_key_parts.append(v)
-
-    #     cache_key = tuple(cache_key_parts)
-
-    #     if cache_key in self._parent._cache and seed is not None:
-    #         # NOTE: avoid to fetch from the cache if the seed is None because we want
-    #         # to trigger the computation in this case. We only have the permutation
-    #         # stored as a workaround for the serialization for skore-hub as explained
-    #         # earlier.
-    #         score = self._parent._cache[cache_key]
-    #     else:
-    #         if not isinstance(self._parent.estimator_, Pipeline) or at_step == 0:
-    #             feature_engineering, estimator = None, self._parent.estimator_
-    #             X_transformed = X_
-
-    #         else:
-    #             pipeline = self._parent.estimator_
-    #             if not isinstance(at_step, str | int):
-    #                 raise ValueError(
-    #                     f"at_step must be an integer or a string; got {at_step!r}"
-    #                 )
-
-    #             if isinstance(at_step, str):
-    #                 # Make at_step an int and process it as usual
-    #                 at_step = list(pipeline.named_steps.keys()).index(at_step)
-
-    #             if isinstance(at_step, int):
-    #                 if abs(at_step) >= len(pipeline.steps):
-    #                     raise ValueError(
-    #                         "at_step must be strictly smaller in magnitude than the "
-    #                         "number of steps in the Pipeline, which is "
-    #                         f"{len(pipeline.steps)}; got {at_step}"
-    #                     )
-    #                 feature_engineering, estimator = (
-    #                     pipeline[:at_step],
-    #                     pipeline[at_step:],
-    #                 )
-    #                 X_transformed = feature_engineering.transform(X_)
-
-    #         feature_names = _get_feature_names(
-    #             estimator,
-    #             n_features=_num_features(X_transformed),
-    #             X=X_transformed,
-    #             transformer=feature_engineering,
-    #         )
-
-    #         if issparse(X_transformed):
-    #             X_transformed = X_transformed.todense()
-
-    #         sklearn_score = permutation_importance(
-    #             estimator=estimator,
-    #             X=X_transformed,
-    #             y=y_true,
-    #             scoring=checked_metric,
-    #             n_repeats=n_repeats,
-    #             n_jobs=n_jobs,
-    #             random_state=seed,
-    #             max_samples=max_samples,
-    #         )
-    #         score = sklearn_score.get("importances")
-
-    #         # If there is more than one metric
-    #         if score is None:
-    #             data = np.concatenate(
-    #                 [v["importances"] for v in sklearn_score.values()]
-    #             )
-    #             index = pd.MultiIndex.from_product(
-    #                 [sklearn_score, feature_names], names=("Metric", "Feature")
-    #             )
-    #         else:
-    #             data = score
-
-    #             # Get metric name
-    #             if metric is None:
-    #                 if is_classifier(estimator):
-    #                     metric_name = "accuracy"
-    #                 elif is_regressor(estimator):
-    #                     metric_name = "r2"
-    #             else:
-    #                 # e.g. if metric is a callable
-    #                 metric_name = None
-
-    #                 # no other cases to deal with explicitly, because
-    #                 # metric cannot possibly be a string at this stage
-
-    #             if metric_name is None:
-    #                 index = pd.Index(feature_names, name="Feature")
-    #             else:
-    #                 index = pd.MultiIndex.from_product(
-    #                     [[metric_name], feature_names], names=("Metric", "Feature")
-    #                 )
-
-    #         n_repeats = data.shape[1]
-    #         columns = pd.Index(
-    #             (f"Repeat #{i}" for i in range(n_repeats)), name="Repeat"
-    #         )
-    #         score = pd.DataFrame(data=data, index=index, columns=columns)
-
-    #         if cache_key is not None:
-    #             # NOTE: for the moment, we will always store the permutation importance
-    #             self._parent._cache[cache_key] = score
-
-    #     if aggregate:
-    #         if isinstance(aggregate, str):
-    #             aggregate = [aggregate]
-    #         score = score.aggregate(func=aggregate, axis=1)
-
-    #     if flat_index and isinstance(score.index, pd.MultiIndex):
-    #         score.index = flatten_multi_index(score.index)
-
-    #     return score
+            return display
 
     ####################################################################################
     # Methods related to the help tree
