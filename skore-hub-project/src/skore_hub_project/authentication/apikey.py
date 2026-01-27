8bdeacefd7a88d273fab8c2138009c16e96ce9be
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-01-27 17:17:46 +0100

    [ci skip] WIP

--- skore-hub-project/src/skore_hub_project/authentication/apikey.py
+++ skore-hub-project/src/skore_hub_project/authentication/apikey.py
@@ -2,8 +2,11 @@
 
 from collections.abc import Callable
 from os import environ
+from typing import Final
+
+ENVARNAME: Final[str] = "SKORE_HUB_API_KEY"
 
 
 def APIKey() -> Callable[[], dict[str, str]]:
     """API key used for ``skore hub`` authentication, as HTTP header parameters."""
-    return lambda: {"X-API-Key": environ["SKORE_HUB_API_KEY"]}  # when is evaluate ?!
+    return lambda: {"X-API-Key": environ[ENVARNAME]}  # when is evaluate ?!
--- skore-hub-project/src/skore_hub_project/authentication/login.py
+++ skore-hub-project/src/skore_hub_project/authentication/login.py
@@ -2,6 +2,13 @@
 
 from collections.abc import Callable
 
+#
+# Global variable storing credentials used for authentication by the ``HUBClient``, both
+# with an API key or a temporary token.
+#
+# By default, it is empty and must be initialized by the user by calling explicitly the
+# function ``login``.
+#
 credentials: Callable[[], dict[str, str]] | None = None
 
 
@@ -17,6 +24,9 @@ def login(*, timeout: int = 600) -> None:
         credentials = APIKey()
     except KeyError:
         console.rule("[cyan]Skore Hub[/cyan]")
-        console.print(f"", soft_wrap=True)
+        console.print("BAD", soft_wrap=True)
 
         credentials = Token(timeout=timeout)
+    else:
+        console.rule("[cyan]Skore Hub[/cyan]")
+        console.print("GOOD", soft_wrap=True)
