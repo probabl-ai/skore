b84239025913454848a54d41e35b8f9824560f6b
Nouman Shakeel <noumannorm648@gmail.com>
2026-02-06 02:11:54 +0500

    docs: add minimal working example to demonstrate contribution

---
+++ examples/quick_example.py
@@ -0,0 +1,7 @@
+print("Hello Skore! This example runs without any imports.")
+
+# If you want, you can also include a tiny dummy function
+def add(a, b):
+    return a + b
+
+print("2 + 3 =", add(2, 3))
