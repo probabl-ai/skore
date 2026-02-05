e88b56c136ddbb2d55c20a880a1b79f98b5c0b62
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-02-05 17:41:05 +0100

    [ci skip] rename feature_importance to inspection and all underlying functions

--- skore-hub-project/src/skore_hub_project/artifact/media/inspection.py
+++ skore-hub-project/src/skore_hub_project/artifact/media/inspection.py
@@ -1,64 +1,57 @@
-"""Definition of the payload used to associate feature importance media with report."""
+"""Definition of the payload used to associate inspection media with report."""
 
 from __future__ import annotations
 
 from abc import ABC
 from collections.abc import Callable
 from functools import reduce
-from typing import TYPE_CHECKING, ClassVar, Literal, cast
+from typing import ClassVar, Literal, cast
 
 from orjson import OPT_NON_STR_KEYS, OPT_SERIALIZE_NUMPY, dumps
 
 from skore_hub_project.artifact.media.media import Media, Report
 from skore_hub_project.protocol import Display, EstimatorReport
 
-if TYPE_CHECKING:
-    from pandas import DataFrame
 
-
-class FeatureImportance(Media[Report], ABC):  # noqa: D101
+class Inspection(Media[Report], ABC):  # noqa: D101
     accessor: ClassVar[str]
     content_type: Literal["application/vnd.dataframe"] = "application/vnd.dataframe"
 
     def content_to_upload(self) -> bytes | None:  # noqa: D102
         try:
             function = cast(
-                "Callable[..., Display | DataFrame]",
+                "Callable[..., Display]",
                 reduce(getattr, self.accessor.split("."), self.report),
             )
         except AttributeError:
             return None
 
-        result = function()
-
-        if isinstance(result, Display):
-            result = result.frame()
+        display = function()
+        frame = display.frame()
 
         return dumps(
-            result.fillna("NaN").to_dict(orient="tight"),
+            frame.fillna("NaN").to_dict(orient="tight"),
             option=(OPT_NON_STR_KEYS | OPT_SERIALIZE_NUMPY),
         )
 
 
-class Permutation(FeatureImportance[EstimatorReport], ABC):  # noqa: D101
-    accessor: ClassVar[str] = "feature_importance.permutation"
-    name: Literal["permutation"] = "permutation"
+class PermutationImportance(Inspection[EstimatorReport], ABC):  # noqa: D101
+    accessor: ClassVar[str] = "inspection.permutation_importance"
+    name: Literal["permutation_importance"] = "permutation_importance"
 
     def content_to_upload(self) -> bytes | None:  # noqa: D102
         for key, obj in reversed(list(self.report._cache.items())):
-            if len(key) < 7:
+            if len(key) < 6:
                 continue
 
-            if len(key) == 7:
-                parent_hash, metric, data_source, scoring, *_ = key
-            else:
-                parent_hash, metric, data_source, data_source_hash, scoring, *_ = key
+            parent_hash, name, data_source, at_step, _, metric, *_ = key
 
             if (
                 parent_hash == self.report._hash
-                and metric == "permutation_importance"
+                and name == "permutation_importance"
                 and data_source == self.data_source
-                and scoring is None
+                and at_step == 0
+                and metric is None
             ):
                 return dumps(
                     obj.fillna("NaN").to_dict(orient="tight"),
@@ -68,21 +61,21 @@ class Permutation(FeatureImportance[EstimatorReport], ABC):  # noqa: D101
         return None
 
 
-class PermutationTrain(Permutation):  # noqa: D101
+class PermutationImportanceTrain(PermutationImportance):  # noqa: D101
     data_source: Literal["train"] = "train"
 
 
-class PermutationTest(Permutation):  # noqa: D101
+class PermutationImportanceTest(PermutationImportance):  # noqa: D101
     data_source: Literal["test"] = "test"
 
 
-class MeanDecreaseImpurity(FeatureImportance[EstimatorReport]):  # noqa: D101
-    accessor: ClassVar[str] = "feature_importance.mean_decrease_impurity"
-    name: Literal["mean_decrease_impurity"] = "mean_decrease_impurity"
+class ImpurityDecrease(Inspection[EstimatorReport]):  # noqa: D101
+    accessor: ClassVar[str] = "inspection.impurity_decrease"
+    name: Literal["impurity_decrease"] = "impurity_decrease"
     data_source: None = None
 
 
-class Coefficients(FeatureImportance[Report]):  # noqa: D101
-    accessor: ClassVar[str] = "feature_importance.coefficients"
+class Coefficients(Inspection[Report]):  # noqa: D101
+    accessor: ClassVar[str] = "inspection.coefficients"
     name: Literal["coefficients"] = "coefficients"
     data_source: None = None
