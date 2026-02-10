4eea1d28e2dc85ec0db20883f6038bd197949fa3
GaetandeCast <gaetan@probabl.ai>
2026-02-10 17:29:41 +0100

    fix

--- skore/src/skore/_sklearn/_plot/inspection/coefficients.py
+++ skore/src/skore/_sklearn/_plot/inspection/coefficients.py
@@ -441,11 +441,19 @@ class CoefficientsDisplay(DisplayMixin):
         add_background_features = hue is not None
 
         self.figure_, self.ax_ = self.facet_.figure, self.facet_.axes.squeeze()
-        for ax, col_value in zip(self.ax_.flatten(), frame[col].unique(), strict=True):
+        n_features = (
+            [frame["feature"].nunique()]
+            if col is None
+            else [
+                frame.query(f"{col} == '{col_value}'")["feature"].nunique()
+                for col_value in frame[col].unique()
+            ]
+        )
+        for ax, n_feature in zip(self.ax_.flatten(), n_features, strict=True):
             _decorate_matplotlib_axis(
                 ax=ax,
                 add_background_features=add_background_features,
-                n_features=frame.query(f"{col} == '{col_value}'")["feature"].nunique(),
+                n_features=n_feature,
                 xlabel="Magnitude of coefficient",
                 ylabel="",
             )
