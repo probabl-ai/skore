33e077c3e7c2a9ebdf9d799e072484e8582309fd
GaetandeCast <gaetan@probabl.ai>
2026-02-10 17:16:02 +0100

    fix(skore): different y axis for comparisons in CoefficientsDisplay

--- skore/src/skore/_sklearn/_plot/inspection/coefficients.py
+++ skore/src/skore/_sklearn/_plot/inspection/coefficients.py
@@ -441,11 +441,11 @@ class CoefficientsDisplay(DisplayMixin):
         add_background_features = hue is not None
 
         self.figure_, self.ax_ = self.facet_.figure, self.facet_.axes.squeeze()
-        for ax in self.ax_.flatten():
+        for ax, col_value in zip(self.ax_.flatten(), frame[col].unique(), strict=True):
             _decorate_matplotlib_axis(
                 ax=ax,
                 add_background_features=add_background_features,
-                n_features=frame["feature"].nunique(),
+                n_features=frame.query(f"{col} == '{col_value}'")["feature"].nunique(),
                 xlabel="Magnitude of coefficient",
                 ylabel="",
             )
@@ -656,7 +656,7 @@ class CoefficientsDisplay(DisplayMixin):
             col=col,
             barplot_kwargs={"sharey": has_same_features} | barplot_kwargs,
             boxplot_kwargs=boxplot_kwargs,
-            stripplot_kwargs=stripplot_kwargs,
+            stripplot_kwargs={"sharey": has_same_features} | stripplot_kwargs,
         )
 
         title = "Coefficients"
