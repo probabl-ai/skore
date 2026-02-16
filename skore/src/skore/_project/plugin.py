ea1cc73088f5e571ccde042930dcd579cac7a6bc
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-02-16 16:21:40 +0100

    Add plugin docstr

--- skore/src/skore/_project/plugin.py
+++ skore/src/skore/_project/plugin.py
@@ -1,5 +1,7 @@
+"""Tools used to interact with ``skore`` plugin."""
+
 from importlib.metadata import entry_points
-from typing import get_args
+from typing import Any, get_args
 
 from skore._project.types import PluginGroup, ProjectMode
 
@@ -7,7 +9,42 @@ GROUPS = get_args(PluginGroup)
 MODES = get_args(ProjectMode)
 
 
-def get(*, group: PluginGroup, mode: ProjectMode):
+def get(*, group: PluginGroup, mode: ProjectMode) -> Any:
+    """
+    Load and return a ``skore`` plugin implementation for the given group and mode.
+
+    There are currently two types of plugins allowed:
+    - the classes implementing the ``Project`` API, registered under the
+      ``skore.plugins.project`` group,
+    - the functions used to login, registered under the ``skore.plugins.login`` group.
+
+    This function uses internally the python entry points mechanism: each package
+    compatible with ``skore`` could expose its own plugins, as long as they are
+    registered in the right groups and comply with APIs.
+
+    It is usually used to retrieve the plugins exposed by ``skore-local-project``
+    or ``skore-hub-project``.
+
+    Parameters
+    ----------
+    group : PluginGroup
+        The group of plugin to search for. Must be one of:
+        - "skore.plugins.project"
+        - "skore.plugins.login"
+
+    mode : ProjectMode
+        The project mode used to select the plugin implementation.
+        Must be one of:
+        - "hub"
+        - "local"
+
+    Returns
+    -------
+    Any
+        The loaded plugin object corresponding to the given group and mode.
+        The exact return type depends on the registered plugin implementation: class or
+        function.
+    """
     assert group in GROUPS, f"`group` must be in {GROUPS} (found {group})"
     assert mode in MODES, f"`mode` must be in {MODES} (found {mode})"
 
