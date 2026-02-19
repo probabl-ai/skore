145a094fa34b18bb4cd9e7b3441ab9d373f370ca
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-02-19 15:38:23 +0100

    add docstr to delayed

--- skore/src/skore/_utils/_parallel.py
+++ skore/src/skore/_utils/_parallel.py
@@ -7,6 +7,24 @@ from skore._config import LocalConfiguration
 
 
 def delayed(function):
+    """Capture the arguments of a function to delay its execution.
+
+    This alternative to ``joblib.delayed`` is meant to be used in conjunction
+    with ``joblib.Parallel``.
+
+    It ensures each task has its own copy of the ``skore`` configuration.
+
+    Parameters
+    ----------
+    function : callable
+        The function to be delayed.
+
+    Returns
+    -------
+    output: tuple
+        Tuple containing the delayed function, the positional arguments, and the
+        keyword arguments.
+    """
     local = configuration.local.__dict__.copy()
 
     @wraps(function)
@@ -24,5 +42,11 @@ class _FuncWrapper:
         update_wrapper(self, self.function)
 
     def __call__(self, *args, **kwargs):
+        # `joblib.Parallel` is capable of reusing a process/thread for multiple tasks,
+        # so we must reset the configuration to its initial state before executing each
+        # task.
+        #
+        # This way, we ensure that there is no side-effect between tasks executed on the
+        # same computing unit.
         configuration.local = LocalConfiguration(**self.local)
         return self.function(*args, **kwargs)
