c3a0ee02524a91fef5dfbdbe3074049181861f79
GaetandeCast <gaetan@probabl.ai>
2026-02-20 17:34:51 +0100

    tests

--- skore/tests/unit/displays/permutation_importance/test_comparison_cross_validation.py
+++ skore/tests/unit/displays/permutation_importance/test_comparison_cross_validation.py
@@ -4,108 +4,116 @@ from sklearn.metrics import make_scorer, precision_score, r2_score
 
 
 @pytest.mark.parametrize(
-    "fixture_name, valid_values",
+    "task",
     [
-        (
-            "comparison_cross_validation_reports_binary_classification",
-            ["estimator", "split", "auto", "None"],
-        ),
-        (
-            "comparison_cross_validation_reports_multiclass_classification",
-            ["estimator", "split", "auto", "None"],
-        ),
-        (
-            "comparison_cross_validation_reports_regression",
-            ["estimator", "split", "auto", "None"],
-        ),
-        (
-            "comparison_cross_validation_reports_multioutput_regression",
-            ["estimator", "split", "auto", "None"],
-        ),
+        "binary_classification",
+        "multiclass_classification",
+        "regression",
+        "multioutput_regression",
     ],
 )
-def test_invalid_subplot_by(pyplot, fixture_name, valid_values, request):
-    report = request.getfixturevalue(fixture_name)
+def test_invalid_subplot_by(pyplot, task, request):
+    report = request.getfixturevalue(f"comparison_cross_validation_reports_{task}")
     display = report.inspection.permutation_importance(seed=0, n_repeats=2)
     err_msg = (
         "The column 'incorrect' is not available for subplotting. You can use "
-        f"the following values to create subplots: {', '.join(valid_values)}"
+        f"the following values to create subplots: {
+            ', '.join(
+                ['estimator', 'split', 'auto', 'None'],
+            )
+        }"
     )
     with pytest.raises(ValueError, match=err_msg):
         display.plot(subplot_by="incorrect")
 
 
-def test_valid_subplot_by(pyplot, comparison_cross_validation_reports_regression):
-    report = comparison_cross_validation_reports_regression
+@pytest.mark.parametrize(
+    "task",
+    [
+        "binary_classification",
+        "multiclass_classification",
+        "regression",
+        "multioutput_regression",
+    ],
+)
+@pytest.mark.parametrize(
+    "subplot_by, expected_len",
+    [
+        ("estimator", 2),
+        ("split", 2),
+        ("auto", 0),
+        (None, 0),
+    ],
+)
+def test_valid_subplot_by(pyplot, task, subplot_by, expected_len, request):
+    report = request.getfixturevalue(f"comparison_cross_validation_reports_{task}")
     display = report.inspection.permutation_importance(seed=0, n_repeats=2)
-    display.plot(subplot_by=None)
-    assert isinstance(display.ax_, mpl.axes.Axes)
-
-    display.plot(subplot_by="split")
-    assert len(display.ax_) == 2
-
-    display.plot(subplot_by="estimator")
-    assert len(display.ax_) == 2
+    display.plot(subplot_by=subplot_by)
+    if expected_len == 0:
+        assert isinstance(display.ax_, mpl.axes.Axes)
+    else:
+        assert len(display.ax_.flatten()) == expected_len
 
 
 @pytest.mark.parametrize(
-    "fixture_name, metric, metric_name, subplot_by",
+    "task",
     [
-        (
-            "comparison_cross_validation_reports_multiclass_classification",
-            make_scorer(precision_score, average=None),
-            "precision score",
-            "label",
-        ),
-        (
-            "comparison_cross_validation_reports_multioutput_regression",
-            make_scorer(r2_score, multioutput="raw_values"),
-            "r2 score",
-            "output",
-        ),
+        "binary_classification",
+        "multiclass_classification",
+        "regression",
+        "multioutput_regression",
     ],
 )
-def test_subplot_by_none_multiclass_or_multioutput(
-    pyplot, fixture_name, metric, metric_name, subplot_by, request
-):
-    report = request.getfixturevalue(fixture_name)
-    display = report.inspection.permutation_importance(
-        seed=0, n_repeats=2, metric=metric
+def test_different_features(pyplot, task, request):
+    report = request.getfixturevalue(
+        f"comparison_cross_validation_reports_{task}_different_features"
     )
-    err_msg = (
-        "There are multiple labels or outputs and `subplot_by` is `None`. "
-        "There is too much information to display on a single plot. "
-        "Please provide a column to group by using `subplot_by`."
-    )
-    with pytest.raises(ValueError, match=err_msg):
-        display.plot(metric=metric_name, subplot_by=None)
-
-    display.plot(metric=metric_name, subplot_by=subplot_by)
-    assert len(display.ax_) > 1
-
-
-@pytest.mark.parametrize(
-    "fixture_name, subplot_by",
-    [
-        (
-            "comparison_cross_validation_reports_binary_classification_different_features",
-            None,
-        ),
-        (
-            "comparison_cross_validation_reports_multiclass_classification_different_features",
-            "split",
-        ),
-    ],
-)
-def test_different_features(pyplot, fixture_name, subplot_by, request):
-    report = request.getfixturevalue(fixture_name)
     display = report.inspection.permutation_importance(seed=0, n_repeats=2)
     err_msg = (
         "The estimators have different features and should be plotted on different "
         "axis using `subplot_by='estimator'`."
     )
-    with pytest.raises(ValueError, match=err_msg):
-        display.plot(subplot_by=subplot_by)
+    for subplot_by in [None, "split"]:
+        with pytest.raises(ValueError, match=err_msg):
+            display.plot(subplot_by=subplot_by)
 
     display.plot(subplot_by="estimator")
-    assert hasattr(display, "facet_")
+    assert len(display.ax_) == 2
+
+
+@pytest.mark.parametrize(
+    "task, metric, metric_name, subplot_by, expected_len",
+    [
+        (
+            "multiclass_classification",
+            make_scorer(precision_score, average=None),
+            "precision score",
+            "label",
+            3,
+        ),
+        (
+            "multioutput_regression",
+            make_scorer(r2_score, multioutput="raw_values"),
+            "r2 score",
+            "output",
+            2,
+        ),
+    ],
+)
+def test_subplot_by_non_averaged_metrics(
+    pyplot, task, metric, metric_name, subplot_by, expected_len, request
+):
+    report = request.getfixturevalue(f"comparison_cross_validation_reports_{task}")
+    display = report.inspection.permutation_importance(
+        n_repeats=2, seed=0, metric=metric
+    )
+    display.plot(metric=metric_name, subplot_by=subplot_by)
+    assert len(display.ax_) == expected_len
+
+    valid_values = ["estimator", subplot_by, "split", "auto", "None"]
+    err_msg = (
+        f"The column 'invalid' is not available for subplotting. You can use the "
+        f"following values to create subplots: {', '.join(valid_values)}"
+    )
+    with pytest.raises(ValueError, match=err_msg):
+        display.plot(subplot_by="invalid")
