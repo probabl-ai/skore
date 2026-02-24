f2a2c7dad957fd954215394c64f58d0420c8b4c4
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-02-24 22:08:28 +0100

    prefer environ to epilog

--- examples/technical_details/plot_skore_hub_project.py
+++ examples/technical_details/plot_skore_hub_project.py
@@ -10,22 +10,39 @@ reports remotely and inspect them. A key point is that
 :meth:`~skore.Project.summarize` returns a :class:`~skore.project._summary.Summary`,
 which is a :class:`pandas.DataFrame`. In Jupyter you get an interactive widget, but
 you can always inspect and filter the summary as a DataFrame if you prefer.
+
+Examples
+--------
+
+Basic usage:
+
+.. code-block:: bash
+
+    WORKSPACE=<workspace> PROJECT=<project> python plot_skore_hub_project.py
 """
 
 # %%
 # .. testsetup::
+from logging import getLogger
 from os import environ
 from sys import exit
 
-from skore import Project
+logger = getLogger(__name__)
 
-if ("SPHINX_BUILD" in environ) and ("SKORE_HUB_API_KEY" not in environ):
-    exit(0)
+if environ.get("SPHINX_BUILD"):
+    GITHUB = environ.get("GITHUB_ACTIONS")
+    API_KEY = environ.get("SPHINX_EXAMPLE_API_KEY")
+    WORKSPACE = environ.get("SPHINX_EXAMPLE_WORKSPACE")
+    PROJECT = environ.get("SPHINX_EXAMPLE_PROJECT")
 
-WORKSPACE = environ["SPHINX_EXAMPLE_WORKSPACE"]
-PROJECT = environ["SPHINX_EXAMPLE_PROJECT"]
+    if not (GITHUB and API_KEY and WORKSPACE and PROJECT):
+        logger.warning("Example `_example_skore_hub_project` skipped.")
+        exit(0)
 
-Project.delete(f"{WORKSPACE}/{PROJECT}", mode="hub")
+    environ["SKORE_HUB_API_KEY"] = API_KEY
+else:
+    assert (WORKSPACE := environ.get("WORKSPACE")), "`WORKSPACE` must be defined."
+    assert (PROJECT := environ.get("PROJECT")), "`PROJECT` must be defined."
 
 # %%
 from skore import login
@@ -35,4 +52,9 @@ login(mode="hub")
 # %%
 from skore import Project
 
-project = Project(f"{WORKSPACE}/{PROJECT}", mode="hub")
+Project.delete(f"{WORKSPACE}/{PROJECT}", mode="hub")
+
+# %%
+from skore import Project
+
+Project(f"{WORKSPACE}/{PROJECT}", mode="hub")
--- sphinx/conf.py
+++ sphinx/conf.py
@@ -28,12 +28,6 @@ version = os.environ["SPHINX_VERSION"]
 release = os.environ["SPHINX_RELEASE"]
 domain = os.environ["SPHINX_DOMAIN"]
 
-
-# Expose variables to RST
-rst_epilog = f"""
-.. |example_project_url| replace:: {hub_example_url}
-"""
-
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
