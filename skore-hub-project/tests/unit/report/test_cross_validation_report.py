f96c56215270e712d8ca0223d039fd312c8ce277
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-02-26 16:18:41 +0100

    feat(skore-hub-project/put): Send confusion matrix

--- skore-hub-project/tests/unit/report/test_cross_validation_report.py
+++ skore-hub-project/tests/unit/report/test_cross_validation_report.py
@@ -10,6 +10,10 @@ from sklearn.model_selection import KFold, RepeatedKFold
 from skore import CrossValidationReport, EstimatorReport
 
 from skore_hub_project.artifact.media import (
+    ConfusionMatrixDataFrameTest,
+    ConfusionMatrixDataFrameTrain,
+    ConfusionMatrixSVGTest,
+    ConfusionMatrixSVGTrain,
     EstimatorHtmlRepr,
     ImpurityDecrease,
     PermutationImportanceTest,
@@ -381,6 +385,10 @@ class TestCrossValidationReportPayload:
     @mark.respx()
     def test_medias(self, payload):
         assert list(map(type, payload.medias)) == [
+            ConfusionMatrixDataFrameTest,
+            ConfusionMatrixDataFrameTrain,
+            ConfusionMatrixSVGTest,
+            ConfusionMatrixSVGTrain,
             EstimatorHtmlRepr,
             ImpurityDecrease,
             PermutationImportanceTest,
