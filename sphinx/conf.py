314b7cf20a155c35a65e498c2c80f8bd613a1f27
Camille Troillard <cam@probabl.ai>
2026-03-18 09:14:20 +0100

    chore: Replace GTM script with our own (#2594)

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -89,8 +89,7 @@ html_static_path = ["_static"]
 
 html_css_files = ["css/custom.css"]
 html_js_files = [
-    "js/sg_plotly_resize.js",
-    "js/gtm.js",
+    "js/sg_plotly_resize.js"
 ]
 
 # sphinx_gallery options
