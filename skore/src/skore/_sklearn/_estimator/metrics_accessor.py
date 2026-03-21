9f5cc8c109f9ae7f0d1fcb82436a74533cde37e5
Direk Kakkar <millioniar1717@gmail.com>
2026-03-21 18:47:59 +0530

    added the _METRIC_ALIASES dictionary to below _score_or_loss_info  dictionary

--- skore/src/skore/_sklearn/_estimator/metrics_accessor.py
+++ skore/src/skore/_sklearn/_estimator/metrics_accessor.py
@@ -57,6 +57,18 @@ class _MetricsAccessor(
         "confusion_matrix": {"name": "Confusion Matrix", "icon": ""},
     }
 
+    _METRIC_ALIASES: dict[str, str] = {
+        "mean_squared_error": "neg_meansquared_error",
+        "mean_absolute_error": "neg_mean_absolute_error",
+        "root_mean_squared_error": "neg_root_mean_squared_error",
+        "mean_absolute_percentage_error": "neg_mean_absolute_percentage_error",
+        "median_absolute_error": "neg_median_absolute_error",
+        "log_loss": "neg_log_loss",
+        "brier_score": "neg_brier_score",
+        "mean_poisson_deviance": "neg_mean_poisson_deviance",
+        "mean_gamma_deviance": "neg_mean_gamma_deviance",
+    }
+
     def __init__(self, parent: EstimatorReport) -> None:
         super().__init__(parent)
 
