1ecfc861632f5ef981b407d723e01c29575e6a7f
Arthur <arthur.lcte@gmail.com>
2026-03-25 12:06:10 +0100

    temp. disable sphinx parral.

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -115,7 +115,7 @@ sphinx_gallery_conf = {
     "doc_module": "skore",
     # "reset_modules": (reset_mpl, "seaborn"),
     "abort_on_example_error": True,
-    "parallel": True,
+    "parallel": False,
 }
 
 # Build the HUB examples conditionally, only when credentials are available:
