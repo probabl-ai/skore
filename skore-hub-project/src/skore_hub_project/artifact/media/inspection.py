7b19e3e89f76aee2313c7665d4abfc8b17995c2f
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-03-26 13:42:00 +0100

    ensure back and forward compatibility

--- skore-hub-project/src/skore_hub_project/artifact/media/inspection.py
+++ skore-hub-project/src/skore_hub_project/artifact/media/inspection.py
@@ -5,9 +5,11 @@ from __future__ import annotations
 from abc import ABC
 from collections.abc import Callable
 from functools import reduce
+from importlib import metadata
 from inspect import signature
 from typing import ClassVar, Literal, cast
 
+from packaging.version import Version
 from pandas import concat
 
 from skore_hub_project.artifact.media.media import Media, Report
@@ -45,71 +47,101 @@ class PermutationImportance(Inspection[Report], ABC):  # noqa: D101
     accessor: ClassVar[str] = "inspection.permutation_importance"
     name: Literal["permutation_importance"] = "permutation_importance"
 
-    @staticmethod
-    def __display_from_cross_validation_report(
-        data_source: str | None,
-        report: CrossValidationReport,
-    ) -> Display | None:
-        from skore import PermutationImportanceDisplay
+    if Version("0.0.1") < Version(metadata.version("skore")) <= Version("0.14"):
 
-        frames = []
+        def content_to_upload(self) -> bytes | None:  # noqa: D102
+            for key, display in reversed(list(self.report._cache.items())):  # type: ignore[union-attr]
+                if len(key) < 6:
+                    continue
 
-        for i, display in enumerate(
-            PermutationImportance.__display_from_estimator_report(data_source, report)
-            for report in report.estimator_reports_
-        ):
+                parent_hash, name, data_source, at_step, _, metric, *_ = key
+
+                if (
+                    parent_hash == self.report._hash
+                    and name == "permutation_importance"
+                    and data_source == self.data_source
+                    and at_step == 0
+                    and metric is None
+                ):
+                    frame = display.frame(aggregate=None)
+
+                    return dumps(
+                        frame.astype(object)
+                        .where(frame.notna(), "NaN")
+                        .to_dict(orient="tight")
+                    )
+
+            return None
+
+    else:
+
+        @staticmethod
+        def __display_from_cross_validation_report(
+            data_source: str | None,
+            report: CrossValidationReport,
+        ) -> Display | None:
+            from skore import PermutationImportanceDisplay
+
+            frames = []
+
+            for i, display in enumerate(
+                PermutationImportance.__display_from_estimator_report(
+                    data_source, report
+                )
+                for report in report.estimator_reports_
+            ):
+                if display is None:
+                    return None
+
+                frames.append(display.importances.assign(split=i))  # type: ignore[attr-defined]
+
+            return cast(
+                Display,
+                PermutationImportanceDisplay(
+                    importances=concat(frames, ignore_index=True),
+                    report_type="cross-validation",
+                ),
+            )
+
+        @staticmethod
+        def __display_from_estimator_report(
+            data_source: str | None,
+            report: EstimatorReport,
+        ) -> Display | None:
+            for key, display in reversed(list(report._cache.items())):
+                if len(key) != 3:
+                    continue
+
+                data_source, name, kwargs = key
+                kwargs = str(kwargs)
+
+                if (
+                    data_source == data_source
+                    and name == "permutation_importance"
+                    and "('at_step', 0)" in kwargs
+                    and "('metric', None)" in kwargs
+                ):
+                    return cast(Display, display)
+
+            return None
+
+        def content_to_upload(self) -> bytes | None:  # noqa: D102
+            display = (
+                self.__display_from_estimator_report(self.data_source, self.report)
+                if isinstance(self.report, EstimatorReport)
+                else self.__display_from_cross_validation_report(
+                    self.data_source, self.report
+                )
+            )
+
             if display is None:
                 return None
 
-            frames.append(display.importances.assign(split=i))  # type: ignore[attr-defined]
+            frame = display.frame(aggregate=None)
 
-        return cast(
-            Display,
-            PermutationImportanceDisplay(
-                importances=concat(frames, ignore_index=True),
-                report_type="cross-validation",
-            ),
-        )
-
-    @staticmethod
-    def __display_from_estimator_report(
-        data_source: str | None,
-        report: EstimatorReport,
-    ) -> Display | None:
-        for key, display in reversed(list(report._cache.items())):
-            if len(key) != 3:
-                continue
-
-            data_source, name, kwargs = key
-            kwargs = str(kwargs)
-
-            if (
-                data_source == data_source
-                and name == "permutation_importance"
-                and "('at_step', 0)" in kwargs
-                and "('metric', None)" in kwargs
-            ):
-                return cast(Display, display)
-
-        return None
-
-    def content_to_upload(self) -> bytes | None:  # noqa: D102
-        display = (
-            self.__display_from_estimator_report(self.data_source, self.report)
-            if isinstance(self.report, EstimatorReport)
-            else self.__display_from_cross_validation_report(
-                self.data_source, self.report
+            return dumps(
+                frame.astype(object).where(frame.notna(), "NaN").to_dict(orient="tight")
             )
-        )
-
-        if display is None:
-            return None
-
-        frame = display.frame(aggregate=None)
-
-        return dumps(
-            frame.astype(object).where(frame.notna(), "NaN").to_dict(orient="tight")
-        )
 
 
 class PermutationImportanceTrain(PermutationImportance[Report]):  # noqa: D101
