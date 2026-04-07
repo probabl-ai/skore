a9978761c416ba351149a6a12fa3323057405e1e
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-04-07 11:56:36 +0200

    backport `hashlib.file_digest` from python==3.14 to python<3.11

--- skore-hub-project/src/skore_hub_project/artifact/serializer.py
+++ skore-hub-project/src/skore_hub_project/artifact/serializer.py
@@ -3,10 +3,44 @@
 from __future__ import annotations
 
 from functools import cached_property
-from hashlib import file_digest
+from hashlib import blake2b
 from pathlib import Path
+from sys import version_info
 from tempfile import NamedTemporaryFile
 from typing import Any
+
+if version_info >= (3, 11):
+    from hashlib import file_digest
+else:
+
+    def file_digest(fileobj, digest, /, *, _bufsize=2**18):  # type: ignore
+        """Backported from https://github.com/python/cpython/blob/3.14/Lib/hashlib.py#L195."""
+        digestobj = digest()
+
+        if hasattr(fileobj, "getbuffer"):
+            digestobj.update(fileobj.getbuffer())
+            return digestobj
+
+        if not (
+            hasattr(fileobj, "readinto")
+            and hasattr(fileobj, "readable")
+            and fileobj.readable()
+        ):
+            raise ValueError(
+                f"'{fileobj!r}' is not a file-like object in binary reading mode."
+            )
+
+        buf = bytearray(_bufsize)  # Reusable buffer to reduce allocations.
+        view = memoryview(buf)
+        while True:
+            size = fileobj.readinto(buf)
+            if size is None:
+                raise BlockingIOError("I/O operation would block.")
+            if size == 0:
+                break  # EOF
+            digestobj.update(view[:size])
+
+        return digestobj
 
 
 class Serializer:
@@ -34,7 +68,7 @@ class Serializer:
     def checksum(self) -> str:
         """The checksum of the serialized content using `BLAKE2b`."""
         with open(self.filepath, "rb") as file:
-            digest = file_digest(file, "blake2b")
+            digest = file_digest(file, blake2b)
 
         return f"blake2b-{digest.hexdigest()}"
 
