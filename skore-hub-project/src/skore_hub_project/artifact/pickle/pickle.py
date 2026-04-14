c284ea26fa761153e8aa22d1ae59a8f8fa0da110
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-04-14 15:41:29 +0200

    [ci skip] WIP

--- skore-hub-project/src/skore_hub_project/artifact/pickle/pickle.py
+++ skore-hub-project/src/skore_hub_project/artifact/pickle/pickle.py
@@ -49,4 +49,16 @@ class Pickle(Artifact):
             return
 
         self.computed = True
-        dump(self.report, self.filepath)
+
+        reports = [self.report] + getattr(self.report, "estimator_reports_", [])
+        reports_with_cache = [
+            (report, report._cache) for report in reports if hasattr(report, "_cache")
+        ]
+
+        self.report.clear_cache()
+
+        try:
+            dump(self.report, self.filepath)
+        finally:
+            for report, cache in reports_with_cache:
+                report._cache = cache
