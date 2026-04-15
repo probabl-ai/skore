db222ab19707545fcce79566ff7ec7050f0aa6f3
Thomas J. Fan <thomasjpfan@gmail.com>
2026-04-15 18:39:15 +0000

    Fix lint

--- skore/src/skore/__init__.py
+++ skore/src/skore/__init__.py
@@ -1,5 +1,6 @@
 """Configure logging and global settings."""
 
+from importlib.metadata import version
 from logging import INFO, NullHandler, getLogger
 from warnings import warn
 
@@ -94,8 +95,4 @@ skore_console_theme = Theme(
 
 
 console = Console(theme=skore_console_theme, width=88)
-
-
-from importlib.metadata import version
-
 __version__ = version("skore")
