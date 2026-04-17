a1061767a0060c035eb2c96895fa2d4faf524f8a
Arthur <arthur.lcte@gmail.com>
2026-04-17 22:43:54 +0200

    initial work

--- skore-local-project/src/skore_local_project/project.py
+++ skore-local-project/src/skore_local_project/project.py
@@ -4,9 +4,17 @@ from __future__ import annotations
 
 import io
 import os
+from collections.abc import Generator
 from functools import wraps
 from pathlib import Path
-from typing import TYPE_CHECKING, ParamSpec, Protocol, TypeVar, cast, runtime_checkable
+from typing import (
+    TYPE_CHECKING,
+    ParamSpec,
+    Protocol,
+    TypeVar,
+    cast,
+    runtime_checkable,
+)
 from uuid import uuid4
 
 import joblib
@@ -170,6 +178,36 @@ class Project:
         """The workspace of the project."""
         return self.__workspace
 
+    @staticmethod
+    def pickle_parts(
+        report: EstimatorReport | CrossValidationReport,
+    ) -> Generator[tuple[str, bytes], None, None]:
+        """
+        Pickle ``report``, return the bytes and the corresponding hash.
+
+        Notes
+        -----
+        The report is pickled without its cache, to avoid salting the hash.
+        """
+        state = report.get_state()
+        state_hashes = {}
+
+        for key, value in state.items():
+            with io.BytesIO() as stream:
+                joblib.dump(value, stream)
+                pickle_bytes = stream.getvalue()
+            pickle_hash = joblib.hash(pickle_bytes)
+            state_hashes[key] = pickle_hash
+            yield pickle_hash, pickle_bytes
+
+        state_hashes["__report_type__"] = report._report_type
+
+        with io.BytesIO() as stream:
+            joblib.dump(value, stream)
+            pickle_bytes = stream.getvalue()
+        pickle_hash = joblib.hash(pickle_bytes)
+        yield pickle_hash, pickle_bytes
+
     @ensure_project_is_not_deleted
     def put(self, key: str, report: EstimatorReport | CrossValidationReport) -> None:
         """
@@ -207,12 +245,10 @@ class Project:
                 f"ort` (found '{type(report)}')"
             )
 
-        artifact_id = str(report.id)
-
-        if artifact_id not in self.__artifacts_storage:
-            with io.BytesIO() as stream:
-                joblib.dump(report, stream)
-                self.__artifacts_storage[artifact_id] = stream.getvalue()
+        for artifact_id, artifact_bytes in self.pickle_parts(report):
+            if artifact_id in self.__artifacts_storage:
+                continue
+            self.__artifacts_storage[artifact_id] = artifact_bytes
 
         self.__metadata_storage[uuid4().hex] = dict(
             Metadata(
@@ -226,13 +262,27 @@ class Project:
     @ensure_project_is_not_deleted
     def get(self, id: str) -> EstimatorReport | CrossValidationReport:
         """Get a persisted report by its id."""
-        if id in self.__artifacts_storage:
-            with io.BytesIO(self.__artifacts_storage[id]) as stream:
-                return cast(
-                    "EstimatorReport | CrossValidationReport", joblib.load(stream)
-                )
+        if id not in self.__artifacts_storage:
+            raise KeyError(id)
 
-        raise KeyError(id)
+        with io.BytesIO(self.__artifacts_storage[id]) as stream:
+            state_hashes = joblib.load(stream)
+
+        if not isinstance(state_hashes, dict):
+            # BW compatibility
+            return cast("EstimatorReport" | "CrossValidationReport", state_hashes)
+
+        report_type = state_hashes.pop("__report_type__")
+
+        state = {}
+        for key, pickle_id in state_hashes.items():
+            with io.BytesIO(self.__artifacts_storage[pickle_id]) as stream:
+                state[key] = joblib.load(stream)
+
+        if report_type == "estimator":
+            return EstimatorReport.from_state(state)
+        else:
+            return CrossValidationReport.from_state(state)
 
     @ensure_project_is_not_deleted
     def summarize(self) -> list[Metadata]:
