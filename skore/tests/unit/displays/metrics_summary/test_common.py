4e7312cbd1fb7da7f9612a6c73c2a6580f3a483e
GaetandeCast <gaetan2c@gmail.com>
2026-06-05 11:10:32 +0200

    iter

--- skore/tests/unit/displays/metrics_summary/test_common.py
+++ skore/tests/unit/displays/metrics_summary/test_common.py
@@ -2,9 +2,6 @@
 
 from skore import EstimatorReport
 
-_FRAME_HINT = "Use .frame() to control the format of the output."
-_FRAME_HINT_HTML = "<code>.frame()</code>"
-
 
 def test_repr_includes_frame_and_hint(forest_binary_classification_with_test):
     """Check that __repr__ shows the default frame and a trailing hint."""
@@ -15,7 +12,7 @@ def test_repr_includes_frame_and_hint(forest_binary_classification_with_test):
 
     repr_str = repr(display)
     assert repr_str.startswith(repr(display.frame()))
-    assert repr_str.endswith(_FRAME_HINT)
+    assert repr_str.endswith("Use .frame() to control the format of the output.")
 
 
 def test_repr_html_includes_frame_and_hint(forest_binary_classification_with_test):
@@ -27,16 +24,4 @@ def test_repr_html_includes_frame_and_hint(forest_binary_classification_with_tes
 
     html = display._repr_html_()
     assert html.startswith(display.frame()._repr_html_())
-    assert _FRAME_HINT_HTML in html
-
-
-def test_repr_mimebundle(forest_binary_classification_with_test):
-    """Check that _repr_mimebundle_ returns text/plain and text/html."""
-    estimator, X_test, y_test = forest_binary_classification_with_test
-    display = EstimatorReport(
-        estimator, X_test=X_test, y_test=y_test
-    ).metrics.summarize()
-
-    bundle = display._repr_mimebundle_()
-    assert bundle["text/plain"] == repr(display)
-    assert bundle["text/html"] == display._repr_html_()
+    assert "Use <code>.frame()</code> to control the format of the output." in html
