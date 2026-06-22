fc273d26a5257102a4b9f7f3bafe2fb80b39caff
GaetandeCast <gaetan2c@gmail.com>
2026-06-22 11:57:39 +0200

    fix

--- skore/tests/unit/reports/comparison/estimator/test_report.py
+++ skore/tests/unit/reports/comparison/estimator/test_report.py
@@ -39,14 +39,6 @@ def test_different_test_data(
             ]
         )
 
-    # If there is an X_test but no y_test, it should not raise an error
-    ComparisonReport(
-        [
-            EstimatorReport(estimator, fit=False, X_test=X_test, y_test=None),
-            EstimatorReport(estimator, fit=False, X_test=X_test, y_test=None),
-        ]
-    )
-
 
 def test_init_different_ml_usecases(
     estimator_reports_binary_classification, estimator_reports_regression
