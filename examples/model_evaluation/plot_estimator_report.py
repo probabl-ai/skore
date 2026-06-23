221e1a710cd5bf1c0e9b1b4758a16987e168e525
Auguste Baum <auguste@probabl.ai>
2026-06-23 15:32:41 +0200

    docs(EstimatorReport): Clean up (#3082)

--- examples/model_evaluation/plot_estimator_report.py
+++ examples/model_evaluation/plot_estimator_report.py
@@ -14,8 +14,8 @@ quickly get insights from any scikit-learn estimator.
 # ==============================================
 #
 # First, we load a dataset from skrub. Our goal is to predict if a healthcare
-# manufacturing companies paid a medical doctors or hospitals, in order to detect
-# potential conflict of interest.
+# manufacturing company paid medical doctors or hospitals, in order to detect
+# potential conflicts of interest.
 
 # %%
 from skrub.datasets import fetch_open_payments
@@ -34,7 +34,7 @@ TableReport(y.to_frame())
 
 # %%
 # Looking at the distributions of the target, we observe that this classification
-# task is quite imbalanced. It means that we have to be careful when selecting a set
+# task is quite imbalanced. This means that we have to be careful when selecting a set
 # of statistical metrics to evaluate the classification performance of our predictive
 # model. In addition, we see that the class labels are not specified by an integer
 # 0 or 1 but instead by a string "allowed" or "disallowed".
@@ -43,7 +43,7 @@ TableReport(y.to_frame())
 pos_label, neg_label = "allowed", "disallowed"
 
 # %%
-# Now, we need to define a predictive model. Thankfully, `skrub` provides a convenient
+# Let's create a predictive model. Thankfully, `skrub` provides a convenient
 # function (:func:`skrub.tabular_pipeline`) when it comes to getting strong baseline
 # predictive models with a single line of code. As its feature engineering is generic,
 # it does not provide some handcrafted and tailored feature engineering but still
@@ -56,30 +56,25 @@ estimator = tabular_pipeline("classifier")
 estimator
 
 # %%
-# Getting insights from our estimator
-# ===================================
+# Introducing the :class:`EstimatorReport`
+# ========================================
 #
-# Introducing the :class:`skore.EstimatorReport` class
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#
-# Now, we would be interested in getting some insights from our predictive model.
-# One way is to use the :class:`skore.EstimatorReport` class which we will
-# construct using the `evaluate` function. This function will detect that our
-# estimator is unfitted and will fit it for us on the training data and return an
-# :class:`~skore.EstimatorReport` object.
-#
-# Specifying a `splitter` of 0.2 will perform a 80/20 train-test split.
+# Let's gather some insights from our predictive model.
+# We can use :func:`skore.evaluate` for this: the function will perform a train-test
+# split and create a :class:`~skore.EstimatorReport` containing the model fitted on
+# the training data, ready to investigate.
 
 from skore import evaluate
 
+# Reserve 20% of the data for the test set
 report = evaluate(estimator, X=df, y=y, pos_label=pos_label, splitter=0.2)
 report
 
 # %%
 #
 # Once the report is created, we get some information regarding the available tools
-# allowing us to get some insights from our specific model on our specific task by
-# calling the :meth:`~skore.EstimatorReport.help` method.
+# allowing us to get some insights on our model by calling the
+# :meth:`~skore.EstimatorReport.help` method.
 report.help()
 
 # %%
@@ -90,13 +85,11 @@ report.metrics.help()
 # %%
 #
 # Metrics computation with aggressive caching
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ===========================================
 #
-# At this point, we might be interested to have a first look at the statistical
-# performance of our model on the validation set that we provided. We can access it
-# by calling any of the metrics displayed above. Since we are greedy, we want to get
-# several metrics at once and we will use the
-# :meth:`~skore.EstimatorReport.metrics.summarize` method.
+# Let's have a first look at the statistical performance of our model. skore knows
+# that we are doing classification, and can give us an array of classic ML metrics,
+# all at once, with :meth:`~skore.EstimatorReport.metrics.summarize`:
 import time
 
 start = time.time()
@@ -133,7 +126,7 @@ report.metrics.timings()
 
 # %%
 #
-# Since we obtain a pandas dataframe, we can also use the plotting interface of
+# Since the output is a pandas dataframe, we can also use the plotting interface of
 # pandas.
 ax = metric_report.plot.barh()
 _ = ax.set_title("Metrics report")
@@ -167,20 +160,22 @@ print(f"Time taken to compute the log loss: {end - start:.2f} seconds")
 
 # %%
 #
-# By default, the metrics are computed on the test set only. However, if a training set
-# is provided, we can also compute the metrics by specifying the `data_source`
-# parameter.
+# By default, the metrics are computed on the test set only, but we can also compute
+# them on the train set:
 report.metrics.log_loss(data_source="train")
+
 
 # %%
 #
-# Be aware that we can also benefit from the caching mechanism with our own custom
-# metrics. Skore only expects that we define our own metric function to take `y_true`
-# and `y_pred` as the first two positional arguments. It can take any other arguments.
-# Let's see an example.
+# Defining custom metrics
+# =======================
+#
+# skore can compute user-defined metrics as well. It accepts metrics in the form of
+# scikit-learn scorers, i.e. functions taking `estimator`, `X` and `y` (and optional
+# keyword arguments). Let's take a look at an example.
 
 
-def operational_decision_cost(y_true, y_pred, amount):
+def operational_decision_cost(y_true, y_pred, *, amount):
     mask_true_positive = (y_true == pos_label) & (y_pred == pos_label)
     mask_true_negative = (y_true == neg_label) & (y_pred == neg_label)
     mask_false_positive = (y_true == neg_label) & (y_pred == pos_label)
@@ -194,34 +189,34 @@ def operational_decision_cost(y_true, y_pred, amount):
 
 # %%
 #
-# In our use case, we have a operational decision to make that translate the
-# classification outcome into a cost. It translate the confusion matrix into a cost
-# matrix based on some amount linked to each sample in the dataset that are provided to
-# us. Here, we randomly generate some amount as an illustration.
+# In our example use case, each classification decision we make has a cost.
+# The function above models this by translating the confusion matrix into a cost
+# matrix; this cost also depends on an extra parameter named ``amount``, to illustrate
+# that skore can handle custom metrics with non-standard arguments.
+# Let's test adding this metric to our report.
 import numpy as np
 from sklearn.metrics import make_scorer
 
 rng = np.random.default_rng(42)
 amount = rng.integers(low=100, high=1000, size=len(report.y_test))
 
+# We use `make_scorer` to convert the metric to the right format (a function
+# that takes `estimator`, `X`, `y`)
 report.metrics.add(metric=make_scorer(operational_decision_cost, amount=amount))
-
-cost = report.metrics.summarize(metric="operational_decision_cost")
-cost.frame()
 
 # %%
 #
-# By the way, skore caches the model predictions. It is really handy because it means
-# that we can compute some additional metrics without having to recompute the
-# the predictions.
-report.metrics.summarize(
-    metric=["precision", "recall", "operational_decision_cost"]
-).frame()
+# Our custom metric is now registered in the report, and will be shown in the summary.
+# In fact, since the underlying metric function takes `y_pred` as input, skore can use
+# the cached predictions again to speed up the computation.
+
+# The metric name is derived from the function name unless it is explicitly given
+report.metrics.summarize().frame()
 
 # %%
 #
 # Effortless one-liner plotting
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# =============================
 #
 # The :class:`skore.EstimatorReport` class also implements a number of the most common
 # data science plots.
@@ -231,16 +226,16 @@ report.metrics.help()
 
 # %%
 #
-# Let's start by plotting the ROC curve for our binary classification task.
+# Let's plot the ROC curve for our binary classification task.
 display = report.metrics.roc()
 display.plot()
 
 # %%
 #
-# The plot functionality is built upon the scikit-learn display objects. We return
-# those display (slightly modified to improve the UI) in case we want to tweak some
-# of the plot properties. We can have quick look at the available attributes and
-# methods by calling the ``help`` method or simply by printing the display.
+# The plot functionality is built upon the scikit-learn Display objects. We return
+# those Display objects (slightly modified to improve the UI) in case we want to tweak some
+# of the plot properties. We can have a quick look at the available attributes and
+# methods by calling the ``help`` method.
 display.help()
 
 # %%
@@ -284,13 +279,13 @@ print(f"Time taken to compute the ROC curve: {end - start:.2f} seconds")
 
 # %%
 # Visualizing the confusion matrix
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ================================
 #
 # Another useful visualization for classification tasks is the confusion matrix,
 # which shows the counts of correct and incorrect predictions for each class.
 
 # %%
-# Let's first start with a basic confusion matrix:
+# Let's start with a basic confusion matrix:
 cm_display = report.metrics.confusion_matrix()
 cm_display.plot()
 
@@ -299,7 +294,7 @@ cm_display.plot()
 # to convert predicted probabilities into class labels. By default, skore uses a
 # threshold of 0.5, but confusion matrices are actually computed at every threshold
 # internally.
-
+#
 # To visualize the confusion matrix at a different threshold, use the
 # ``threshold_value`` parameter. For example, a threshold of 0.3 will classify
 # more samples as positive:
@@ -320,7 +315,6 @@ cm_display.plot()
 # Finally, the confusion matrix can also be exported as a pandas DataFrame for further
 # analysis:
 cm_display.frame()
-
 
 # %%
 # .. seealso::
