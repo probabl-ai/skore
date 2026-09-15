212c3b9d2cf58f0fda1860c345ba3b8b96cbfa0f
Thomas S. <2565098+thomass-dev@users.noreply.github.com>
2026-09-15 17:11:56 +0200

    [ci skip] feat(plugins/hub/client): Fallback to API keys registry

--- skore/src/skore/_plugins/hub/authentication/login.py
+++ skore/src/skore/_plugins/hub/authentication/login.py
@@ -1,7 +1,5 @@
 """Login to ``skore hub``."""
 
-from collections.abc import Callable
-from logging import getLogger
 from os import environ
 
 from rich.align import Align
@@ -9,68 +7,32 @@ from rich.live import Live
 from rich.panel import Panel
 
 from skore import console
-from skore._plugins.hub.authentication.api_key import API_key, APIKeyError
-from skore._plugins.hub.authentication.token import Token
-from skore._plugins.hub.authentication.uri import URI
-
-logger = getLogger(__name__)
-
-#
-# Global variable storing credentials used for authentication by the ``HUBClient``, both
-# with an API key or a temporary token.
-#
-# By default, it is empty and must be initialized by the user by calling explicitly the
-# function ``login``.
-#
-credentials: Callable[[], dict[str, str]] | None = None
+from skore._plugins.hub.authentication.api_key import API_key
+from skore._plugins.hub.authentication.token import Token, token
 
 
 def login(*, timeout: int = 600) -> None:
-    """Login to ``skore hub``.
+    """Login to ``skore hub``."""
+    global token
 
-    This function is a no op if SKORE_HUB_JUPYTERLITE
-    """
-    if environ.get("SKORE_HUB_JUPYTERLITE", "").lower() in ("1", "true", "yes"):
+    if (
+        (environ.get("SKORE_HUB_JUPYTERLITE", "").lower() in ("1", "true", "yes"))
+        or (token is not None)
+        or API_key.available()
+    ):
         return
 
-    global credentials
+    with Live(console=console, auto_refresh=False) as live:
+        token = Token(timeout=timeout, live=live)
 
-    if credentials is not None:
-        logger.debug(f"Already logged in {URI()} with {credentials.__module__}.")
-        console.print(
+        live.update(
             Panel(
-                Align.center("Already logged in."),
-                title="[cyan]Login to [b]Skore Hub",
-                border_style="cyan",
-                padding=1,
-            )
-        )
-
-        return
-
-    try:
-        credentials = API_key()
-    except APIKeyError:
-        with Live(console=console, auto_refresh=False) as live:
-            credentials = Token(timeout=timeout, live=live)
-
-            live.update(
-                Panel(
-                    Align.center(
-                        "Successfully logged in, using [b]interactive authentication."
-                    ),
-                    title="[cyan]Login to [bold]Skore Hub",
-                    border_style="cyan",
-                    padding=1,
-                )
-            )
-            live.refresh()
-    else:
-        console.print(
-            Panel(
-                Align.center("Successfully logged in, using [b]API key."),
+                Align.center(
+                    "Successfully logged in, using [b]interactive authentication."
+                ),
                 title="[cyan]Login to [bold]Skore Hub",
                 border_style="cyan",
                 padding=1,
             )
         )
+        live.refresh()
